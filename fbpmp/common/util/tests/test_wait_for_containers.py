b572195dd8a9c0d6a4514e69523bc17226b037b0
Jacob Rodal <jrodal@fb.com>
2021-08-17 18:26:53 -0700

    wait_for_containers_async function (#121)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcp/pull/121
    
    ## What is this diff stack
    
    * Reduces ID Match thrift latency from 96 minutes -> 6 minutes without any thrift level changes or PrivateComputationService changes
    * Modifies how PIDDispatcher and PIDStage.run flow works. Specifically, it allows individual stages from the PID DAG to be run (instead of running all) and makes waiting for containers to finish optional
    
    ## What is this diff
    
    * Adds a `wait_for_containers_async` function to a util file that will wait for containers to reach COMPLETED
    * Adds unit tests for the new function
    
    ## Why
    
    * each PIDStage and MPCService all wait for containers to complete. Instead of sharing the same function, they each have their own effectively identical implementation. 4+ scattered implementations of the same method is unnecessary.
    * tests are good
    
    ## Next diff
    
    * wait_for_containers argument added as a parameter to PIDStage.run
    
    Differential Revision: D30268090
    
    fbshipit-source-id: bb4929c68183e7e2aab8411afecf6364c9f291e9

---
+++ fbpmp/common/util/tests/test_wait_for_containers.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from unittest import mock, IsolatedAsyncioTestCase
+from unittest.mock import patch
+
+from fbpcp.entity.container_instance import ContainerInstance, ContainerInstanceStatus
+from fbpcp.service.onedocker import (
+    OneDockerService,
+)
+from fbpmp.common.util.wait_for_containers import wait_for_containers_async
+
+
+class TestWaitForContainersAsync(IsolatedAsyncioTestCase):
+    @patch("fbpcp.service.container.ContainerService")
+    def setUp(self, MockContainerService):
+        self.container_svc = MockContainerService()
+        self.onedocker_svc = OneDockerService(self.container_svc, "task_def")
+
+    @mock.patch("fbpcp.service.onedocker.OneDockerService.get_containers")
+    async def test_wait_for_containers_success(self, get_containers):
+        container_1_start = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_1",
+            "192.0.2.0",
+            ContainerInstanceStatus.STARTED,
+        )
+        container_2_start = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_2",
+            "192.0.2.1",
+            ContainerInstanceStatus.STARTED,
+        )
+
+        container_1_complete = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_1",
+            "192.0.2.0",
+            ContainerInstanceStatus.COMPLETED,
+        )
+
+        container_2_complete = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_2",
+            "192.0.2.1",
+            ContainerInstanceStatus.COMPLETED,
+        )
+
+        get_containers.side_effect = [
+            [container_1_start],
+            [container_1_complete],
+            [container_2_complete],
+        ]
+
+        containers = [
+            container_1_start,
+            container_2_start,
+        ]
+
+        updated_containers = await wait_for_containers_async(
+            self.onedocker_svc, containers, poll=0
+        )
+
+        self.assertEqual(updated_containers[0], container_1_complete)
+        self.assertEqual(updated_containers[1], container_2_complete)
+
+    @mock.patch("fbpcp.service.onedocker.OneDockerService.get_containers")
+    async def test_wait_for_containers_fail(self, get_containers):
+        container_1_start = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_1",
+            "192.0.2.0",
+            ContainerInstanceStatus.STARTED,
+        )
+        container_2_start = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_2",
+            "192.0.2.1",
+            ContainerInstanceStatus.STARTED,
+        )
+
+        container_1_complete = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_1",
+            "192.0.2.0",
+            ContainerInstanceStatus.COMPLETED,
+        )
+
+        container_2_fail = ContainerInstance(
+            "arn:aws:ecs:region:account_id:task/container_id_2",
+            "192.0.2.1",
+            ContainerInstanceStatus.FAILED,
+        )
+
+        get_containers.side_effect = [
+            [container_1_start],
+            [container_1_complete],
+            [container_2_fail],
+        ]
+
+        containers = [
+            container_1_start,
+            container_2_start,
+        ]
+
+        updated_containers = await wait_for_containers_async(
+            self.onedocker_svc, containers, poll=0
+        )
+
+        self.assertEqual(updated_containers[0], container_1_complete)
+        self.assertEqual(updated_containers[1], container_2_fail)
---
+++ fbpmp/common/util/wait_for_containers.py
@@ -0,0 +1,40 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import asyncio
+from typing import List
+
+from fbpcp.entity.container_instance import ContainerInstanceStatus, ContainerInstance
+from fbpcp.service.onedocker import OneDockerService
+
+
+DEFAULT_WAIT_FOR_CONTAINER_POLL = 5
+
+
+async def wait_for_containers_async(
+    onedocker_svc: OneDockerService,
+    containers: List[ContainerInstance],
+    poll: int = DEFAULT_WAIT_FOR_CONTAINER_POLL,
+) -> List[ContainerInstance]:
+    updated_containers = containers.copy()
+    end_states = {
+        ContainerInstanceStatus.COMPLETED,
+        ContainerInstanceStatus.FAILED,
+    }
+    for i, container in enumerate(updated_containers):
+        instance_id = container.instance_id
+        onedocker_svc.logger.info(f"Waiting for container {instance_id} to complete")
+        status = container.status
+        while status not in end_states:
+            await asyncio.sleep(poll)
+            container = onedocker_svc.get_containers([instance_id])[0]
+            status = container.status
+            updated_containers[i] = container
+        if status is not ContainerInstanceStatus.COMPLETED:
+            onedocker_svc.logger.warning(
+                f"Container {instance_id} failed with status {status}"
+            )
+            return updated_containers
+    return updated_containers
