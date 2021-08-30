a55701fbaf39bbd321129471d20e0441d224b4c3
Zhenhuan Zhang <anthonyzhang@fb.com>
2021-08-30 09:57:32 -0700

    semi-automated data ingestion: Initial AWS resource definitions by terraform script (#29)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/29
    
    **Context**
    semi-automated data ingestion is a mitigation to support semi-automated app events ingestion for Cloudbridge. It could also be a generic, semi-automated solution for ingesting any other event types in future.
    
    More details can be found in this design doc: https://docs.google.com/document/d/1fGiwVHVIG3xa5fuQR80d-Z43vpXaE2yZDWoB6F892Hk/edit?usp=sharing
    
    **What**
    
    In this diff, we have two python scripts:
    1) `lambda_trigger.py`: a trigger function when a new dataset (in csv format) uploaded to a given S3 bucket.
    2) `glue_ETL.py`: the ETL python script to process the data and merge with the same S3 destination of the standard CB data ingestion pipeline.
    3) `main.tf`, `lambda.tf`, `glue.tf`, `variable.tf`: `main.tf` contains the necessary init, `lambda.tf` (`glue.tf`) contains all resources related to lambda trigger function (glue etl jobs). `variable.tf` contains all variables being used.
    
    **Next**
    1. Integrate these tf scripts and python scripts into deploy.sh
    2. load test (maximum data size per dataset)
    
    Differential Revision: D30535385
    
    fbshipit-source-id: c2f8fa6f4f021ce642acc2dcd845a368041880d2

---
+++ fbpmp/infra/cloud_bridge/semi_automated_data_ingestion/glue.tf
@@ -0,0 +1,87 @@
+resource "aws_s3_bucket_object" "upload_glue_ETL" {
+  bucket = "${var.app_data_input_bucket}${var.tag_postfix}"
+  key    = "glue_ETL.py"
+  source = "glue_ETL.py"
+  etag   = filemd5("glue_ETL.py")
+}
+
+resource "aws_iam_role" "glue_ETL_role" {
+  name               = "glue-service-role${var.tag_postfix}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "glue.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = "${aws_iam_role.glue_ETL_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_access" {
+  role       = "${aws_iam_role.glue_ETL_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy" "s3_policy" {
+  name   = "s3-policy${var.tag_postfix}"
+  role   = "${aws_iam_role.glue_ETL_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${var.app_data_input_bucket}",
+        "arn:aws:s3:::${var.app_data_input_bucket}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "kms_policy_glue" {
+  name   = "kms-policy${var.tag_postfix}"
+  role   = "${aws_iam_role.glue_ETL_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "kms:*",
+      "Resource": [
+        "arn:aws:kms:*:${var.aws_account_id}:key/*",
+        "arn:aws:kms:*:${var.aws_account_id}:alias/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_glue_job" "glue_job" {
+  name     = "glue-ETL${var.tag_postfix}"
+  role_arn = aws_iam_role.glue_ETL_role.arn
+
+  command {
+    script_location = "s3://${var.app_data_input_bucket}${var.tag_postfix}/glue_ETL.py"
+  }
+}
---
+++ fbpmp/infra/cloud_bridge/semi_automated_data_ingestion/lambda_trigger.py
@@ -0,0 +1,54 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import print_function
+
+# Set up logging
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Import Boto 3 for AWS Glue
+import boto3
+
+client = boto3.client("glue")
+
+# Variables for the job:
+# same name as the aws_glue_job resource in glue.tf
+# [TODO]: in deploy.sh replace the hardcoded name with shell edits (i.e., sed i...)
+glueJobName = "glue-ETL"
+
+# Define Lambda function
+def lambda_handler(event, context):
+
+    ### should be one single upload (it should be larger than 1 by default)
+    if len(event["Records"]) >= 2:
+        logger.info("multiple csv uploaded. please upload only one csv at a time")
+    elif len(event["Records"]) == 0:
+        logger.info("no csv uploaded to S3. wrong trigger of lambda")
+    else:
+        record = event["Records"][0]
+
+        ### extract s3 bucket and path
+        s3_info = record["s3"]
+        logger.info("s3_info: ")
+        logger.info(s3_info)
+        s3_bucket = s3_info["bucket"]["name"]
+        s3_object_key = s3_info["object"]["key"]
+        s3_read_path = s3_bucket + "/" + s3_object_key
+        # [TODO: current s3_write_path is hardcoded, will be updated when integrating into deploy.sh]
+        s3_write_path = "semi-automated-app-event-ingestion/write/"
+        logger.info("s3_read_path: " + s3_read_path)
+        response = client.start_job_run(
+            JobName=glueJobName,
+            Arguments={
+                "--s3_read_path": s3_read_path,
+                "--s3_write_path": s3_write_path,
+            },
+        )
+        logger.info("## STARTED GLUE JOB: " + glueJobName)
+        logger.info("## GLUE JOB RUN ID: " + response["JobRunId"])
+        return response
