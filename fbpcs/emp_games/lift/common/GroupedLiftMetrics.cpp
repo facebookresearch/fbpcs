52a0be1c7886271125c292e9e06be0601d3a5c4c
Logan Gore <gorel@fb.com>
2021-09-21 21:51:30 -0700

    Define publisherBreakdowns field for lift
    Differential Revision: D30662591
    
    fbshipit-source-id: e2c8a53db72bcb1208e1558b11876a324e477024

--- fbpcs/emp_games/lift/common/GroupedLiftMetrics.cpp
+++ fbpcs/emp_games/lift/common/GroupedLiftMetrics.cpp
@@ -17,21 +17,24 @@
 namespace private_lift {
 bool GroupedLiftMetrics::operator==(
     const GroupedLiftMetrics& other) const noexcept {
-  return metrics == other.metrics && cohortMetrics == other.cohortMetrics;
+  return metrics == other.metrics && cohortMetrics == other.cohortMetrics
+      && publisherBreakdowns == other.publisherBreakdowns;
 }
 
 GroupedLiftMetrics GroupedLiftMetrics::operator+(
     const GroupedLiftMetrics& other) const noexcept {
   return GroupedLiftMetrics{
       metrics + other.metrics,
-      fbpcf::vector::Add(cohortMetrics, other.cohortMetrics)};
+      fbpcf::vector::Add(cohortMetrics, other.cohortMetrics),
+      fbpcf::vector::Add(publisherBreakdowns, other.publisherBreakdowns)};
 }
 
 GroupedLiftMetrics GroupedLiftMetrics::operator^(
     const GroupedLiftMetrics& other) const noexcept {
   return GroupedLiftMetrics{
       metrics ^ other.metrics,
-      fbpcf::vector::Xor(cohortMetrics, other.cohortMetrics)};
+      fbpcf::vector::Xor(cohortMetrics, other.cohortMetrics),
+      fbpcf::vector::Xor(publisherBreakdowns, other.publisherBreakdowns)};
 }
 
 std::ostream& operator<<(
@@ -41,28 +44,42 @@ std::ostream& operator<<(
 }
 
 std::string GroupedLiftMetrics::toJson() const {
-  auto container = folly::dynamic::array();
+  auto cohortContainer = folly::dynamic::array();
   std::transform(
       cohortMetrics.begin(),
       cohortMetrics.end(),
-      std::back_inserter(container),
+      std::back_inserter(cohortContainer),
+      [](auto m) { return m.toDynamic(); });
+  auto breakdownContainer = folly::dynamic::array();
+  std::transform(
+      publisherBreakdowns.begin(),
+      publisherBreakdowns.end(),
+      std::back_inserter(breakdownContainer),
       [](auto m) { return m.toDynamic(); });
   folly::dynamic obj = folly::dynamic::object("metrics", metrics.toDynamic())(
-      "cohortMetrics", container);
+      "cohortMetrics", cohortContainer)(
+      "publisherBreakdowns", breakdownContainer);
 
   return folly::toJson(obj);
 }
 
 GroupedLiftMetrics GroupedLiftMetrics::fromJson(const std::string& str) {
   auto obj = folly::parseJson(str);
-  std::vector<LiftMetrics> container;
+  std::vector<LiftMetrics> cohortContainer;
   std::transform(
       obj["cohortMetrics"].begin(),
       obj["cohortMetrics"].end(),
-      std::back_inserter(container),
+      std::back_inserter(cohortContainer),
+      [](auto m) { return LiftMetrics::fromDynamic(m); });
+
+  std::vector<LiftMetrics> breakdownContainer;
+  std::transform(
+      obj["publisherBreakdowns"].begin(),
+      obj["publisherBreakdowns"].end(),
+      std::back_inserter(breakdownContainer),
       [](auto m) { return LiftMetrics::fromDynamic(m); });
 
   return GroupedLiftMetrics{
-      LiftMetrics::fromDynamic(obj["metrics"]), container};
+      LiftMetrics::fromDynamic(obj["metrics"]), cohortContainer, breakdownContainer};
 }
 } // namespace private_lift
