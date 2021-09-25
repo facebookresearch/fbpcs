b2515ab5724568ddb1e4981759d7021305d7659e
Olivia Liu <olivialpx@fb.com>
2021-09-24 17:30:21 -0700

    id match
    Differential Revision: D31099065
    
    fbshipit-source-id: 6974af167c0da8adc74d3af10cff84e59c711966

--- fbpcs/private_computation/test/repository/test_private_computation_instance_local.py
+++ fbpcs/private_computation/test/repository/test_private_computation_instance_local.py
@@ -47,6 +47,7 @@ class TestLocalPrivateComputationInstanceRepository(unittest.TestCase):
             num_pid_containers=4,
             num_mpc_containers=4,
             concurrency=1,
+            fail_fast=True,
         )
         self.repo.create(test_read_private_computation_instance)
         self.assertEqual(
@@ -69,6 +70,7 @@ class TestLocalPrivateComputationInstanceRepository(unittest.TestCase):
             num_pid_containers=4,
             num_mpc_containers=4,
             concurrency=1,
+            fail_fast=True,
         )
         # Create a new MPC instance to be added to instances
         self.repo.create(test_update_private_computation_instance)
