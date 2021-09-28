16d9cceee13cd34e3441601bdf70570e9381ced6
Jacob Rodal <jrodal@fb.com>
2021-09-27 21:35:07 -0700

    add IdMatchStageService tests
    Summary:
    ## What
    
    * Added tests for id match stage service
    
    ## Why
    
    tests are good
    
    Context here: https://fb.workplace.com/groups/164332244998024/permalink/711906473573929/
    
    Differential Revision: D31223828
    
    fbshipit-source-id: d437616a78bd7f5156df682aad416f5a6a7ddfeb

---
+++ fbpcs/private_computation/test/service/test_id_match_stage_service.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from fbpcs.private_computation.service.id_match_stage_service import IdMatchStageService
+from unittest import IsolatedAsyncioTestCase
+from fbpcs.private_computation.entity.private_computation_instance import (
+    PrivateComputationGameType,
+    PrivateComputationInstance,
+    PrivateComputationRole,
+)
+from unittest.mock import AsyncMock, MagicMock, patch
+from fbpcs.pid.entity.pid_instance import PIDInstance, PIDInstanceStatus
+from fbpcs.private_computation.entity.private_computation_instance import (
+    PrivateComputationInstanceStatus,
+)
+from fbpcs.pid.entity.pid_instance import PIDProtocol, PIDRole
+
+
+class TestIdMatchStageService(IsolatedAsyncioTestCase):
+    @patch("fbpcs.pid.service.pid_service.pid.PIDService")
+    async def test_run_async(self, pid_svc_mock) -> None:
+
+        pc_instance = PrivateComputationInstance(
+            instance_id="123",
+            role=PrivateComputationRole.PUBLISHER,
+            instances=[],
+            status=PrivateComputationInstanceStatus.CREATED,
+            status_update_ts=1600000000,
+            num_pid_containers=1,
+            num_mpc_containers=1,
+            num_files_per_mpc_container=1,
+            game_type=PrivateComputationGameType.LIFT,
+            input_path="456",
+            output_dir="789",
+        )
+
+        pid_instance = PIDInstance(
+            instance_id="123_id_match0",
+            protocol=PIDProtocol.UNION_PID,
+            pid_role=PIDRole.PUBLISHER,
+            num_shards=2,
+            input_path=pc_instance.input_path,
+            output_path=pc_instance.pid_stage_output_data_path,
+            status=PIDInstanceStatus.STARTED,
+        )
+
+        pid_svc_mock.create_instance = MagicMock(return_value=pid_instance)
+        pid_svc_mock.run_instance = AsyncMock()
+
+        stage_svc = IdMatchStageService(
+            pid_svc_mock,
+            pid_config={},
+            protocol=PIDProtocol.UNION_PID,
+        )
+        await stage_svc.run_async(pc_instance)
+        self.assertIsInstance(pc_instance.instances[0], PIDInstance)
+
+    def test_map_private_computation_role_to_pid_role(self):
+        self.assertEqual(
+            PIDRole.PUBLISHER,
+            IdMatchStageService._map_private_computation_role_to_pid_role(
+                PrivateComputationRole.PUBLISHER
+            ),
+        )
+        self.assertEqual(
+            PIDRole.PARTNER,
+            IdMatchStageService._map_private_computation_role_to_pid_role(
+                PrivateComputationRole.PARTNER
+            ),
+        )
