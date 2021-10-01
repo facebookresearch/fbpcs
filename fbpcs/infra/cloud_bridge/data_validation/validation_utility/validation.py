18d1216bcfb0c1e35d41768633f50ba1e78ce5c8
Mark Sliva <marksliva@fb.com>
2021-09-30 17:32:57 -0700

    Add header and line validation
    Summary:
    Check that the header has at least 1 identity field, and that all required
    fields are in the header and in each row when generating the validation report.
    
    Differential Revision: D31243204
    
    fbshipit-source-id: bf4d29a15709a160dd3447f1117b74b72eef3cfa

--- fbpcs/infra/cloud_bridge/data_validation/validation_utility/validation.py
+++ fbpcs/infra/cloud_bridge/data_validation/validation_utility/validation.py
@@ -4,14 +4,99 @@
 # LICENSE file in the root directory of this source tree.
 
 import boto3
+import csv
+from botocore.response import StreamingBody
+from typing import List, Optional
 
-def validate_and_generate_report(bucket, key):
-    report = ['Validation Summary:']
+ALL_REQUIRED_FIELDS = {
+    'action_source',
+    'conversion_value',
+    'currency_type',
+    'event_type',
+    'timestamp',
+}
+ONE_OR_MORE_REQUIRED_FIELDS = {'email','device_id'}
+
+def validate_and_generate_report(bucket: str, key: str) -> str:
     s3_client = boto3.client('s3')
     response = s3_client.get_object(Bucket=bucket, Key=key)
     body = response['Body']
-    lines = 0
-    for _ in body.iter_lines():
-        lines += 1
-    report.append('Total lines: %s' % lines)
+    try:
+        return generate_from_body(body)
+    except BaseException as e:
+        return f'Something went wrong while validating the data. Exception details if available:\n{e}'
+
+def header_check_fields_missing(header_fields: List[str]) -> bool:
+    fields_missing = ALL_REQUIRED_FIELDS.difference(set(header_fields))
+    return sorted(fields_missing)
+
+def header_contains_identity_fields(header_fields: List[str]) -> bool:
+    intersection = ONE_OR_MORE_REQUIRED_FIELDS.intersection(set(header_fields))
+    return len(intersection) > 0
+
+def is_line_valid(line: str) -> bool:
+    for field in ALL_REQUIRED_FIELDS:
+        if field not in line or value_empty(line[field]):
+            return False
+
+    return any(
+        field in line and not value_empty(line[field])
+        for field in ONE_OR_MORE_REQUIRED_FIELDS
+    )
+
+def value_empty(value: Optional[str]) -> bool:
+    return (
+        str(value).strip() == '' or
+        value is None
+    )
+
+def generate_from_body(body: StreamingBody) -> str:
+    validation_state = ValidationState()
+    valid_header_row = None
+
+    for line in body.iter_lines():
+        line_string = line.decode('utf-8')
+        if valid_header_row:
+            reader = csv.DictReader([valid_header_row, line_string])
+            for parsed_line in reader:
+                if is_line_valid(parsed_line):
+                    validation_state.valid_rows += 1
+                else:
+                    validation_state.error_rows += 1
+                validation_state.total_rows += 1
+        else:
+            header_fields = csv.DictReader([line_string]).fieldnames
+            header_row_valid = True
+            missing_fields = header_check_fields_missing(header_fields)
+            if len(missing_fields) > 0:
+                missing_fields_str = ','.join(missing_fields)
+                validation_state.header_validation_messages.append(
+                    f'Header row not valid, missing `{missing_fields_str}` required fields.'
+                )
+                header_row_valid = False
+            if not header_contains_identity_fields(header_fields):
+                required_header_fields = ','.join(sorted(ONE_OR_MORE_REQUIRED_FIELDS))
+                validation_state.header_validation_messages.append(
+                    f'Header row not valid, at least one of `{required_header_fields}` is required.'
+                )
+                header_row_valid = False
+            if not header_row_valid:
+                validation_state.header_validation_messages.append(
+                    'Validation processing stopped.'
+                )
+                break
+            valid_header_row = line_string
+
+    report = ['Validation Summary:']
+    report.append(f'Total rows: {validation_state.total_rows}')
+    report.append(f'Valid rows: {validation_state.valid_rows}')
+    report.append(f'Rows with errors: {validation_state.error_rows}')
+    report.extend(validation_state.header_validation_messages)
     return '\n'.join(report) + '\n'
+
+class ValidationState:
+    def __init__(self):
+        self.total_rows = 0
+        self.valid_rows = 0
+        self.error_rows = 0
+        self.header_validation_messages = []
