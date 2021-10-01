25eefa71356739db601da1fa6d215f7fee06aa39
Mark Sliva <marksliva@fb.com>
2021-10-01 10:27:17 -0700

    add lambda job that triggers on S3 CSV uploads (#102)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/102
    
    Add terraform deployment that processes uploaded events_data CSVs in an
    automated lambda processing pipeline.
    CSVs uploaded to:
    - s3://upload_and_validation_s3_bucket/events_data_upload_s3_key
    are processed and the result is uploaded to:
    - s3://upload_and_validation_s3_bucket/validation_results_s3_key
    
    [Data Validation doc](https://docs.google.com/document/d/1-BSpAUqCjHmUJUjdzUbww6DrRk0DLSVLyMiowHlF8Gg/edit)
    
    Reviewed By: peking2
    
    Differential Revision: D31173407
    
    fbshipit-source-id: 4203e9f816d0615fad1264bc1b07eb50ff0f28ef

---
+++ fbpcs/infra/cloud_bridge/data_validation/main.tf
@@ -0,0 +1,109 @@
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+provider "archive" {}
+
+terraform {
+  backend "s3" {}
+}
+
+data "archive_file" "lambda_source_package" {
+  type        = "zip"
+  source_dir  = "validation_utility/"
+  output_path = "validation_lambda${var.tag_postfix}.zip"
+}
+
+resource "aws_s3_bucket_object" "lambda_trigger_object" {
+  bucket = var.upload_and_validation_s3_bucket
+  key    = "data-validation-lambda-package${var.tag_postfix}.zip"
+  source = data.archive_file.lambda_source_package.output_path
+  etag   = filemd5("validation_lambda${var.tag_postfix}.zip")
+}
+
+resource "aws_iam_role" "lambda_iam" {
+  name = "lambda-iam${var.tag_postfix}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "upload_lambda_trigger" {
+  s3_bucket     = var.upload_and_validation_s3_bucket
+  s3_key        = "data-validation-lambda-package${var.tag_postfix}.zip"
+  function_name = "data-validation-lambda-trigger${var.tag_postfix}"
+  role          = aws_iam_role.lambda_iam.arn
+  handler       = "lambda_main.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 900
+  memory_size   = 1024
+  environment {
+    variables = {
+      VALIDATION_DEBUG_MODE           = var.validation_debug_mode
+      UPLOAD_AND_VALIDATION_S3_BUCKET = var.upload_and_validation_s3_bucket
+      VALIDATION_RESULTS_S3_KEY       = var.validation_results_s3_key
+    }
+  }
+}
+
+resource "aws_lambda_permission" "allow_upload_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upload_lambda_trigger.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.upload_and_validation_s3_bucket}"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = var.upload_and_validation_s3_bucket
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.upload_lambda_trigger.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "${var.events_data_upload_s3_key}/"
+    filter_suffix       = ".csv"
+  }
+
+  depends_on = [aws_lambda_permission.allow_upload_bucket]
+}
+
+resource "aws_iam_role_policy" "s3_policy_lambda" {
+  name   = "lambda-s3-policy${var.tag_postfix}"
+  role   = aws_iam_role.lambda_iam.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": [
+          "arn:aws:s3:::${var.upload_and_validation_s3_bucket}/${var.validation_results_s3_key}",
+          "arn:aws:s3:::${var.upload_and_validation_s3_bucket}/${var.validation_results_s3_key}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": [
+          "arn:aws:s3:::${var.upload_and_validation_s3_bucket}/${var.events_data_upload_s3_key}",
+          "arn:aws:s3:::${var.upload_and_validation_s3_bucket}/${var.events_data_upload_s3_key}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
---
+++ fbpcs/infra/cloud_bridge/data_validation/validation_utility/lambda_main.py
@@ -0,0 +1,76 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import boto3
+import os
+import urllib
+import validation
+
+from datetime import datetime
+
+DEBUG_MODE = str(os.environ.get('VALIDATION_DEBUG_MODE')) == '1'
+
+def debug_log(msg: str) -> None:
+    if DEBUG_MODE:
+        print(f'{msg}\n')
+
+def write_result_to_s3(bucket_name: str, bucket_key: str, file_name: str, file_contents: str) -> None:
+    debug_log(
+        '\n'.join([
+            'write_result_to_s3:',
+            f'bucket_name: {bucket_name}',
+            f'bucket_key: {bucket_key}',
+            f'file_name: {file_name}',
+            f'file_contents: {file_contents}',
+        ])
+    )
+    s3_client = boto3.client('s3')
+    key = bucket_key + '/' + file_name
+    response = s3_client.put_object(
+        Body=file_contents.encode('utf-8'),
+        Bucket=bucket_name,
+        Key=key
+    )
+    debug_log(response)
+
+# context type is: awslambdaric.lambda_context.LambdaContext
+def lambda_handler(event, context):
+    output_bucket_name = os.environ.get('UPLOAD_AND_VALIDATION_S3_BUCKET')
+    output_bucket_key = os.environ.get('VALIDATION_RESULTS_S3_KEY')
+    if not (output_bucket_name and output_bucket_key):
+        raise Exception(f'Exception: missing either output_bucket_name: `{output_bucket_name}` or output_bucket_key: `{output_bucket_key}`')
+
+    debug_log(
+        f'output bucket: {output_bucket_name}\noutput key: {output_bucket_key}'
+    )
+
+    try:
+        # todo: loop over all records
+        record = event['Records'][0]
+        input_bucket = record['s3']['bucket']['name']
+        input_key = urllib.parse.unquote_plus(record['s3']['object']['key'], encoding='utf-8')
+        debug_log(
+            f'input bucket: {input_bucket}\ninput key: {input_key}'
+        )
+        validation_results = validation.validate_and_generate_report(input_bucket, input_key)
+
+        input_filename = input_key.split('/')[-1]
+        validation_result_file_path = '_'.join([
+            input_filename,
+            'validation-results',
+            datetime.now().isoformat(),
+        ])
+
+        write_result_to_s3(
+            output_bucket_name,
+            output_bucket_key,
+            validation_result_file_path,
+            validation_results
+        )
+    except BaseException as e:
+        print(f'Unexpected error occurred: {e}')
+
+    debug_log('Finished validation processing')
+    return {'records': []}
