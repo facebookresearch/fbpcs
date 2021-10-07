4be5cf78363c586320d3e696c07bcd1af35f583f
Lucas Tavares <lucaskt@fb.com>
2021-10-07 05:50:02 -0700

    Add Logging to Deployment Controller
    Summary:
    Currently it's very difficult to track any problems in the deployment controller, and this diff adds logging through the process of deployment
    Despite being debug information, Logs are mostly set on info level, as debug and trace add a LOT of noise from the server itself.
    
    Differential Revision: D30459731
    
    fbshipit-source-id: 0ee0012f3307aa76a816095fbbfcecd108b3ddf7

--- fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeployController.java
+++ fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeployController.java
@@ -17,6 +17,8 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +29,8 @@ public class DeployController {
   private Lock singleProvisioningLock = new ReentrantLock();
   private Process provisioningProcess;
   private final String NO_UPDATES_MESSAGE = "No updates";
+
+  Logger logger = LoggerFactory.getLogger(DeployController.class);
 
   enum DeploymentStatusRunning {
     DEPLOYMENT_NOT_STARTED,
@@ -74,38 +78,61 @@ public class DeployController {
       consumes = "application/json",
       produces = "application/json")
   public DeploymentResult deploymentCreate(@RequestBody DeploymentParams deployment) {
-    if (!deployment.validRegion())
+    logger.info("Received deployment request: " + deployment.toString());
+
+    if (!deployment.validRegion()) {
+      logger.warn("  Invalid region: " + deployment.region);
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED, "Invalid Region: " + deployment.region);
-    if (!deployment.validAccountID())
+    }
+    if (!deployment.validAccountID()) {
+      logger.warn("  Invalid account ID: " + deployment.accountId);
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED, "Invalid Account ID: " + deployment.accountId);
-    if (!deployment.validPubAccountID())
+    }
+    if (!deployment.validPubAccountID()) {
+      logger.warn("  Invalid publisher account ID: " + deployment.pubAccountId);
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED,
           "Invalid Publisher Account ID: " + deployment.pubAccountId);
-    if (!deployment.validVpcID())
+    }
+    if (!deployment.validVpcID()) {
+      logger.warn("  Invalid VPC ID: " + deployment.vpcId);
       return new DeploymentResult(
-          DeploymentResultSuccessful.STATUS_FAILED, "Invalid VPC: " + deployment.vpcId);
-    if (!deployment.validTagPostfix())
+          DeploymentResultSuccessful.STATUS_FAILED, "Invalid VPC ID: " + deployment.vpcId);
+    }
+    if (!deployment.validTagPostfix()) {
+      logger.warn("  Invalid tag postfix: " + deployment.tag);
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED, "Invalid Tag Postfix: " + deployment.tag);
-    if (!deployment.validStorageID())
+    }
+    if (!deployment.validStorageID()) {
+      logger.warn("  Invalid terraform config storage bucket: " + deployment.storage);
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED,
-          "Invalid S3 storage bucket: " + deployment.storage);
-    if (!deployment.validIngestionOutputID())
+          "Invalid terraform config storage bucket: " + deployment.storage);
+    }
+    if (!deployment.validIngestionOutputID()) {
+      logger.warn("  Invalid data ingestion bucket: " + deployment.ingestionOutput);
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED,
-          "Invalid S3 ingestion output bucket: " + deployment.ingestionOutput);
-    if (!deployment.validAccessKeyId())
+          "Invalid data ingestion bucket: " + deployment.ingestionOutput);
+    }
+    if (!deployment.validAccessKeyId()) {
+      logger.warn("  Invalid AWS access key ID");
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED, "Invalid AWS Access Key ID");
-    if (!deployment.validSecretAccessKey())
+    }
+    if (!deployment.validSecretAccessKey()) {
+      logger.warn("  Invalid AWS secret access key");
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_FAILED, "Invalid AWS Secret Access Key");
+    }
+
+    logger.info("  Validated input");
 
     if (singleProvisioningLock.tryLock()) {
+      logger.info("  No deployment conflicts found");
       deploymentStatus.status = DeploymentStatusRunning.DEPLOYMENT_RUNNING;
       deploymentStatus.message = null;
 
@@ -128,6 +155,7 @@ public class DeployController {
         deployCommand.add("-t");
         deployCommand.add(deployment.tag);
       }
+      logger.info("  Deploy command built: " + deployCommand);
 
       try {
         ProcessBuilder pb = new ProcessBuilder(deployCommand);
@@ -144,27 +172,32 @@ public class DeployController {
         pb.redirectErrorStream(true);
         pb.directory(new File("/terraform_deployment"));
         provisioningProcess = pb.start();
+        logger.info("  Creating deployment process");
         try {
           provisioningProcess.waitFor(30, TimeUnit.MINUTES);
+          logger.info("  Deployment process finished");
 
           synchronized (singleUpdateMutex) {
             deploymentStatus.status = DeploymentStatusRunning.DEPLOYMENT_FINISHED;
           }
           int exitCode = provisioningProcess.exitValue();
           if (exitCode == 0) {
+            logger.info("  Deployment finished successfully");
             return new DeploymentResult(
                 DeploymentResultSuccessful.STATUS_SUCCESS, "Deployed successfully");
           } else {
+            logger.error("  Deployment failed with exit code: " + String.valueOf(exitCode));
             return new DeploymentResult(
                 DeploymentResultSuccessful.STATUS_FAILED,
-                "Deployment failed with exit code: "
-                    + String.valueOf(provisioningProcess.exitValue()));
+                "Deployment failed with exit code: " + String.valueOf(exitCode));
           }
         } catch (InterruptedException e) {
+          logger.error("  Deployment timed out. Message: " + e.getMessage());
           return new DeploymentResult(
               DeploymentResultSuccessful.STATUS_TIMEOUT, "Deployment timed out");
         }
       } catch (IOException e) {
+        logger.error("  Deployment could not be started. Message: " + e.getMessage());
         return new DeploymentResult(
             DeploymentResultSuccessful.STATUS_FAILED, "Could not start deployment");
       } finally {
@@ -173,6 +206,7 @@ public class DeployController {
         singleProvisioningLock.unlock();
       }
     } else {
+      logger.error("  Another deployment is in progress");
       return new DeploymentResult(
           DeploymentResultSuccessful.STATUS_BLOCKED, "Another deployment is in progress");
     }
@@ -180,12 +214,15 @@ public class DeployController {
 
   @GetMapping(path = "/v1/deployment", produces = "application/json")
   public DeploymentStatus deploymentStatus() {
+    logger.info("Received status command");
     if (deploymentStatus.status == DeploymentStatusRunning.DEPLOYMENT_NOT_STARTED) {
+      logger.info("  No deployment is running");
       return new DeploymentStatus(
           DeploymentStatusRunning.DEPLOYMENT_NOT_STARTED, NO_UPDATES_MESSAGE);
     }
 
     if (provisioningProcess == null) {
+      logger.warn("  Deployment process unavailable, with status: " + deploymentStatus.status);
       deploymentStatus = new DeploymentStatus();
       return deploymentStatus;
     }
@@ -202,12 +239,15 @@ public class DeployController {
           sb.append('\n');
         }
       } catch (IOException e) {
+        logger.debug("  Problem reading deployment process logs: " + e.getMessage());
       }
+      logger.trace("  Read " + sb.length() + " chars from deployment process logs");
 
       if (deploymentStatus.status == DeploymentStatusRunning.DEPLOYMENT_RUNNING) {
         deploymentStatus.message = sb.length() == 0 ? NO_UPDATES_MESSAGE : sb.toString();
         return deploymentStatus;
       } else {
+        logger.info("  Deployment finished");
         deploymentStatus = new DeploymentStatus();
         provisioningProcess = null;
         return new DeploymentStatus(DeploymentStatusRunning.DEPLOYMENT_FINISHED, sb.toString());
