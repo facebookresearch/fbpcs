67e0823effbbb6f6feddaabdcc05fb755c38cf48
Lucas Tavares <lucaskt@fb.com>
2021-10-07 05:50:02 -0700

    Separate Deploy Controller from Runner so that the shell can become non-blocking
    Differential Revision: D30484886
    
    fbshipit-source-id: 5df15fe19ab9e9110badb3dc544c5926ddfc8c6b

--- fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeployController.java
+++ fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeployController.java
@@ -12,16 +12,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,33 +26,8 @@ public class DeployController {
 
   private final Logger logger = LoggerFactory.getLogger(DeployController.class);
 
-  private Object singleUpdateMutex = new Object();
-  private Lock singleProvisioningLock = new ReentrantLock();
-
-  private Process provisioningProcess;
-
-  private DeploymentState deploymentState;
-
-  public DeployController() {
-    this.deploymentState = DeploymentState.STATE_NOT_STARTED;
-  }
-
-  enum DeploymentState {
-    STATE_NOT_STARTED("not started"),
-    STATE_RUNNING("running"),
-    STATE_FINISHED("finished");
-
-    private String state;
-
-    private DeploymentState(String state) {
-      this.state = state;
-    }
-
-    @JsonValue
-    public String getState() {
-      return state;
-    }
-  };
+  private DeploymentRunner runner;
+  private Semaphore singleProvisioningLock = new Semaphore(1);
 
   enum APIReturnStatus {
     STATUS_SUCCESS("success"),
@@ -114,132 +80,48 @@ public class DeployController {
     }
     logger.info("  Validated input");
 
-    if (singleProvisioningLock.tryLock()) {
+    try {
+      if (!singleProvisioningLock.tryAcquire()) {
+        String errorMessage = "Another deployment is in progress";
+        logger.error("  " + errorMessage);
+        return new DeploymentAPIReturn(APIReturnStatus.STATUS_FAIL, errorMessage);
+      }
       logger.info("  No deployment conflicts found");
-      deploymentState = DeploymentState.STATE_RUNNING;
 
-      List<String> deployCommand = new ArrayList<String>();
-      deployCommand.add("/bin/sh");
-      deployCommand.add("/terraform_deployment/deploy.sh");
-      deployCommand.add("-r");
-      deployCommand.add(deployment.region);
-      deployCommand.add("-a");
-      deployCommand.add(deployment.accountId);
-      deployCommand.add("-p");
-      deployCommand.add(deployment.pubAccountId);
-      deployCommand.add("-v");
-      deployCommand.add(deployment.vpcId);
-      deployCommand.add("-s");
-      deployCommand.add(deployment.storage);
-      deployCommand.add("-d");
-      deployCommand.add(deployment.ingestionOutput);
-      if (deployment.tag != null && !deployment.tag.isEmpty()) {
-        deployCommand.add("-t");
-        deployCommand.add(deployment.tag);
-      }
-      logger.info("  Deploy command built: " + deployCommand);
-
-      try {
-        ProcessBuilder pb = new ProcessBuilder(deployCommand);
-
-        Map<String, String> env = pb.environment();
-        env.put("AWS_ACCESS_KEY_ID", deployment.awsAccessKeyId);
-        env.put("AWS_SECRET_ACCESS_KEY", deployment.awsSecretAccessKey);
-        if (deployment.logLevel != DeploymentParams.LogLevel.DISABLED) {
-          if (deployment.logLevel == null) deployment.logLevel = DeploymentParams.LogLevel.DEBUG;
-          env.put("TF_LOG", deployment.logLevel.getLevel());
-          env.put("TF_LOG_PATH", "/tmp/deploy.log");
-        }
-
-        pb.redirectErrorStream(true);
-        pb.directory(new File("/terraform_deployment"));
-        provisioningProcess = pb.start();
-        logger.info("  Creating deployment process");
-        try {
-          provisioningProcess.waitFor(30, TimeUnit.MINUTES);
-          logger.info("  Deployment process finished");
-
-          synchronized (singleUpdateMutex) {
-            deploymentState = DeploymentState.STATE_FINISHED;
-          }
-          int exitCode = provisioningProcess.exitValue();
-          if (exitCode == 0) {
-            logger.info("  Deployment finished successfully");
-            return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, "Deployed successfully");
-          } else {
-            logger.error("  Deployment failed with exit code: " + String.valueOf(exitCode));
-            return new DeploymentAPIReturn(
-                APIReturnStatus.STATUS_ERROR,
-                "Deployment failed with exit code: " + String.valueOf(exitCode));
-          }
-        } catch (InterruptedException e) {
-          logger.error("  Deployment timed out. Message: " + e.getMessage());
-          return new DeploymentAPIReturn(APIReturnStatus.STATUS_ERROR, "Deployment timed out");
-        }
-      } catch (IOException e) {
-        logger.error("  Deployment could not be started. Message: " + e.getMessage());
-        return new DeploymentAPIReturn(APIReturnStatus.STATUS_FAIL, "Could not start deployment");
-      } finally {
-        if (provisioningProcess != null && provisioningProcess.isAlive())
-          provisioningProcess.destroy();
-        singleProvisioningLock.unlock();
-      }
-    } else {
-      logger.error("  Another deployment is in progress");
+      runner =
+          new DeploymentRunner(
+              deployment,
+              () -> {
+                singleProvisioningLock.release();
+              });
+      runner.start();
       return new DeploymentAPIReturn(
-          APIReturnStatus.STATUS_ERROR, "Another deployment is in progress");
+          APIReturnStatus.STATUS_SUCCESS, "Deployment Started Successfully");
+    } catch (DeploymentException ex) {
+      return new DeploymentAPIReturn(APIReturnStatus.STATUS_ERROR, ex.getMessage());
+    } finally {
+      logger.info("  Deployment request finalized");
     }
   }
 
   @GetMapping(path = "/v1/deployment", produces = "application/json")
   public DeploymentAPIReturn deploymentStatus() {
+    logger.info("Received status command");
+
+    if (runner == null
+        || runner.getDeploymentState() == DeploymentRunner.DeploymentState.STATE_NOT_STARTED) {
+      logger.info("  No deployment is running");
+      return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, NO_UPDATES_MESSAGE);
+    }
+
+    DeploymentRunner.DeploymentState state = runner.getDeploymentState();
+
     ObjectMapper mapper = new ObjectMapper();
     ObjectNode rootNode = mapper.createObjectNode();
-    rootNode.put("state", deploymentState.getState());
+    rootNode.put("state", state.toString());
+    if (state == DeploymentRunner.DeploymentState.STATE_FINISHED)
+      rootNode.put("exitValue", runner.getExitValue());
 
-    logger.info("Received status command");
-    if (deploymentState == DeploymentState.STATE_NOT_STARTED) {
-      logger.info("  No deployment is running");
-      return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, NO_UPDATES_MESSAGE, rootNode);
-    }
-
-    if (provisioningProcess == null) {
-      String stateString =
-          "Deployment process unavailable, with status: " + deploymentState.getState();
-      deploymentState = DeploymentState.STATE_NOT_STARTED;
-      logger.warn("  " + stateString);
-      return new DeploymentAPIReturn(APIReturnStatus.STATUS_ERROR, stateString);
-    }
-
-    synchronized (singleUpdateMutex) {
-      StringBuilder sb = new StringBuilder();
-      BufferedReader stdout =
-          new BufferedReader(new InputStreamReader(provisioningProcess.getInputStream()));
-
-      try {
-        String s;
-        while (stdout.ready() && (s = stdout.readLine()) != null) {
-          sb.append(s);
-          sb.append('\n');
-        }
-      } catch (IOException e) {
-        logger.debug("  Problem reading deployment process logs: " + e.getMessage());
-      }
-      logger.trace("  Read " + sb.length() + " chars from deployment process logs");
-
-      if (deploymentState == DeploymentState.STATE_RUNNING) {
-        String message = sb.length() == 0 ? NO_UPDATES_MESSAGE : sb.toString();
-        return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, message, rootNode);
-      } else {
-        logger.info("  Deployment finished");
-
-        provisioningProcess = null;
-
-        deploymentState = DeploymentState.STATE_FINISHED;
-        rootNode.put("state", deploymentState.getState());
-
-        return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, sb.toString(), rootNode);
-      }
-    }
+    return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, runner.getOutput(), rootNode);
   }
 }
