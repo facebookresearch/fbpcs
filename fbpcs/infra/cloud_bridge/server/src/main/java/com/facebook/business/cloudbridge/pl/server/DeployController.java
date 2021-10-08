a3ea05535de29876f39dd908c3a4a612de4c8165
Lucas Tavares <lucaskt@fb.com>
2021-10-07 17:57:13 -0700

    Separate API Return structures from Controller for readability
    Differential Revision: D30484888
    
    fbshipit-source-id: 0616a0cb6270988eee5c1c307bbdffca3ea400a3

--- fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeployController.java
+++ fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeployController.java
@@ -7,9 +7,6 @@
 
 package com.facebook.business.cloudbridge.pl.server;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.concurrent.Semaphore;
@@ -29,54 +26,17 @@ public class DeployController {
   private DeploymentRunner runner;
   private Semaphore singleProvisioningLock = new Semaphore(1);
 
-  enum APIReturnStatus {
-    STATUS_SUCCESS("success"),
-    STATUS_FAIL("fail"),
-    STATUS_ERROR("error");
-
-    private String status;
-
-    APIReturnStatus(String status) {
-      this.status = status;
-    }
-
-    @JsonValue
-    public String getStatus() {
-      return status;
-    }
-  }
-
-  class DeploymentAPIReturn {
-    public APIReturnStatus status;
-    public String message;
-
-    @JsonInclude(Include.NON_NULL)
-    public Object data;
-
-    public DeploymentAPIReturn(APIReturnStatus status, String message) {
-      this.status = status;
-      this.message = message;
-      this.data = null;
-    }
-
-    public DeploymentAPIReturn(APIReturnStatus status, String message, Object data) {
-      this.status = status;
-      this.message = message;
-      this.data = data;
-    }
-  }
-
   @PostMapping(
       path = "/v1/deployment",
       consumes = "application/json",
       produces = "application/json")
-  public DeploymentAPIReturn deploymentCreate(@RequestBody DeploymentParams deployment) {
+  public APIReturn deploymentCreate(@RequestBody DeploymentParams deployment) {
     logger.info("Received deployment request: " + deployment.toString());
 
     try {
       deployment.validate();
     } catch (InvalidDeploymentArgumentException ex) {
-      return new DeploymentAPIReturn(APIReturnStatus.STATUS_FAIL, ex.getMessage());
+      return new APIReturn(APIReturn.Status.STATUS_FAIL, ex.getMessage());
     }
     logger.info("  Validated input");
 
@@ -84,7 +44,7 @@ public class DeployController {
       if (!singleProvisioningLock.tryAcquire()) {
         String errorMessage = "Another deployment is in progress";
         logger.error("  " + errorMessage);
-        return new DeploymentAPIReturn(APIReturnStatus.STATUS_FAIL, errorMessage);
+        return new APIReturn(APIReturn.Status.STATUS_FAIL, errorMessage);
       }
       logger.info("  No deployment conflicts found");
 
@@ -95,23 +55,22 @@ public class DeployController {
                 singleProvisioningLock.release();
               });
       runner.start();
-      return new DeploymentAPIReturn(
-          APIReturnStatus.STATUS_SUCCESS, "Deployment Started Successfully");
+      return new APIReturn(APIReturn.Status.STATUS_SUCCESS, "Deployment Started Successfully");
     } catch (DeploymentException ex) {
-      return new DeploymentAPIReturn(APIReturnStatus.STATUS_ERROR, ex.getMessage());
+      return new APIReturn(APIReturn.Status.STATUS_ERROR, ex.getMessage());
     } finally {
       logger.info("  Deployment request finalized");
     }
   }
 
   @GetMapping(path = "/v1/deployment", produces = "application/json")
-  public DeploymentAPIReturn deploymentStatus() {
+  public APIReturn deploymentStatus() {
     logger.info("Received status command");
 
     if (runner == null
         || runner.getDeploymentState() == DeploymentRunner.DeploymentState.STATE_NOT_STARTED) {
       logger.info("  No deployment is running");
-      return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, NO_UPDATES_MESSAGE);
+      return new APIReturn(APIReturn.Status.STATUS_SUCCESS, NO_UPDATES_MESSAGE);
     }
 
     DeploymentRunner.DeploymentState state = runner.getDeploymentState();
@@ -122,6 +81,6 @@ public class DeployController {
     if (state == DeploymentRunner.DeploymentState.STATE_FINISHED)
       rootNode.put("exitValue", runner.getExitValue());
 
-    return new DeploymentAPIReturn(APIReturnStatus.STATUS_SUCCESS, runner.getOutput(), rootNode);
+    return new APIReturn(APIReturn.Status.STATUS_SUCCESS, runner.getOutput(), rootNode);
   }
 }
