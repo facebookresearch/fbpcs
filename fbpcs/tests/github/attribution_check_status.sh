3fd84cec0e540cadd30826203ab07b89f78013d6
Yutong Wang <yutongwang@fb.com>
2021-10-13 12:52:44 -0700

    E2E test: Adding scripts to run on github actions [3/n]
    Summary:
    config.sh: config files for both PL and PA
    start_container.sh: this is to start containers (both PL and PA)
    validate_result.sh: to validate if results match
    
    attribution_check_status.sh and attribution_run_stages.sh are for attribution specially.
    
    Lift checks status and run stages scripts will be added later
    
    Differential Revision: D31590094
    
    fbshipit-source-id: 292af6e4f32cca1dae93f5da391d48d451060132

---
+++ fbpcs/tests/github/attribution_check_status.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Usage: Check if all status are complete for attribution stages
+set -e
+container_name=$1
+
+# shellcheck source=fbcode/fbpcs/tests/github/config.sh
+source "$(dirname "${BASH_SOURCE[0]}")/config.sh"
+
+docker_command="docker exec $container_name $ATTRIBUTION_COORDINATOR"
+
+# check if all the status are COMPLETED, if yes, return true
+function check_status_complete() {
+    $docker_command get_instance "$1" --config="$DOCKER_CLOUD_CONFIG_FILE" || exit 1
+    #filter out "status": "COMPLETED"
+    non_complete_status=$($docker_command get_instance "$1" \
+        --config="$DOCKER_CLOUD_CONFIG_FILE" 2>&1 | \
+        grep -o -E "\"status\": \"[a-zA-Z]+\"" | \
+        awk -F: '$2 != " \"COMPLETED\""')
+    if [ -z "$non_complete_status" ]
+    then
+        echo "$1 status is all complete"
+        return 0
+    else
+        echo "$1 has following status: $non_complete_status"
+        return 1
+    fi
+}
+
+until check_status_complete $"$ATTRIBUTION_PUBLIHSER_NAME" \
+&& check_status_complete $"$ATTRIBUTION_PARTNER_NAME"
+    do echo "Waiting status to complete ..."
+    sleep 60
+done
+
+echo "Stage complets successfully"
