e8a511379903745875eb38bd3b0b5fdeedfc5f64
Lucas Tavares <lucaskt@fb.com>
2021-10-27 09:31:07 -0700

    Fix issue with status command not properly returning end states (#259)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/259
    
    There was an issue where once a process stopped running its final logs could be discarded by the cleanup work.
    It was caused if there were no calls to get the logs just before the process finished.
    
    Reviewed By: marksliva
    
    Differential Revision: D31957781
    
    fbshipit-source-id: a1aa58f2dadf80d034b13b0a0420aeb95cbd165c

--- fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeploymentRunner.java
+++ fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeploymentRunner.java
@@ -41,9 +41,14 @@ public class DeploymentRunner extends Thread {
     synchronized (processOutputMutex) {
       output = processOutput;
       processOutput = new String();
-    }
 
-    if (provisioningProcess == null) deploymentState = DeploymentState.STATE_NOT_STARTED;
+      if (provisioningProcess == null) {
+        deploymentState = DeploymentState.STATE_NOT_STARTED;
+      } else if (!provisioningProcess.isAlive()
+          && deploymentState != DeploymentState.STATE_FINISHED) {
+        finish();
+      }
+    }
 
     return output;
   }
@@ -148,6 +153,12 @@ public class DeploymentRunner extends Thread {
     }
   }
 
+  private void finish() {
+    deploymentState = DeploymentState.STATE_FINISHED;
+    provisioningProcess = null;
+    deploymentFinishedCallback.run();
+  }
+
   public void run() {
     deploymentState = DeploymentState.STATE_RUNNING;
 
@@ -165,7 +176,7 @@ public class DeploymentRunner extends Thread {
       BufferedReader stdout =
           new BufferedReader(new InputStreamReader(provisioningProcess.getInputStream()));
 
-      while (provisioningProcess.isAlive()) {
+      while (provisioningProcess != null && provisioningProcess.isAlive()) {
         logOutput(readOutput(stdout));
 
         try {
@@ -173,7 +184,9 @@ public class DeploymentRunner extends Thread {
         } catch (InterruptedException e) {
         }
       }
-      logOutput(readOutput(stdout));
+      if (provisioningProcess != null) {
+        logOutput(readOutput(stdout));
+      }
 
       exitValue = provisioningProcess.exitValue();
       logger.info("  Deployment process exited with value: " + exitValue);
@@ -182,14 +195,15 @@ public class DeploymentRunner extends Thread {
       logger.error("  Deployment could not be started. Message: " + e.getMessage());
       throw new DeploymentException("Deployment could not be started");
     } finally {
-      deploymentState = DeploymentState.STATE_FINISHED;
-      if (provisioningProcess != null && provisioningProcess.isAlive()) {
-        provisioningProcess.destroy();
+      synchronized (processOutputMutex) {
+        if (provisioningProcess != null && provisioningProcess.isAlive()) {
+          provisioningProcess.destroy();
+        }
+        if (processOutput.isEmpty()) {
+          finish();
+        }
       }
-      provisioningProcess = null;
       logger.info("  Deployment finished");
-
-      deploymentFinishedCallback.run();
     }
   }
 }
