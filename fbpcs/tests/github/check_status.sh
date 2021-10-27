21ae5bc4bfbc6a0b4616e6ba855b7e67da08a3c2
Yutong Wang <yutongwang@fb.com>
2021-10-27 10:03:57 -0700

    PL/PA e2e tests update on github actions (#254)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/254
    
    What:
    Since PL/PA Coordinator is being deprecated, I am migrating pl/pa e2e tests to private_computation_cli and update each step name to match with new cli.  Since there is "run_next" being introduced, I am using that to call different stages.
    
    I also merge attribution_check_status.sh and lift_check_status.sh to be one script: check_status.sh
    
    Next:
    Running the workflows for a few times to catch edge cases; if all good, merge them with original image creation workflow
    
    Reviewed By: corbantek, peking2
    
    Differential Revision: D31943726
    
    fbshipit-source-id: be0b78f5749db2181b199382bd22acaa5d520d0d

--- fbpcs/tests/github/check_status.sh
+++ fbpcs/tests/github/check_status.sh
@@ -4,20 +4,33 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# Usage: Check if all status are complete for attribution stages
+# Usage: Check if all status are complete for lift|attribution stages: id_match, compute and aggregate_shards
 set -e
+
 container_name=$1
+game=$2
+
+if [ "$game" == 'lift' ]||[ "$game" == 'attribution' ]
+then
+    upper_game=${game^^}
+    publisher_name=${upper_game}_PUBLIHSER_NAME
+    partner_name=${upper_game}_PARTNER_NAME
+else
+    echo "Invalid Game"
+    exit 1
+fi
+
 
 # shellcheck disable=SC1091,SC1090
 source "$(dirname "${BASH_SOURCE[0]}")/config.sh" || exit 1
 
-docker_command="docker exec $container_name $ATTRIBUTION_COORDINATOR"
+docker_command="docker exec $container_name $COORDINATOR"
 
 # check if all the status are COMPLETED, if yes, return true
 function check_status_complete() {
     $docker_command get_instance "$1" --config="$DOCKER_CLOUD_CONFIG_FILE" || exit 1
     #filter out "status": "COMPLETED"
-    non_complete_status=$($docker_command get_instance "$1" \
+    non_complete_status=$($docker_command get "$1" \
         --config="$DOCKER_CLOUD_CONFIG_FILE" 2>&1 | \
         grep -o -E "\"status\": \"[a-zA-Z]+\"" | \
         awk -F: '$2 != " \"COMPLETED\""')
@@ -30,11 +43,10 @@ function check_status_complete() {
         return 1
     fi
 }
-
-until check_status_complete $"$ATTRIBUTION_PUBLIHSER_NAME" \
-&& check_status_complete $"$ATTRIBUTION_PARTNER_NAME"
+# timeout is added in github workflows
+until check_status_complete "${!publisher_name}" && check_status_complete "${!partner_name}"
     do echo "Waiting status to complete ..."
     sleep 60
 done
 
-echo "Stage complets successfully"
+echo "Stage completes successfully"
