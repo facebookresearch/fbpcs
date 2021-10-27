025e52ecdbb121ec2b90ea7a639ae9543f21b1d8
Zhenhuan Zhang <anthonyzhang@fb.com>
2021-10-27 13:39:27 -0700

    user_data field update in semi-automated data ingestion pipeline (#265)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/265
    
    **Context**
    PL installation improvement, E2E test session: https://fb.workplace.com/groups/164332244998024/permalink/742724257158817/
    
    **What**
    We identified the user_data field is outdated given the updates in the `data_transformation_lambda.py` in D31939077 (https://github.com/facebookresearch/fbpcs/commit/93cba22f5caea08b335dc63f5c9f602748290379). We update the user_data field to match the `user_data` field in data_transformation_lambda.py
    
    **Next**
    
    Reviewed By: marksliva
    
    Differential Revision: D31950736
    
    fbshipit-source-id: 4bcb8e7af32d8c85f1b130301bc1dca825af2903

--- fbpcs/infra/cloud_bridge/semi_automated_data_ingestion/glue_ETL.py
+++ fbpcs/infra/cloud_bridge/semi_automated_data_ingestion/glue_ETL.py
@@ -8,40 +8,49 @@
 ### IMPORT LIBRARIES AND SET VARIABLES
 #########################################
 
-#Import python modules
-from datetime import datetime
+# Import python modules
 import sys
+from datetime import datetime
 
-#Import pyspark modules
-from pyspark.context import SparkContext
-from pyspark.sql.functions import col,year,month,dayofmonth,hour,to_date,lit,from_unixtime
-from pyspark.sql.types import IntegerType
-
-#Import glue modules
-from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
+from awsglue.transforms import DropNullFields
+
+# Import glue modules
+from awsglue.utils import getResolvedOptions
+
+# Import pyspark modules
+from pyspark.context import SparkContext
+from pyspark.sql.functions import (
+    col,
+    year,
+    month,
+    dayofmonth,
+    hour,
+    to_date,
+    lit,
+    from_unixtime,
+    struct,
+)
+from pyspark.sql.types import IntegerType
 
 
-#Initialize contexts and session
+# Initialize contexts and session
 spark_context = SparkContext.getOrCreate()
 glue_context = GlueContext(spark_context)
 session = glue_context.spark_session
 
-args = getResolvedOptions(sys.argv,
-                          ['JOB_NAME',
-                           's3_read_path',
-                           's3_write_path'])
+args = getResolvedOptions(sys.argv, ["JOB_NAME", "s3_read_path", "s3_write_path"])
 
-#Parameters
+# Parameters
 
-s3_options = {"paths": ["s3://" + args['s3_read_path']]}
-s3_write_path = "s3://" + args['s3_write_path']
+s3_options = {"paths": ["s3://" + args["s3_read_path"]]}
+s3_write_path = "s3://" + args["s3_write_path"]
 #########################################
 ### EXTRACT (READ DATA)
 #########################################
 
-#Log starting time
+# Log starting time
 dt_start = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("Start time:", dt_start)
 
@@ -56,12 +65,33 @@ dynamic_frame_read = glue_context.create_dynamic_frame.from_options(
 # #Convert dynamic frame to data frame to use standard pyspark functions
 data_frame = dynamic_frame_read.toDF()
 
-### debug
-data_frame.show(10)
 
 #########################################
 ### TRANSFORM (MODIFY DATA)
 #########################################
+### first, check column existence, if not, add dummy identifier columns
+listColumns = data_frame.columns
+expected_column_list = [
+    "email",
+    "phone",
+    "device_id",
+    "client_ip_address",
+    "client_user_agent",
+    "click_id",
+    "login_id",
+    "browser_name",
+    "device_os",
+    "device_os_version",
+    "data_source_id",
+    "timestamp",
+    "currency_type",
+    "conversion_value",
+    "event_type",
+]
+for column_name in expected_column_list:
+    if column_name not in listColumns:
+        data_frame = data_frame.withColumn(column_name, lit(None))
+
 
 # create columns
 augmented_df = (
@@ -72,39 +102,62 @@ augmented_df = (
     .withColumn("month", month(col("date_col")))
     .withColumn("day", dayofmonth(col("date_col")))
     .withColumn("hour", hour(col("date_col")))
+    .withColumn(
+        "user_data",
+        struct(
+            "email",
+            "phone",
+            "device_id",
+            "client_ip_address",
+            "client_user_agent",
+            "click_id",
+            "login_id",
+            "browser_name",
+            "device_os",
+            "device_os_version",
+        ),
+    )
+    .drop(col("email"))
+    .drop(col("phone"))
+    .drop(col("device_id"))
+    .drop(col("client_ip_address"))
+    .drop(col("client_user_agent"))
+    .drop(col("click_id"))
+    .drop(col("login_id"))
+    .drop(col("browser_name"))
+    .drop(col("device_os"))
+    .drop(col("device_os_version"))
     .drop(col("date_col"))
     .drop(col("unixtime"))
     .repartition(1)
 )
 
-#Print result table
-#Note: Show function is an action. Actions force the execution of the data frame plan.
-#With big data the slowdown would be significant without cacching.
 final_df = augmented_df
-final_df.show(10)
+
 
 #########################################
 ### LOAD (WRITE DATA)
 #########################################
 
-#Create just 1 partition, because there is so little data
+# Create just 1 partition, because there is so little data
 final_df = final_df.repartition(1)
 
-#Convert back to dynamic frame
+# Convert back to dynamic frame
 dynamic_frame_write = DynamicFrame.fromDF(final_df, glue_context, "dynamic_frame_write")
-
-#Write data back to S3
+# Drop columns with all NULL values
+dynamic_frame_write = DropNullFields.apply(frame=dynamic_frame_write)
+# Write data back to S3
 glue_context.write_dynamic_frame.from_options(
-frame = dynamic_frame_write,
-connection_type = "s3",
-connection_options = {
-"path": s3_write_path,
-#Here you could create S3 prefixes according to a values in specified columns
-"partitionKeys": ["year", "month", "day", "hour"]
-},
-format = "json"
+    frame=dynamic_frame_write,
+    connection_type="s3",
+    connection_options={
+        "path": s3_write_path,
+        # Here you could create S3 prefixes according to a values in specified columns
+        "partitionKeys": ["year", "month", "day", "hour"],
+    },
+    format="json",
 )
 
-#Log end time
+# Log end time
 dt_end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("End time:", dt_end)
