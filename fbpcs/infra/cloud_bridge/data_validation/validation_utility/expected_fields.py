94438b364f2b6aa6dd51acea7c6b215a155f0e3c
Mark Sliva <marksliva@fb.com>
2021-11-05 13:55:29 -0700

    add support for PA and PL data validation
    Differential Revision: D32198896
    
    fbshipit-source-id: da96f3eac8274d70011d561a343a2643805abfeb

--- fbpcs/infra/cloud_bridge/data_validation/validation_utility/expected_fields.py
+++ fbpcs/infra/cloud_bridge/data_validation/validation_utility/expected_fields.py
@@ -6,15 +6,15 @@
 from typing import Dict, Set
 import re
 
-ALL_REQUIRED_FIELDS: Set[str] = {
+UNFILTERED_ALL_REQUIRED_FIELDS: Set[str] = {
     'action_source',
     'conversion_value',
     'currency_type',
     'event_type',
     'timestamp',
 }
-ONE_OR_MORE_REQUIRED_FIELDS: Set[str] = {'email','device_id'}
-FORMAT_VALIDATION_FOR_FIELD: Dict[str, re.Pattern] = {
+UNFILTERED_ONE_OR_MORE_REQUIRED_FIELDS: Set[str] = {'email','device_id'}
+UNFILTERED_FORMAT_VALIDATION_FOR_FIELD: Dict[str, re.Pattern] = {
     'email': re.compile(r"^[a-f0-9]{64}$"),
     'device_id': re.compile(r"^([a-f0-9]{32}|[a-f0-9-]{36})$"),
     'timestamp': re.compile(r"^[0-9]+$"),
@@ -22,4 +22,30 @@ FORMAT_VALIDATION_FOR_FIELD: Dict[str, re.Pattern] = {
     'conversion_value': re.compile(r"^[0-9]+$"),
     'action_source': re.compile(r"^(email|website|phone_call|chat|physical_store|system_generated|other)$"),
     'event_type': re.compile(r"^.+$"),
+}
+
+PA_ALL_REQUIRED_FIELDS: Set[str] = {
+    'id_',
+    'conversion_timestamp',
+    'conversion_value',
+    'conversion_metadata'
+}
+
+PA_FORMAT_VALIDATION_FOR_FIELD: Dict[str, re.Pattern] = {
+    'id_': re.compile(r"^.+$"),
+    'conversion_timestamp': re.compile(r"^.+$"),
+    'conversion_value': re.compile(r"^.+$"),
+    'conversion_metadata': re.compile(r"^.+$"),
+}
+
+PL_ALL_REQUIRED_FIELDS: Set[str] = {
+    'id_',
+    'event_timestamp',
+    'value',
+}
+
+PL_FORMAT_VALIDATION_FOR_FIELD: Dict[str, re.Pattern] = {
+    'id_': re.compile(r"^.+$"),
+    'event_timestamp': re.compile(r"^.+$"),
+    'value': re.compile(r"^.+$"),
 }
