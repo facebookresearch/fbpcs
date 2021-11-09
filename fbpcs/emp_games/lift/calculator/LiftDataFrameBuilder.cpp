b9075a76ec8d910a1611900d07904f7fd21f6f17
Logan Gore <gorel@fb.com>
2021-11-08 19:13:51 -0800

    Add LiftDataFrameBuilder utility class (#284)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/284
    
    # What
    * A class to build a Lift-specific dataframe given filePath and conversion cap
    # Why
    * In order to make the code more composable, I'm introducing a layer in between DataFrame and InputData in order to make the logic more testable
    * How is this more testable? We can expose a class with public methods clearly modifying a DataFrame to test what's actually happening
    * This *should* prevent bugs like what we saw in S249300
    
    Reviewed By: Olivia-liu
    
    Differential Revision: D32050989
    
    fbshipit-source-id: 0c7d73fb0bbf5915065be4dd49923fb27552ebcb

---
+++ fbpcs/emp_games/lift/calculator/LiftDataFrameBuilder.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "fbpcs/emp_games/lift/calculator/LiftDataFrameBuilder.h"
+
+#include "fbpcs/emp_games/lift/common/Column.h"
+#include "fbpcs/emp_games/lift/common/DataFrame.h"
+
+using namespace private_lift;
+
+void LiftDataFrameBuilder::addTestControlPopulationColumns(
+    df::DataFrame &df) const {
+  // Will implement in next diff
+}
+
+void LiftDataFrameBuilder::applyConversionCap(df::DataFrame &df) const {
+  // Will implement in next diff
+}
+
+void LiftDataFrameBuilder::precomputeValuesSquared(df::DataFrame &df) const {
+  // Will implement in next diff
+}
+
+void LiftDataFrameBuilder::dropUnnecessaryColumns(df::DataFrame &df) const {
+  // Will implement in next diff
+}
