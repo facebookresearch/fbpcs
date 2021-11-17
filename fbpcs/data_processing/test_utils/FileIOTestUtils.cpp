ec93508e13513f2515a3655ce58952100f9c07f0
Logan Gore <gorel@fb.com>
2021-11-16 16:58:33 -0800

    Add FileIOTestUtils::expectFileRowsEqual function for testing
    Differential Revision: D32416619
    
    fbshipit-source-id: 03eb008d110b26243a10156df8ac61641f706adc

--- fbpcs/data_processing/test_utils/FileIOTestUtils.cpp
+++ fbpcs/data_processing/test_utils/FileIOTestUtils.cpp
@@ -13,11 +13,26 @@
 #include <string>
 #include <vector>
 
+#include <gtest/gtest.h>
+
 namespace data_processing::test_utils {
 void writeVecToFile(const std::vector<std::string> &rows,
                     const std::string &filePath) {
   std::ofstream oFile{filePath};
   std::ostream_iterator<std::string> outputIterator{oFile, "\n"};
   std::copy(rows.begin(), rows.end(), outputIterator);
+}
+
+void expectFileRowsEqual(const std::string &filePath,
+                         std::vector<std::string> &rows) {
+  std::ifstream iFile{filePath};
+  std::string line;
+  std::size_t idx = 0;
+  while (std::getline(iFile, line)) {
+    EXPECT_EQ(line, rows.at(idx));
+    ++idx;
+  }
+  // Ensure the file wasn't too *short*
+  EXPECT_EQ(idx, rows.size());
 }
 } // namespace data_processing::test_utils
