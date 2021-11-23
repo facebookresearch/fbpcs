cfae8bd1f71d82870ad73230112d84b236a5c97f
Logan Gore <gorel@fb.com>
2021-11-23 10:52:42 -0800

    Define RoundRobinBasedSharder
    Differential Revision: D32550062
    
    fbshipit-source-id: 64ef981941b6332e72b23ceee42d5a89fc9caeb6

---
+++ fbpcs/data_processing/sharding/test/RoundRobinBasedSharderTest.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "fbpcs/data_processing/sharding/RoundRobinBasedSharder.h"
+
+namespace data_processing::sharder {
+TEST(RoundRobinBasedSharderTest, TestGetShardFor) {
+  RoundRobinBasedSharder sharder{"unused", {/* unused */}, 123};
+  EXPECT_EQ(0, sharder.getShardFor("foo", 2));
+  EXPECT_EQ(1, sharder.getShardFor("bar", 2));
+  EXPECT_EQ(0, sharder.getShardFor("baz", 2));
+  EXPECT_EQ(1, sharder.getShardFor("quux", 2));
+}
+} // namespace data_processing::sharder
