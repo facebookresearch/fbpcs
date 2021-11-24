ad1fadcc3ea2f7d1f624a80c1ce434aea9269261
Logan Gore <gorel@fb.com>
2021-11-23 21:00:33 -0800

    Add simple PC-CLI tests (#392)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/392
    
    # What
    * Skeleton code to start adding tests
    # Why
    * Tests are good
    
    Reviewed By: Olivia-liu
    
    Differential Revision: D32611463
    
    fbshipit-source-id: b600343306155b6ef04727766c3ec61fcbf0e493

---
+++ fbpcs/private_computation_cli/tests/test_private_computation_cli.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from unittest import TestCase
+
+
+class TestPrivateComputationCli(TestCase):
+    def test_create_instance(self):
+        pass
+
+    def test_id_match(self):
+        pass
+
+    def test_prepare_compute_input(self):
+        pass
+
+    def test_compute_metrics(self):
+        pass
+
+    def test_aggregate_shards(self):
+        pass
+
+    def test_validate(self):
+        pass
+
+    def test_run_post_processing_handlers(self):
+        pass
+
+    def test_run_next(self):
+        pass
+
+    def test_run_stage(self):
+        pass
+
+    def test_get_instance(self):
+        pass
+
+    def test_get_server_ips(self):
+        pass
+
+    def test_get_pid(self):
+        pass
+
+    def test_get_mpc(self):
+        pass
+
+    def test_run_instance(self):
+        pass
+
+    def test_run_instances(self):
+        pass
+
+    def test_run_study(self):
+        pass
+
+    def test_cancel_current_stage(self):
+        pass
+
+    def test_print_instance(self):
+        pass
