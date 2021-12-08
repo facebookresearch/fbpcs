5f1cb75b2e832dc647298a98e41d6f8fcd58b429
Logan Gore <gorel@fb.com>
2021-12-08 12:03:56 -0800

    Add test for shard, refactor code to be more testable
    Differential Revision: D32940987
    
    fbshipit-source-id: 57f76aefe85d62e718a5fb66a26b56e4f633da30

--- fbpcs/data_processing/sharding/shard_pid.cpp
+++ fbpcs/data_processing/sharding/shard_pid.cpp
@@ -10,39 +10,28 @@
 #include <fbpcf/aws/AwsSdk.h>
 #include <folly/init/Init.h>
 
-#include "fbpcs/data_processing/sharding/ShardPid.h"
+#include "fbpcs/data_processing/sharding/Sharding.h"
 
 DEFINE_string(input_filename, "", "Name of the input file");
-DEFINE_string(
-    output_filenames,
-    "",
-    "Comma-separated list of file paths for output");
-DEFINE_string(
-    output_base_path,
-    "",
-    "Local or s3 base path where output files are written to");
-DEFINE_int32(
-    file_start_index,
-    0,
-    "First file that will be created from base path");
+DEFINE_string(output_filenames, "",
+              "Comma-separated list of file paths for output");
+DEFINE_string(output_base_path, "",
+              "Local or s3 base path where output files are written to");
+DEFINE_int32(file_start_index, 0,
+             "First file that will be created from base path");
 DEFINE_int32(num_output_files, 0, "Number of files that should be created");
 DEFINE_int32(log_every_n, 1000000, "How frequently to log updates");
-DEFINE_string(
-    hmac_base64_key,
-    "",
-    "key to be used in optional hash salting step");
+DEFINE_string(hmac_base64_key, "",
+              "key to be used in optional hash salting step");
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   folly::init(&argc, &argv);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   fbpcf::AwsSdk::aquire();
 
-  data_processing::sharder::runShardPid(FLAGS_input_filename,
-      FLAGS_output_filenames,
-      FLAGS_output_base_path,
-      FLAGS_file_start_index,
-      FLAGS_num_output_files,
-      FLAGS_log_every_n,
+  data_processing::sharder::runShardPid(
+      FLAGS_input_filename, FLAGS_output_filenames, FLAGS_output_base_path,
+      FLAGS_file_start_index, FLAGS_num_output_files, FLAGS_log_every_n,
       FLAGS_hmac_base64_key);
   return 0;
 }
