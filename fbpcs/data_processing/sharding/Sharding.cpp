c0cbcd2d1d5963c5cca6084e087bfa489dda9c64
Logan Gore <gorel@fb.com>
2021-12-08 12:03:41 -0800

    Add test for shard, refactor code to be more testable
    Differential Revision: D32940987
    
    fbshipit-source-id: 1253367114cf862ca0410bd1c1cf4896caa1211e

---
+++ fbpcs/data_processing/sharding/Sharding.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "fbpcs/data_processing/sharding/Sharding.h"
+
+#include <folly/String.h>
+#include <folly/logging/xlog.h>
+
+#include "fbpcs/data_processing/sharding/HashBasedSharder.h"
+#include "fbpcs/data_processing/sharding/RoundRobinBasedSharder.h"
+
+#include "fbpcs/data_processing/common/FilepathHelpers.h"
+#include "fbpcs/data_processing/common/Logging.h"
+#include "fbpcs/data_processing/common/S3CopyFromLocalUtil.h"
+namespace data_processing::sharder {
+void runShard(const std::string &inputFilename,
+              const std::string &outputFilenames,
+              const std::string &outputBasePath, int32_t fileStartIndex,
+              int32_t numOutputFiles, int32_t logEveryN) {
+  if (!outputFilenames.empty()) {
+    std::vector<std::string> outputFilepaths;
+    folly::split(',', outputFilenames, outputFilepaths);
+    RoundRobinBasedSharder sharder{inputFilename, outputFilepaths, logEveryN};
+    sharder.shard();
+  } else if (!outputBasePath.empty() && numOutputFiles > 0) {
+    std::size_t startIndex = static_cast<std::size_t>(fileStartIndex);
+    std::size_t endIndex = startIndex + numOutputFiles;
+    RoundRobinBasedSharder sharder{inputFilename, outputBasePath, startIndex,
+                                   endIndex, logEveryN};
+    sharder.shard();
+  } else {
+    XLOG(FATAL) << "Error: specify --output_filenames or --output_base_path, "
+                   "--file_start_index, and --num_output_files";
+  }
+}
+
+void runShardPid(const std::string &inputFilename,
+                 const std::string &outputFilenames,
+                 const std::string &outputBasePath, int32_t fileStartIndex,
+                 int32_t numOutputFiles, int32_t logEveryN,
+                 const std::string &hmacBase64Key) {
+  if (!outputFilenames.empty()) {
+    std::vector<std::string> outputFilepaths;
+    folly::split(',', outputFilenames, outputFilepaths);
+    HashBasedSharder sharder{inputFilename, outputFilepaths, logEveryN,
+                             hmacBase64Key};
+    sharder.shard();
+  } else if (!outputBasePath.empty() && numOutputFiles > 0) {
+    std::size_t startIndex = fileStartIndex;
+    std::size_t endIndex = startIndex + numOutputFiles;
+    HashBasedSharder sharder{inputFilename, outputBasePath, startIndex,
+                             endIndex,      logEveryN,      hmacBase64Key};
+    sharder.shard();
+  } else {
+    XLOG(FATAL) << "Error: specify --output_filenames or --output_base_path, "
+                   "--file_start_index, and --num_output_files";
+  }
+}
+
+} // namespace data_processing::sharder
--- fbpcs/data_processing/sharding/shard.cpp
+++ fbpcs/data_processing/sharding/shard.cpp
@@ -5,19 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <string>
-#include <vector>
-
 #include <gflags/gflags.h>
 
 #include <fbpcf/aws/AwsSdk.h>
 #include <folly/init/Init.h>
-#include <folly/logging/xlog.h>
 
-#include "fbpcs/data_processing/common/FilepathHelpers.h"
-#include "fbpcs/data_processing/common/Logging.h"
-#include "fbpcs/data_processing/common/S3CopyFromLocalUtil.h"
-#include "fbpcs/data_processing/sharding/RoundRobinBasedSharder.h"
+#include "fbpcs/data_processing/sharding/Sharding.h"
 
 DEFINE_string(input_filename, "", "Name of the input file");
 DEFINE_string(output_filenames, "",
@@ -27,40 +20,15 @@ DEFINE_string(output_base_path, "",
 DEFINE_int32(file_start_index, 0,
              "First file that will be created from base path");
 DEFINE_int32(num_output_files, 0, "Number of files that should be created");
-DEFINE_string(
-    tmp_directory, "/tmp/",
-    "Directory where temporary files should be saved before final write");
 DEFINE_int32(log_every_n, 1000000, "How frequently to log updates");
-
-using namespace data_processing::sharder;
 
 int main(int argc, char **argv) {
   folly::init(&argc, &argv);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   fbpcf::AwsSdk::aquire();
 
-  if (!FLAGS_output_filenames.empty()) {
-    std::stringstream ss{FLAGS_output_filenames};
-    std::vector<std::string> outputFilepaths;
-    while (ss.good()) {
-      std::string substr;
-      getline(ss, substr, ',');
-      outputFilepaths.push_back(std::move(substr));
-    }
-    RoundRobinBasedSharder sharder{FLAGS_input_filename, outputFilepaths,
-                                   FLAGS_log_every_n};
-    sharder.shard();
-  } else if (!FLAGS_output_base_path.empty() && FLAGS_num_output_files > 0) {
-    std::size_t startIndex = FLAGS_file_start_index;
-    std::size_t endIndex = startIndex + FLAGS_num_output_files;
-    RoundRobinBasedSharder sharder{FLAGS_input_filename, FLAGS_output_base_path,
-                                   startIndex, endIndex, FLAGS_log_every_n};
-    sharder.shard();
-  } else {
-    XLOG(ERR) << "Error: specify --output_filenames or --output_base_path, "
-                 "--file_start_index, and --num_output_files";
-    std::exit(1);
-  }
-
+  data_processing::sharder::runShardPid(
+      FLAGS_input_filename, FLAGS_output_filenames, FLAGS_output_base_path,
+      FLAGS_file_start_index, FLAGS_num_output_files, FLAGS_log_every_n);
   return 0;
 }
