cfcafd0a46562242e91f62c8f4d807fb71673581
Logan Gore <gorel@fb.com>
2021-12-08 12:03:37 -0800

    Add test for shard, refactor code to be more testable
    Differential Revision: D32940987
    
    fbshipit-source-id: 08342e1df2b733cca81e466f12751f443ce067f0

---
+++ fbpcs/data_processing/sharding/Sharding.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace data_processing::sharder {
+void runShard(const std::string &inputFilename,
+              const std::string &outputFilenames,
+              const std::string &outputBasePath, int32_t fileStartIndex,
+              int32_t numOutputFiles, int32_t logEveryN);
+
+void runShardPid(const std::string &inputFilename,
+                 const std::string &outputFilenames,
+                 const std::string &outputBasePath, int32_t fileStartIndex,
+                 int32_t numOutputFiles, int32_t logEveryN,
+                 const std::string &hmacBase64Key);
+} // namespace data_processing::sharder
