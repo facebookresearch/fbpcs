71a2dea0d75214ef4b0c3b4462dff2b2abd6cb0d
Logan Gore <gorel@fb.com>
2021-12-08 12:03:41 -0800

    Add test for shard_pid, refactor code to be more testable
    Differential Revision: D32936474
    
    fbshipit-source-id: 3c439ee169c64fd05f8200332e50bc3ea4ebe22c

--- fbpcs/data_processing/sharding/shard_pid.cpp
+++ fbpcs/data_processing/sharding/shard_pid.cpp
@@ -5,17 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include <sstream>
-#include <string>
-#include <vector>
-
 #include <gflags/gflags.h>
 
 #include <fbpcf/aws/AwsSdk.h>
 #include <folly/init/Init.h>
-#include <folly/logging/xlog.h>
 
-#include "fbpcs/data_processing/sharding/HashBasedSharder.h"
+#include "fbpcs/data_processing/sharding/ShardPid.h"
 
 DEFINE_string(input_filename, "", "Name of the input file");
 DEFINE_string(
@@ -31,48 +26,23 @@ DEFINE_int32(
     0,
     "First file that will be created from base path");
 DEFINE_int32(num_output_files, 0, "Number of files that should be created");
-DEFINE_string(
-    tmp_directory,
-    "/tmp/",
-    "Directory where temporary files should be saved before final write");
 DEFINE_int32(log_every_n, 1000000, "How frequently to log updates");
 DEFINE_string(
     hmac_base64_key,
     "",
     "key to be used in optional hash salting step");
 
-using namespace data_processing::sharder;
-
 int main(int argc, char** argv) {
   folly::init(&argc, &argv);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   fbpcf::AwsSdk::aquire();
 
-  if (!FLAGS_output_filenames.empty()) {
-    std::stringstream ss{FLAGS_output_filenames};
-    std::vector<std::string> outputFilepaths;
-    while (ss.good()) {
-      std::string substr;
-      getline(ss, substr, ',');
-      outputFilepaths.push_back(std::move(substr));
-    }
-    HashBasedSharder sharder{FLAGS_input_filename, outputFilepaths,
-                             FLAGS_log_every_n, FLAGS_hmac_base64_key};
-    sharder.shard();
-  } else if (!FLAGS_output_base_path.empty() && FLAGS_num_output_files > 0) {
-    XLOG(ERR)
-        << "Error: specify --output_filenames or --output_base_path, --file_start_index, and --num_output_files";
-    std::size_t startIndex = FLAGS_file_start_index;
-    std::size_t endIndex = startIndex + FLAGS_num_output_files;
-    HashBasedSharder sharder{FLAGS_input_filename, FLAGS_output_base_path,
-                             startIndex,           endIndex,
-                             FLAGS_log_every_n,    FLAGS_hmac_base64_key};
-    sharder.shard();
-  } else {
-    XLOG(ERR) << "Error: specify --output_filenames or --output_base_path, "
-                 "--file_start_index, and --num_output_files";
-    std::exit(1);
-  }
-
+  data_processing::sharder::runShardPid(FLAGS_input_filename,
+      FLAGS_output_filenames,
+      FLAGS_output_base_path,
+      FLAGS_file_start_index,
+      FLAGS_num_output_files,
+      FLAGS_log_every_n,
+      FLAGS_hmac_base64_key);
   return 0;
 }
