93c313d00a77eaa1939adf53244aeaf60665b530
Jacob Rodal <jrodal@fb.com>
2021-12-14 16:05:46 -0800

    script to generate instance json (#464)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/464
    
    ## What
    
    * Implement script to serialize instances for test cases
    
    ## Why
    
    * To help stop us from breaking existing serde (or at the very least make us aware it will happen)
    
    Reviewed By: leegross
    
    Differential Revision: D32971626
    
    fbshipit-source-id: ec6e5f71c0fd71870c48d581482051d01e881f73

--- fbpcs/private_computation/test/entity/generate_instance_json.py
+++ fbpcs/private_computation/test/entity/generate_instance_json.py
@@ -6,5 +6,177 @@
 
 # pyre-strict
 
+import os
+import time
+
+from fbpcp.entity.container_instance import ContainerInstance, ContainerInstanceStatus
+from fbpcp.entity.mpc_instance import MPCInstanceStatus, MPCParty
+from fbpcs.common.entity.pcs_mpc_instance import PCSMPCInstance
+from fbpcs.pid.entity.pid_instance import (
+    PIDStageStatus,
+    PIDInstance,
+    PIDInstanceStatus,
+    PIDProtocol,
+    PIDRole,
+)
+from fbpcs.pid.entity.pid_stages import UnionPIDStage
+from fbpcs.post_processing_handler.post_processing_handler import (
+    PostProcessingHandlerStatus,
+)
+from fbpcs.post_processing_handler.post_processing_instance import (
+    PostProcessingInstance,
+    PostProcessingInstanceStatus,
+)
+from fbpcs.private_computation.entity.breakdown_key import BreakdownKey
+from fbpcs.private_computation.entity.pce_config import PCEConfig
+from fbpcs.private_computation.entity.private_computation_instance import (
+    PrivateComputationGameType,
+    PrivateComputationInstance,
+    PrivateComputationRole,
+)
+from fbpcs.private_computation.entity.private_computation_status import (
+    PrivateComputationInstanceStatus,
+)
+
+LIFT_PC_PATH: str = os.path.join(
+    os.path.dirname(__file__),
+    "test_resources",
+    "serialized_instances",
+    "lift_pc_instance.json",
+)
+LIFT_PID_PATH: str = os.path.join(
+    os.path.dirname(__file__),
+    "test_resources",
+    "serialized_instances",
+    "lift_pid_instance.json",
+)
+LIFT_MPC_PATH: str = os.path.join(
+    os.path.dirname(__file__),
+    "test_resources",
+    "serialized_instances",
+    "lift_mpc_instance.json",
+)
+
+
+def gen_dummy_container_instance() -> ContainerInstance:
+    """Creates a dummy container instance to be used in unit tests"""
+
+    return ContainerInstance(
+        instance_id="arn:aws:ecs:us-west-2:000000000000:task/cluster-name/subnet",
+        status=ContainerInstanceStatus.COMPLETED,
+        ip_address="10.0.10.242",
+    )
+
+
+def gen_dummy_pid_instance() -> PIDInstance:
+    """Creates a dummy pid instance to be used in unit tests"""
+
+    return PIDInstance(
+        instance_id="pid_instance_id",
+        protocol=PIDProtocol.UNION_PID,
+        pid_role=PIDRole.PUBLISHER,
+        num_shards=1,
+        input_path="https://bucket.s3.us-west-2.amazonaws.com/lift/partner/partner_e2e_input.csv",
+        output_path="https://bucket.s3.us-west-2.amazonaws.com/lift/partner/partner_instance_1638998680_0_out_dir/pid_stage/out.csv",
+        is_validating=False,
+        synthetic_shard_path=None,
+        status=PIDInstanceStatus.COMPLETED,
+        data_path="",
+        spine_path="",
+        hmac_key="",
+        stages_containers={
+            UnionPIDStage.PUBLISHER_SHARD: [gen_dummy_container_instance()],
+            UnionPIDStage.PUBLISHER_PREPARE: [gen_dummy_container_instance()],
+            UnionPIDStage.PUBLISHER_RUN_PID: [gen_dummy_container_instance()],
+        },
+        stages_status={
+            UnionPIDStage.PUBLISHER_SHARD: PIDStageStatus.COMPLETED,
+            UnionPIDStage.PUBLISHER_PREPARE: PIDStageStatus.COMPLETED,
+            UnionPIDStage.PUBLISHER_RUN_PID: PIDStageStatus.COMPLETED,
+        },
+        current_stage=UnionPIDStage.PUBLISHER_RUN_PID,
+        server_ips=["10.0.10.242"],
+    )
+
+
+def gen_dummy_mpc_instance() -> PCSMPCInstance:
+    """Creates a dummy mpc instance to be used in unit tests"""
+
+    return PCSMPCInstance.create_instance(
+        instance_id="mpc_instance_id",
+        game_name="lift",
+        mpc_party=MPCParty.SERVER,
+        num_workers=1,
+        server_ips=["10.0.10.242"],
+        containers=[gen_dummy_container_instance()],
+        status=MPCInstanceStatus.COMPLETED,
+        game_args=[
+            {
+                "input_base_path": "https://bucket.s3.us-west-2.amazonaws.com/lift/partner/partner_instance_1638998680_0_out_dir/data_processing_stage/out.csv",
+                "output_base_path": "https://bucket.s3.us-west-2.amazonaws.com/lift/partner/partner_instance_1638998680_0_out_dir/compute_stage/out.json",
+                "num_files": 40,
+                "concurrency": 4,
+                "file_start_index": 0,
+            }
+        ],
+    )
+
+
+def gen_dummy_post_processing_instance() -> PostProcessingInstance:
+    """Creates a dummy post processing instance to be used in unit tests"""
+    return PostProcessingInstance(
+        instance_id="post_processing_instance_id",
+        handler_statuses={"handler1": PostProcessingHandlerStatus.COMPLETED},
+        status=PostProcessingInstanceStatus.COMPLETED,
+    )
+
+
+def gen_dummy_pc_instance() -> PrivateComputationInstance:
+    """Creates a dummy private computation instance to be used in unit tests"""
+    return PrivateComputationInstance(
+        instance_id="pc_instance_id",
+        role=PrivateComputationRole.PUBLISHER,
+        instances=[
+            gen_dummy_pid_instance(),
+            gen_dummy_mpc_instance(),
+            gen_dummy_post_processing_instance(),
+        ],
+        status=PrivateComputationInstanceStatus.POST_PROCESSING_HANDLERS_COMPLETED,
+        status_update_ts=int(time.time()),
+        num_files_per_mpc_container=40,
+        game_type=PrivateComputationGameType.LIFT,
+        input_path="https://bucket.s3.us-west-2.amazonaws.com/lift/partner/partner_e2e_input.csv",
+        output_dir="https://bucket.s3.us-west-2.amazonaws.com/lift/partner",
+        num_pid_containers=1,
+        num_mpc_containers=1,
+        attribution_rule=None,
+        aggregation_type=None,
+        partial_container_retry_enabled=False,
+        is_validating=False,
+        synthetic_shard_path=None,
+        retry_counter=0,
+        hmac_key="",
+        concurrency=4,
+        padding_size=25,
+        is_test=False,
+        k_anonymity_threshold=100,
+        fail_fast=False,
+        _stage_flow_cls_name="PrivateComputationStageFlow",
+        breakdown_key=BreakdownKey.get_default_key(),
+        pce_config=PCEConfig(
+            subnets=["subnet"],
+            cluster="onedocker-cluster-name",
+            region="us-west-2",
+            onedocker_task_definition="arn:aws:ecs:us-west-2:000000000000:task/cluster-name/subnet",
+        ),
+    )
+
+
 if __name__ == "__main__":
-    pass
+    for path, instance in zip(
+        (LIFT_PID_PATH, LIFT_MPC_PATH, LIFT_PC_PATH),
+        (gen_dummy_pid_instance(), gen_dummy_mpc_instance(), gen_dummy_pc_instance()),
+    ):
+        json_output = instance.dumps_schema()
+        with open(path, "w") as f:
+            f.write(json_output)
