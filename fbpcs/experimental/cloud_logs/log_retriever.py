1d418aeb75ad283f09919d56b76a89641a10cfeb
Olivia Liu <olivialpx@fb.com>
2021-12-14 17:39:44 -0800

    Move cloud_provider enum
    Differential Revision: D33052890
    
    fbshipit-source-id: 8804b68b8e33bab8b0f7bc69cdaddeec508138d6

--- fbpcs/experimental/cloud_logs/log_retriever.py
+++ fbpcs/experimental/cloud_logs/log_retriever.py
@@ -4,12 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from enum import Enum
-
-
-class CloudProvider(Enum):
-    AWS = 1
-    GCP = 2
+from fbpcs.private_computation.entity.cloud_provider import CloudProvider
 
 
 class LogRetriever:
