213d326c0cc59c5ce8be8fae2e79fbfe3543a827
Jacob Rodal <jrodal@fb.com>
2022-01-12 10:56:31 -0800

    check for TODOs in config.yml parsing (#503)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/503
    
    ## What
    
    * Throw custom exception with suggested remediation when TODOs are present in a needed config.yml entry
    * Create base exception for config yml parsing
    
    ## Why
    
    * To surface issues with broken configs before runs start
    * inspiration exhibit a: https://fb.workplace.com/groups/331044242148818/posts/387529273166981/?comment_id=388897219696853
    
    Reviewed By: gorel
    
    Differential Revision: D33239531
    
    fbshipit-source-id: d5ebc6c3170e8563f37ec0e87ec9f3b40290bc24

--- fbpcs/utils/config_yaml/exceptions.py
+++ fbpcs/utils/config_yaml/exceptions.py
@@ -5,9 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 # pyre-strict
+class ConfigYamlBaseException(Exception):
+    pass
 
 
-class ConfigYamlFieldNotFoundError(KeyError):
+class ConfigYamlFieldNotFoundError(KeyError, ConfigYamlBaseException):
     """Raised when a ConfigYamlDict key access fails"""
 
     def __init__(self, key: str) -> None:
@@ -15,7 +17,7 @@ class ConfigYamlFieldNotFoundError(KeyError):
         super().__init__(msg)
 
 
-class ConfigYamlModuleImportError(ImportError):
+class ConfigYamlModuleImportError(ImportError, ConfigYamlBaseException):
     """Raised when a module path cannot be imported"""
 
     def __init__(self, class_path: str) -> None:
@@ -23,7 +25,7 @@ class ConfigYamlModuleImportError(ImportError):
         super().__init__(msg)
 
 
-class ConfigYamlClassNotFoundError(AttributeError):
+class ConfigYamlClassNotFoundError(AttributeError, ConfigYamlBaseException):
     """Raised when a class is not found at a given module path"""
 
     def __init__(self, class_path: str) -> None:
@@ -31,7 +33,7 @@ class ConfigYamlClassNotFoundError(AttributeError):
         super().__init__(msg)
 
 
-class ConfigYamlWrongClassConfiguredError(AssertionError):
+class ConfigYamlWrongClassConfiguredError(AssertionError, ConfigYamlBaseException):
     """Raised when the type of a class loaded through reflection is different than expected"""
 
     def __init__(self, class_path: str, target_class_name: str) -> None:
@@ -39,9 +41,17 @@ class ConfigYamlWrongClassConfiguredError(AssertionError):
         super().__init__(msg)
 
 
-class ConfigYamlWrongConstructorError(TypeError):
+class ConfigYamlWrongConstructorError(TypeError, ConfigYamlBaseException):
     """Raised when the arguments passed to a constructor are incorrect"""
 
     def __init__(self, class_name: str, cause: str) -> None:
         msg = f"{class_name} (specified in your config.yml) does not have the correct arguments. {cause}. Please make sure that your config is up to date."
+        super().__init__(msg)
+
+
+class ConfigYamlValidationError(ValueError, ConfigYamlBaseException):
+    """Raise when a config.yml fails validation"""
+
+    def __init__(self, class_name: str, cause: str, remediation: str) -> None:
+        msg = f"{class_name} (specified in your config.yml) failed validation. Cause: {cause}. Suggested remediation: {remediation}"
         super().__init__(msg)
