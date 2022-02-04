8c2b28b694ec261b7d3490c608d5c005d5ac317d
Ajinkya Ghonge <ajinkyaghonge@fb.com>
2022-02-04 10:38:33 -0800

    Adding additional tests for decoupled_aggregation.
    Summary: Adding test cases to test MainUtil.h in decoupled_aggregation.
    
    Reviewed By: yanglu-fb
    
    Differential Revision: D33992267
    
    fbshipit-source-id: 8ca133daa24d6b05557b8e3efd4fbc4c273df754

---
+++ fbpcs/emp_games/attribution/decoupled_aggregation/test/MainUtilTest.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <emp-sh2pc/emp-sh2pc.h>
+#include <gtest/gtest.h>
+
+#include <fbpcf/mpc/EmpTestUtil.h>
+#include <string>
+#include "folly/init/Init.h"
+#include "folly/logging/xlog.h"
+
+#include "fbpcs/emp_games/attribution/decoupled_aggregation/MainUtil.h"
+
+namespace aggregation::private_aggregation {
+
+TEST(AggregationMainUtilSingleFileWithPostFixTest, TestConstructor) {
+  fbpcf::mpc::wrapTest<std::function<void()>>([]() {
+    string inputBasePath = "testPath";
+    auto fileStartIndex = 0;
+    auto inputFilePaths = aggregation::private_aggregation::getIOInputFilenames(
+        1, inputBasePath, fileStartIndex, true);
+
+    EXPECT_EQ(1, inputFilePaths.size());
+    string expected_path = inputBasePath + "_";
+    EXPECT_EQ(
+        folly::sformat("{}_{}", inputBasePath, fileStartIndex),
+        inputFilePaths.front());
+  });
+}
+
+TEST(AggregationMainUtilSingleFileWithoutPostFixTest, TestConstructor) {
+  fbpcf::mpc::wrapTest<std::function<void()>>([]() {
+    auto inputBasePath = "testPath";
+    auto fileStartIndex = 0;
+    auto inputFilePaths = aggregation::private_aggregation::getIOInputFilenames(
+        1, inputBasePath, fileStartIndex, false);
+
+    EXPECT_EQ(1, inputFilePaths.size());
+    EXPECT_EQ(inputBasePath, inputFilePaths.front());
+  });
+}
+
+TEST(AggregationMainUtilMultipleFilesTest, TestConstructor) {
+  fbpcf::mpc::wrapTest<std::function<void()>>([]() {
+    auto inputBasePath = "testPath";
+    auto fileStartIndex = 0;
+    auto inputFilePaths = aggregation::private_aggregation::getIOInputFilenames(
+        3, inputBasePath, fileStartIndex, true);
+
+    EXPECT_EQ(3, inputFilePaths.size());
+    EXPECT_EQ(
+        folly::sformat("{}_{}", inputBasePath, fileStartIndex),
+        inputFilePaths.front());
+  });
+}
+
+} // namespace aggregation::private_aggregation
