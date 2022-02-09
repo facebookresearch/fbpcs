d8294d85355085ffc15c2d9cae27421d990d8107
Mark Sliva <marksliva@fb.com>
2022-02-08 20:04:13 -0800

    bootstrap input_data_validation CLI (#558)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/558
    
    Add the validation CLI interface that the validation container will run.
    
    Reviewed By: wenhaizhu
    
    Differential Revision: D34025977
    
    fbshipit-source-id: a461da855591a4de7a294e7a3be9e3a2ba4f1cf5

---
+++ fbpcs/input_data_validation/input_data_validation_cli.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+
+"""
+CLI for running validations on the input data for private computations
+
+
+Usage:
+    input_data_validation_cli
+        --input-file-path=<input-file-path>
+        --cloud-provider=<cloud-provider>
+        [--access-key-id=<access-key-id>]
+        [--access-key-data=<access-key-data>]
+        [--start-timestamp=<start-timestamp>]
+        [--end-timestamp=<end-timestamp>]
+        [--valid-threshold-override=<valid-threshold-override>]
+"""
+
+
+from docopt import docopt
+from fbpcs.private_computation.entity.cloud_provider import CloudProvider
+from schema import Schema, Optional, Or, Use
+
+INPUT_FILE_PATH = "--input-file-path"
+CLOUD_PROVIDER = "--cloud-provider"
+ACCESS_KEY_ID = "--access-key-id"
+ACCESS_KEY_DATA = "--access-key-data"
+START_TIMESTAMP = "--start-timestamp"
+END_TIMESTAMP = "--end-timestamp"
+VALID_THRESHOLD_OVERRIDE = "--valid-threshold-override"
+
+
+def main() -> None:
+    optional_string = Or(None, str)
+    cloud_provider_from_string = Use(lambda arg: CloudProvider[arg])
+
+    s = Schema(
+        {
+            INPUT_FILE_PATH: str,
+            CLOUD_PROVIDER: cloud_provider_from_string,
+            Optional(ACCESS_KEY_ID): optional_string,
+            Optional(ACCESS_KEY_DATA): optional_string,
+            Optional(START_TIMESTAMP): optional_string,
+            Optional(END_TIMESTAMP): optional_string,
+            Optional(VALID_THRESHOLD_OVERRIDE): optional_string,
+        }
+    )
+    arguments = s.validate(docopt(__doc__))
+    assert arguments
+    print("Parsed input_data_validation_cli arguments")
+
+
+if __name__ == "__main__":
+    main()
