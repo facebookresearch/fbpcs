5b5e2b9233646cf3197c266dfdefab6534a18183
Lee Gross <leegross@fb.com>
2022-02-11 09:37:48 -0800

    refactor upload-binaries to separate script that we can reuse in github action
    Differential Revision: D34115242
    
    fbshipit-source-id: 7a130115cdb14d5874428aee305f413659bfb6c4

---
+++ upload-binaries-to-s3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+PROG_NAME=$0
+usage() {
+  cat << EOF >&2
+Usage: $PROG_NAME <emp_games|data_processing> <tag>
+
+package:
+  emp_games - extracts the binaries from fbpcs/emp-games docker image
+  data_processing - extracts the binaries from fbpcs/data-processing docker image
+  tag: used to determine the subfolder/version in s3 for each binary
+EOF
+  exit 1
+}
+
+PACKAGES="emp_games data_processing"
+PACKAGE=$1
+TAG=$2
+if [[ ! " $PACKAGES " =~ $PACKAGE ]] || [[ ! " $TAG " =~ $TAG ]]; then
+   usage
+fi
+shift
+
+one_docker_repo="one-docker-repository-prod"
+lift_package="s3://$one_docker_repo/private_lift/lift/${TAG}/lift"
+attribution_repo="s3://$one_docker_repo/private_attribution"
+decoupled_attribution="$attribution_repo/decoupled_attribution/${TAG}/decoupled_attribution"
+decoupled_aggregation="$attribution_repo/decoupled_aggregation/${TAG}/decoupled_aggregation"
+shard_aggregator_package="$attribution_repo/shard-aggregator/${TAG}/shard-aggregator"
+data_processing_repo="s3://$one_docker_repo/data_processing"
+
+if [ "$PACKAGE" = "emp_games" ]; then
+cd binaries_out || exit
+aws s3 cp lift_calculator "$lift_package"
+aws s3 cp decoupled_attribution_calculator "$decoupled_attribution"
+aws s3 cp decoupled_aggregation_calculator "$decoupled_aggregation"
+aws s3 cp shard_aggregator "$shard_aggregator_package"
+cd .. || exit
+fi
+
+if [ "$PACKAGE" = "data_processing" ]; then
+cd binaries_out || exit
+echo "$data_processing_repo/sharder/${TAG}/sharder"
+aws s3 cp sharder "$data_processing_repo/sharder/${TAG}/sharder"
+aws s3 cp sharder_hashed_for_pid "$data_processing_repo/sharder_hashed_for_pid/${TAG}/sharder_hashed_for_pid"
+aws s3 cp pid_preparer "$data_processing_repo/pid_preparer/${TAG}/pid_preparer"
+aws s3 cp lift_id_combiner "$data_processing_repo/lift_id_combiner/${TAG}/lift_id_combiner"
+aws s3 cp attribution_id_combiner "$data_processing_repo/attribution_id_combiner/${TAG}/attribution_id_combiner"
+fi
