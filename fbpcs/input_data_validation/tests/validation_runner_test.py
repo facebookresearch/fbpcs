aeb88cbc37eb859a2ddc65c629752bb54e26dfbf
Mark Sliva <marksliva@fb.com>
2022-02-11 15:58:31 -0800

    generate a validation report when running the CLI (#569)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/569
    
    The validation CLI now copies the remote input_data file into a /tmp location
    for fast access during the validations, which will be added later. If the copy
    fails, an error is raised and the CLI returns an exit code 1. Else in the
    success case a 0 is returned.
    
    Reviewed By: wenhaizhu
    
    Differential Revision: D34161482
    
    fbshipit-source-id: 10012e6e61fcc5f2be3f10632f6dc54f8238a1a9

--- fbpcs/input_data_validation/tests/validation_runner_test.py
+++ fbpcs/input_data_validation/tests/validation_runner_test.py
@@ -8,6 +8,8 @@
 from unittest import TestCase
 from unittest.mock import patch, MagicMock, Mock
 
+from fbpcs.input_data_validation.constants import INPUT_DATA_TMP_FILE_PATH
+from fbpcs.input_data_validation.enums import ValidationResult
 from fbpcs.input_data_validation.validation_runner import ValidationRunner
 from fbpcs.private_computation.entity.cloud_provider import CloudProvider
 
@@ -45,3 +47,49 @@ class TestValidationRunner(TestCase):
         self.assertEqual(
             validation_runner._storage_service, constructed_storage_service
         )
+
+    @patch("fbpcs.input_data_validation.validation_runner.time")
+    @patch("fbpcs.input_data_validation.validation_runner.S3StorageService")
+    def test_run_validations_success(
+        self, storage_service_mock: Mock, time_mock: Mock
+    ) -> None:
+        timestamp = 1644538741.077141
+        time_mock.time.return_value = timestamp
+        input_file_path = "s3://test-bucket/data.csv"
+        cloud_provider = CloudProvider.AWS
+        expected_temp_filepath = f"{INPUT_DATA_TMP_FILE_PATH}/data.csv-{timestamp}"
+        expected_report = {
+            "status": ValidationResult.SUCCESS.value,
+            "message": f"File: {input_file_path} was validated successfully",
+        }
+        storage_service_mock.__init__(return_value=storage_service_mock)
+
+        validation_runner = ValidationRunner(
+            input_file_path, cloud_provider, "us-west-2"
+        )
+        report = validation_runner.run()
+
+        self.assertEqual(validation_runner._local_file_path, expected_temp_filepath)
+        self.assertEqual(report, expected_report)
+        storage_service_mock.copy.assert_called_with(
+            input_file_path, expected_temp_filepath
+        )
+
+    @patch("fbpcs.input_data_validation.validation_runner.S3StorageService")
+    def test_run_validations_failure(self, storage_service_mock: Mock) -> None:
+        exception_message = "failed to copy"
+        input_file_path = "s3://test-bucket/data.csv"
+        cloud_provider = CloudProvider.AWS
+        expected_report = {
+            "status": ValidationResult.FAILED.value,
+            "message": f"File: {input_file_path} failed validation. Error: {exception_message}",
+        }
+        storage_service_mock.__init__(return_value=storage_service_mock)
+        storage_service_mock.copy.side_effect = Exception(exception_message)
+
+        validation_runner = ValidationRunner(
+            input_file_path, cloud_provider, "us-west-2"
+        )
+        report = validation_runner.run()
+
+        self.assertEqual(report, expected_report)
--- fbpcs/input_data_validation/validation_runner.py
+++ fbpcs/input_data_validation/validation_runner.py
@@ -18,9 +18,12 @@ Error handling:
 * If an unhandled error occurs, it will be returned in the report
 """
 
-from typing import Optional
+import time
+from typing import Dict, Optional
 
 from fbpcp.service.storage_s3 import S3StorageService
+from fbpcs.input_data_validation.constants import INPUT_DATA_TMP_FILE_PATH
+from fbpcs.input_data_validation.enums import ValidationResult
 from fbpcs.private_computation.entity.cloud_provider import CloudProvider
 
 
@@ -37,5 +40,33 @@ class ValidationRunner:
         valid_threshold_override: Optional[str] = None,
     ) -> None:
         self._input_file_path = input_file_path
+        self._local_file_path: str = self._get_local_filepath()
         self._cloud_provider = cloud_provider
         self._storage_service = S3StorageService(region, access_key_id, access_key_data)
+
+    def _get_local_filepath(self) -> str:
+        now = time.time()
+        filename = self._input_file_path.split("/")[-1]
+        return f"{INPUT_DATA_TMP_FILE_PATH}/{filename}-{now}"
+
+    def run(self) -> Dict[str, str]:
+        try:
+            self._storage_service.copy(self._input_file_path, self._local_file_path)
+        except Exception as e:
+            return self._format_validation_result(
+                ValidationResult.FAILED,
+                f"File: {self._input_file_path} failed validation. Error: {e}",
+            )
+
+        return self._format_validation_result(
+            ValidationResult.SUCCESS,
+            f"File: {self._input_file_path} was validated successfully",
+        )
+
+    def _format_validation_result(
+        self, status: ValidationResult, message: str
+    ) -> Dict[str, str]:
+        return {
+            "status": status.value,
+            "message": message,
+        }
