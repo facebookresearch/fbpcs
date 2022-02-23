6a51df6512fc3350ec0b1a8f26c14724c0691971
Mark Sliva <marksliva@fb.com>
2022-02-22 20:08:29 -0800

    process the csv rows (#601)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/601
    
    Loop through each row of the data CSV from the local tmp file, and return the
    total number of rows processed in the report.
    
    Differential Revision: D34350982
    
    fbshipit-source-id: 8ef1965c9f68f09b0a54ef5d8410cb9fa9d956e7

--- fbpcs/input_data_validation/tests/validation_runner_test.py
+++ fbpcs/input_data_validation/tests/validation_runner_test.py
@@ -5,6 +5,8 @@
 
 # pyre-strict
 
+import os
+from typing import Iterable
 from unittest import TestCase
 from unittest.mock import patch, MagicMock, Mock
 
@@ -14,9 +16,22 @@ from fbpcs.input_data_validation.validation_runner import ValidationRunner
 from fbpcs.private_computation.entity.cloud_provider import CloudProvider
 
 TEST_INPUT_FILE_PATH = "s3://test-bucket/data.csv"
+TEST_REGION = "us-west-2"
+TEST_TIMESTAMP = 1644538741.077141
+TEST_TEMP_FILEPATH = f"{INPUT_DATA_TMP_FILE_PATH}/data.csv-{TEST_TIMESTAMP}"
 
 
 class TestValidationRunner(TestCase):
+    def setUp(self) -> None:
+        open(TEST_TEMP_FILEPATH, "a").close()
+
+    def tearDown(self) -> None:
+        os.remove(TEST_TEMP_FILEPATH)
+
+    def write_lines_to_file(self, lines: Iterable[bytes]) -> None:
+        with open(TEST_TEMP_FILEPATH, "wb") as tmp_csv_file:
+            tmp_csv_file.writelines(lines)
+
     @patch("fbpcp.service.storage_s3.S3StorageService")
     def test_initializating_the_validation_runner_fields(self, _mock) -> None:
         cloud_provider = CloudProvider.AWS
@@ -48,33 +63,6 @@ class TestValidationRunner(TestCase):
             validation_runner._storage_service, constructed_storage_service
         )
 
-    @patch("fbpcs.input_data_validation.validation_runner.time")
-    @patch("fbpcs.input_data_validation.validation_runner.S3StorageService")
-    def test_run_validations_success(
-        self, storage_service_mock: Mock, time_mock: Mock
-    ) -> None:
-        timestamp = 1644538741.077141
-        time_mock.time.return_value = timestamp
-        input_file_path = "s3://test-bucket/data.csv"
-        cloud_provider = CloudProvider.AWS
-        expected_temp_filepath = f"{INPUT_DATA_TMP_FILE_PATH}/data.csv-{timestamp}"
-        expected_report = {
-            "status": ValidationResult.SUCCESS.value,
-            "message": f"File: {input_file_path} was validated successfully",
-        }
-        storage_service_mock.__init__(return_value=storage_service_mock)
-
-        validation_runner = ValidationRunner(
-            input_file_path, cloud_provider, "us-west-2"
-        )
-        report = validation_runner.run()
-
-        self.assertEqual(validation_runner._local_file_path, expected_temp_filepath)
-        self.assertEqual(report, expected_report)
-        storage_service_mock.copy.assert_called_with(
-            input_file_path, expected_temp_filepath
-        )
-
     @patch("fbpcs.input_data_validation.validation_runner.S3StorageService")
     def test_run_validations_failure(self, storage_service_mock: Mock) -> None:
         exception_message = "failed to copy"
@@ -83,6 +71,7 @@ class TestValidationRunner(TestCase):
         expected_report = {
             "status": ValidationResult.FAILED.value,
             "message": f"File: {input_file_path} failed validation. Error: {exception_message}",
+            "rows_processed_count": "0",
         }
         storage_service_mock.__init__(return_value=storage_service_mock)
         storage_service_mock.copy.side_effect = Exception(exception_message)
@@ -92,4 +81,31 @@ class TestValidationRunner(TestCase):
         )
         report = validation_runner.run()
 
-        self.assertEqual(report, expected_report)
+        self.assertDictEqual(report, expected_report)
+
+    @patch("fbpcs.input_data_validation.validation_runner.S3StorageService")
+    @patch("fbpcs.input_data_validation.validation_runner.time")
+    def test_run_validations_reads_the_local_csv_rows(
+        self, time_mock: Mock, _storage_service_mock: Mock
+    ) -> None:
+        time_mock.time.return_value = TEST_TIMESTAMP
+        cloud_provider = CloudProvider.AWS
+        lines = [
+            b"id_,value,timestamp\n",
+            b"abcd/1234+WXYZ=,100,1645157987\n",
+            b"abcd/1234+WXYZ=,100,1645157987\n",
+            b"abcd/1234+WXYZ=,100,1645157987\n",
+        ]
+        self.write_lines_to_file(lines)
+        expected_report = {
+            "status": ValidationResult.SUCCESS.value,
+            "message": f"File: {TEST_INPUT_FILE_PATH} was validated successfully",
+            "rows_processed_count": "3",
+        }
+
+        validation_runner = ValidationRunner(
+            TEST_INPUT_FILE_PATH, cloud_provider, TEST_REGION
+        )
+        report = validation_runner.run()
+
+        self.assertDictEqual(report, expected_report)
