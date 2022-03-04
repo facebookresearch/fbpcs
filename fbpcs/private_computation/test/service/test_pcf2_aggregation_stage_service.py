595a7a493851ce175043d49acec5dc9a9a1f9fb9
Ajinkya Ghonge <ajinkyaghonge@fb.com>
2022-03-04 15:40:49 -0800

    Adding pcf2 aggregation service. (#650)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/650
    
    # Context
    With the Open sourcing of PCF 2.0, we will also be releasing attribution and aggregation based on PCF 2.0 to production. While promoting the PCF 2.0 games to production and making them the default games for production runs, we would also like to keep an option to run the current production games (i.e. attribution and aggregation game based on PCF 1.0) in prod for disaster recovery/fall back scenarios.
    
    Thus in this stack of diffs
    1. I will be adding a new flow for PCF 2.0 games in PCS. The new flow will be
        -  CREATED -> INPUT_DATA_VALIDATION -> ID_MATCH -> ID_MATCH_POST_PROCESS -> PREPARE -> PCF2_ATTRIBUTION -> PCF2_AGGREGATION -> AGGREGATE POST_PROCESSING_HANDLERS
    
    2.Will be adding new service for PCF 2.0 attribution
    3.Will be adding a new service for  PCF 2.0 aggregation.
    4.Directory where PCF 2.0 attribution and aggregation binaries are placed would be -
    
    ```
    private_attribution/prf2_attribution and private_attribution/pcf2_aggregation
     ```
    5.Adding end to end tests for the new path.
    
    # Technical decision
    Leveraging the dynamic stage flow architecture in PCS to efficiently manage the stages without many changes that would break backward compatibility. While this approach does involve some temporary duplication of code, the duplicate code will be soon removed, as legacy attribution and aggregation games will be marked deprecated once we release PCF 2.0 to production and after 5 successful production runs (setting this number as we used the same number last half), we will remove the legacy code and flow.
    This is least error prone way to manage different flows as will make it easier for us to fallback on the legacy flow in case of any errors/issues on new flow.
    
    # This Diff
    Adding new service to support PCF 2.0 aggregation as well as test cases for the same.
    
    # This Stack
    1. Specify path for PCF 2.0 binaries and add entries for them in private_computation_game.
    2. Create service for pcf2 attribution.
    3. Create service for pcf 2 aggregation.
    4. Create new flow for PCF2.
    5. Create additional test in fbpcs_e2e_runner.
    6. Create additional test in automated_e2e_runner.
    
    Differential Revision: D34658666
    
    fbshipit-source-id: 364a6222ec6e24fa75b1b5b848ba0b577912d813

---
+++ fbpcs/private_computation/test/service/test_pcf2_aggregation_stage_service.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from collections import defaultdict
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import AsyncMock, MagicMock, patch
+
+from fbpcp.entity.mpc_instance import MPCParty
+from fbpcs.common.entity.pcs_mpc_instance import PCSMPCInstance
+from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
+from fbpcs.private_computation.entity.private_computation_instance import (
+    AttributionRule,
+    PrivateComputationGameType,
+    PrivateComputationInstance,
+    PrivateComputationRole,
+    AggregationType,
+)
+from fbpcs.private_computation.repository.private_computation_game import GameNames
+from fbpcs.private_computation.service.constants import (
+    NUM_NEW_SHARDS_PER_FILE,
+)
+from fbpcs.private_computation.service.pcf2_aggregation_stage_service import (
+    PCF2AggregationStageService,
+)
+from fbpcs.private_computation.stage_flows.private_computation_decoupled_stage_flow import (
+    PrivateComputationInstanceStatus,
+)
+
+
+class TestPCF2AggregationStageService(IsolatedAsyncioTestCase):
+    @patch("fbpcp.service.mpc.MPCService")
+    def setUp(self, mock_mpc_svc) -> None:
+        self.mock_mpc_svc = mock_mpc_svc
+        self.mock_mpc_svc.create_instance = MagicMock()
+
+        onedocker_binary_config_map = defaultdict(
+            lambda: OneDockerBinaryConfig(
+                tmp_directory="/test_tmp_directory/", binary_version="latest"
+            )
+        )
+        self.stage_svc = PCF2AggregationStageService(
+            onedocker_binary_config_map, self.mock_mpc_svc
+        )
+
+    async def test_aggregation_stage(self) -> None:
+        private_computation_instance = self._create_pc_instance()
+        mpc_instance = PCSMPCInstance.create_instance(
+            instance_id=private_computation_instance.instance_id
+            + "_"
+            + GameNames.PCF2_AGGREGATION.value
+            + "0",
+            game_name=GameNames.PCF2_AGGREGATION.value,
+            mpc_party=MPCParty.CLIENT,
+            num_workers=private_computation_instance.num_mpc_containers,
+        )
+
+        self.mock_mpc_svc.start_instance_async = AsyncMock(return_value=mpc_instance)
+
+        test_server_ips = [
+            f"192.0.2.{i}"
+            for i in range(private_computation_instance.num_mpc_containers)
+        ]
+        await self.stage_svc.run_async(private_computation_instance, test_server_ips)
+
+        self.assertEqual(mpc_instance, private_computation_instance.instances[0])
+
+    def test_get_game_args(self) -> None:
+        private_computation_instance = self._create_pc_instance()
+
+        common_game_args = {
+            "input_base_path": private_computation_instance.data_processing_output_path,
+            "input_base_path_secret_share": private_computation_instance.pcf2_attribution_stage_output_base_path,
+            "output_base_path": private_computation_instance.pcf2_aggregation_stage_output_base_path,
+            "num_files": private_computation_instance.num_files_per_mpc_container,
+            "concurrency": private_computation_instance.concurrency,
+            "run_name": private_computation_instance.instance_id
+            if self.stage_svc._log_cost_to_s3
+            else "",
+            "max_num_touchpoints": private_computation_instance.padding_size,
+            "max_num_conversions": private_computation_instance.padding_size,
+            # pyre-fixme[16]: Optional type has no attribute `value`.
+            "attribution_rules": private_computation_instance.attribution_rule.value,
+            # pyre-fixme[16]: Optional type has no attribute `value`.
+            "aggregators": private_computation_instance.aggregation_type.value,
+            "use_xor_encryption": True,
+            "use_postfix": True,
+            "log_cost": True,
+        }
+        test_game_args = [
+            {
+                **common_game_args,
+                "file_start_index": 0,
+            },
+            {
+                **common_game_args,
+                "file_start_index": private_computation_instance.num_files_per_mpc_container,
+            },
+        ]
+
+        actual_value = self.stage_svc._get_compute_metrics_game_args(
+            private_computation_instance
+        )
+        self.assertEqual(
+            test_game_args,
+            actual_value,
+        )
+
+    def _create_pc_instance(self) -> PrivateComputationInstance:
+
+        return PrivateComputationInstance(
+            instance_id="test_instance_123",
+            role=PrivateComputationRole.PARTNER,
+            instances=[],
+            status=PrivateComputationInstanceStatus.DECOUPLED_ATTRIBUTION_COMPLETED,
+            attribution_rule=AttributionRule.LAST_CLICK_1D,
+            aggregation_type=AggregationType.MEASUREMENT,
+            status_update_ts=1600000000,
+            num_pid_containers=2,
+            num_mpc_containers=2,
+            num_files_per_mpc_container=NUM_NEW_SHARDS_PER_FILE,
+            game_type=PrivateComputationGameType.ATTRIBUTION,
+            input_path="456",
+            output_dir="789",
+            padding_size=4,
+        )
