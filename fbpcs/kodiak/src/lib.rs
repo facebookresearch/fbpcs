758d35bab9a5158d42c733edd20ef3c57f28cbe0
Logan Gore <gorel@fb.com>
2022-03-04 11:25:27 -0800

    First commit of Rust mpc app infra library
    Differential Revision: D34647880
    
    fbshipit-source-id: 63559c1a20a26dc5c5f3ca39cf953a4ef8a8d7ad

---
+++ fbpcs/kodiak/src/lib.rs
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
