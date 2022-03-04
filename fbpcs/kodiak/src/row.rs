b4f1fd26a4aa349b7346a93684fee7cb92cfc90f
Logan Gore <gorel@fb.com>
2022-03-04 15:49:53 -0800

    Define row struct (#648)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/648
    
    It's a row of data
    
    Reviewed By: jrodal98
    
    Differential Revision: D34648635
    
    fbshipit-source-id: daf8c64effda21378b7d6c862f870428dc02a43f

--- fbpcs/kodiak/src/row.rs
+++ fbpcs/kodiak/src/row.rs
@@ -5,4 +5,26 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-struct Row;
+pub struct Row {
+    // Dynamic columns because we don't really care about the underlying "data"
+    // This allows us to track which columns have already been calculated
+    // This struct would likely handle building the DAG to know which columns can
+    // be computed next.
+    columns: HashMap<String, Box<dyn MPCColumn>>,
+}
+
+impl Row {
+    fn new() -> Self {
+        Self {
+            columns: HashMap::new(),
+        }
+    }
+
+    fn get_data<T: MPCColumn>(&self) -> Option<T::DType> {
+        self.columns
+            .get(T.name())?
+            .as_any()
+            .downcast_ref::<T>()
+            .data()
+    }
+}
