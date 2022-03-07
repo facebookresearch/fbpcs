c86949c6d99df1a97e83253b0849b5e2188255eb
Logan Gore <gorel@fb.com>
2022-03-06 22:49:59 -0800

    Define network config for host/port/role
    Differential Revision: D34676757
    
    fbshipit-source-id: efe1f3b93f7454e1404c0760fb583af6375861f2

---
+++ fbpcs/kodiak/src/network_config.rs
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+struct NetworkConfig {
+    role: MPCRole,
+    host: String,
+    port: u16,
+}
+
+impl NetworkConfig {
+    pub fn new(role: MPCRole, host: String, port: u16) -> Self {
+        Self { role, host, port }
+    }
+}
