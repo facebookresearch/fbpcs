c663893a1563778bef8acd7758d7770f750ac0a1
Logan Gore <gorel@fb.com>
2022-03-06 22:49:43 -0800

    Define operation config for concurrency and logging
    Differential Revision: D34676753
    
    fbshipit-source-id: b3a04d3eb847766c7fda6bdb6c79c30d7e2c838b

---
+++ fbpcs/kodiak/src/operation_config_builder.rs
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+pub struct OperationConfigBuilder {
+    concurrency: u32,
+    log_level: log::Level,
+    debug: bool,
+}
+
+impl OperationConfigBuilder {
+    pub fn new() -> Self {
+        Self {
+            concurrency: 1,
+            log_level: log::Info,
+            debug: false,
+        }
+    }
+
+    pub fn with_concurrency(&mut self, concurrency: u32) -> Self {
+        self.concurrency = concurrency;
+        self
+    }
+
+    pub fn with_log_level(&mut self, log_level: log::Level) -> Self {
+        self.log_level = log_level;
+        self
+    }
+
+    pub fn with_debug(&mut self, dbug: bool) -> Self {
+        self.debug = debug;
+        self
+    }
+
+    pub fn build(&self) -> OperationConfig {
+        OperationConfig::new(self.concurrency, self.log_level, self.debug)
+    }
+}
