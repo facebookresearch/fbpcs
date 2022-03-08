039732952a18decc98e5165fc12766bab6e93ce2
Jacob Rodal <jrodal@fb.com>
2022-03-07 16:18:39 -0800

    include modules in lib.rs
    Differential Revision: D34696969
    
    fbshipit-source-id: f5d4da263eb0a1365cf6b7e4eeb6486cff6444da

--- fbpcs/kodiak/src/lib.rs
+++ fbpcs/kodiak/src/lib.rs
@@ -5,6 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+pub mod column_metadata;
+// pub mod execution_config;
+// pub mod mpc_game;
+// pub mod mpc_metric;
+// pub mod mpc_role;
+// pub mod mpc_view;
+// pub mod mpc_view_builder;
+// pub mod network_config;
+// pub mod network_config_builder;
+// pub mod operation_config;
+// pub mod operation_config_builder;
+// pub mod row;
+
 #[cfg(test)]
 mod tests {
     #[test]
