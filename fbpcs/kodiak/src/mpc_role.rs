5483dbcebcd6629dc2ec59607c692482c0d07d19
Logan Gore <gorel@fb.com>
2022-03-08 15:38:56 -0800

    impl FromStr for MPCRole (#672)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/672
    
    This will be useful for arg parsing
    
    Reviewed By: jrodal98
    
    Differential Revision: D34727852
    
    fbshipit-source-id: 64d94a3bb680aebc114db5e222211b155e16ebe1

--- fbpcs/kodiak/src/mpc_role.rs
+++ fbpcs/kodiak/src/mpc_role.rs
@@ -5,7 +5,31 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct ParseMPCRoleError {
+    s: String,
+}
+
+impl ParseMPCRoleError {
+    fn new(s: &str) -> ParseMPCRoleError {
+        ParseMPCRoleError { s: s.to_string() }
+    }
+}
+
 pub enum MPCRole {
     Publisher,
     Partner,
+}
+
+impl FromStr for MPCRole {
+    type Err = ParseMPCRoleError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "publisher" => Ok(MPCRole::Publisher),
+            "partner" => Ok(MPCRole::Partner),
+            _ => Err(Self::Err::new(s)),
+        }
+    }
 }
