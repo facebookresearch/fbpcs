4f3ddc195e960d822b21874eec34f91126026a98
Logan Gore <gorel@fb.com>
2022-03-14 19:36:48 -0700

    Use batching with dummy game
    Summary:
    proof of concept using ORAM!
    
    Gotta say, this was harder than I expected. I think there's a lot of potential for building a library of generic "common" functions.
    
    Reviewed By: jrodal98
    
    Differential Revision: D34813311
    
    fbshipit-source-id: 67b8a0ac5a56cf89b1a0065f3c339759e70c15c7

--- fbpcs/emp_games/simple_adding_game/main.cpp
+++ fbpcs/emp_games/simple_adding_game/main.cpp
@@ -11,47 +11,93 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include <fbpcf/engine/communication/SocketPartyCommunicationAgentFactory.h>
 #include <fbpcf/frontend/mpcGame.h>
+#include <fbpcf/mpc_std_lib/oram/IWriteOnlyOram.h>
+#include <fbpcf/mpc_std_lib/oram/LinearOramFactory.h>
 #include <fbpcf/scheduler/IScheduler.h>
 #include <fbpcf/scheduler/SchedulerHelper.h>
 
 constexpr int32_t PUBLISHER_ROLE = 0;
 constexpr int32_t PARTNER_ROLE = 1;
 
+using fbpcf::engine::communication::IPartyCommunicationAgentFactory;
 using fbpcf::engine::communication::SocketPartyCommunicationAgentFactory;
+using fbpcf::engine::util::AesPrgFactory;
 using fbpcf::frontend::MpcGame;
+using fbpcf::mpc_std_lib::oram::IWriteOnlyOram;
+using fbpcf::mpc_std_lib::oram::LinearOram;
+using fbpcf::mpc_std_lib::oram::LinearOramFactory;
 using fbpcf::scheduler::createLazySchedulerWithRealEngine;
 using fbpcf::scheduler::IScheduler;
 using fbpcf::scheduler::SchedulerKeeper;
 
 template <int schedulerId, bool usingBatch = false>
 class SimpleAddingGame : public MpcGame<schedulerId> {
-  // TODO: Batching false, SecSignedInt<64, true> for batch support
   using SecInt =
-      typename fbpcf::frontend::MpcGame<schedulerId>::template SecSignedInt<64>;
+      typename MpcGame<schedulerId>::template SecSignedInt<64, usingBatch>;
+  using AggType = uint32_t;
+  using OramRole = typename IWriteOnlyOram<AggType>::Role;
 
  public:
-  explicit SimpleAddingGame(std::unique_ptr<IScheduler> scheduler)
-      : MpcGame<schedulerId>(std::move(scheduler)) {}
+  SimpleAddingGame(
+      std::unique_ptr<IScheduler> scheduler,
+      std::unique_ptr<IPartyCommunicationAgentFactory> f)
+      : MpcGame<schedulerId>(std::move(scheduler)),
+        commAgentFactory_{std::move(f)} {}
+
+  static OramRole getOramRoleFromInt(int32_t role) {
+    switch (role) {
+      case PUBLISHER_ROLE:
+        return OramRole::Alice;
+      case PARTNER_ROLE:
+        return OramRole::Bob;
+      default:
+        std::cerr << "Unknown role: " << role << '\n';
+        std::exit(1);
+    }
+  }
 
   int64_t run(int32_t myRole, std::vector<int64_t> myInput) {
-    SecInt res;
-    for (size_t i = 0; i < myInput.size(); ++i) {
-      SecInt publisher{myInput.at(i), PUBLISHER_ROLE};
-      SecInt partner{myInput.at(i), PARTNER_ROLE};
-      // TODO: Int::operator+= isn't defined either :(
-      res = res + publisher + partner;
+    SecInt publisher{myInput, PUBLISHER_ROLE};
+    SecInt partner{myInput, PARTNER_ROLE};
+    SecInt res = publisher + partner;
+    return sum(myRole, res);
+  }
+
+  int64_t sum(int32_t myRole, const SecInt& v) {
+    std::cout << "Creating ORAM\n";
+    auto oram = makeOram<AggType>(myRole, 1);
+    std::vector<std::vector<bool>> indexShares;
+    auto valueShares = v.extractIntShare().getBooleanShares();
+    for (size_t i = 0; i < valueShares.size(); ++i) {
+      // All index shares can simply be zero since we're not actually
+      // grouping values in any way
+      indexShares.emplace_back(valueShares.at(0).size(), 0);
     }
-    // TODO: Support XOR share
-    // NOTE: Even though we only care about *myRole* below, we need
-    // to execute both lines so both parties get output.
-    auto a = res.openToParty(PUBLISHER_ROLE).getValue();
-    auto b = res.openToParty(PARTNER_ROLE).getValue();
+    oram->obliviousAddBatch(indexShares, valueShares);
+    auto a = oram->publicRead(0, getOramRoleFromInt(PUBLISHER_ROLE));
+    auto b = oram->publicRead(0, getOramRoleFromInt(PARTNER_ROLE));
     return myRole == PUBLISHER_ROLE ? a : b;
   }
+
+ private:
+  template <typename T>
+  std::unique_ptr<IWriteOnlyOram<T>> makeOram(int32_t myRole, size_t oramSize) {
+    return std::make_unique<
+               fbpcf::mpc_std_lib::oram::LinearOramFactory<T, schedulerId>>(
+               myRole == PUBLISHER_ROLE ? OramRole::Alice : OramRole::Bob,
+               myRole,
+               1 - myRole, // convert 0 to 1 and 1 to 0
+               *commAgentFactory_,
+               std::make_unique<AesPrgFactory>())
+        ->create(oramSize);
+  }
+
+  std::unique_ptr<IPartyCommunicationAgentFactory> commAgentFactory_;
 };
 
 std::vector<int64_t> readInput(const std::string& filename) {
@@ -82,7 +128,7 @@ int main(int argc, char** argv) {
 
   std::cout << "Creating communication agent factory\n";
   std::map<int, SocketPartyCommunicationAgentFactory::PartyInfo> partyInfos{
-      {{0, {host, port}}, {1, {host, port}}}};
+      {{PUBLISHER_ROLE, {host, port}}, {PARTNER_ROLE, {host, port}}}};
   auto commAgentFactory =
       std::make_unique<SocketPartyCommunicationAgentFactory>(
           role, std::move(partyInfos));
@@ -92,7 +138,9 @@ int main(int argc, char** argv) {
 
   // TODO: Check scheduler Id?
   std::cout << "Starting game\n";
-  auto game = SimpleAddingGame<0>(std::move(scheduler));
+  auto game = SimpleAddingGame<0, true>(
+      std::move(scheduler), std::move(commAgentFactory));
+
   auto res = game.run(role, input);
   std::cout << "Game done!\n";
   std::cout << "Output: " << res << '\n';
