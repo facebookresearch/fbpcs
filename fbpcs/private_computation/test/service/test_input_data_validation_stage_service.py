c4cfa005955a70178eb998b5f33074ca3dba5026
Mark Sliva <marksliva@fb.com>
2022-03-16 09:44:41 -0700

    remove the placeholder input_data validation
    Summary:
    All of the input_data validations will be run in the onedocker container
    instead.
    
    Differential Revision: D34899858
    
    fbshipit-source-id: 6fc3f7ccf1e86844dfad9056798544bb269715a3

--- fbpcs/private_computation/test/service/test_input_data_validation_stage_service.py
+++ fbpcs/private_computation/test/service/test_input_data_validation_stage_service.py
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import patch
 
 from fbpcs.private_computation.entity.private_computation_instance import (
     PrivateComputationGameType,
@@ -36,13 +35,9 @@ class TestInputDataValidationStageService(IsolatedAsyncioTestCase):
             output_dir="789",
         )
 
-    @patch("fbpcp.service.storage.StorageService")
-    async def test_run_async_changes_the_status_when_the_file_exists(
-        self, mock_storage_service
-    ) -> None:
+    async def test_run_async_changes_the_status_when_there_are_no_issues(self) -> None:
         pc_instance = self._pc_instance
-        mock_storage_service.file_exists.return_value = True
-        stage_service = InputDataValidationStageService(mock_storage_service)
+        stage_service = InputDataValidationStageService()
 
         self.assertEqual(
             pc_instance.status,
@@ -53,42 +48,4 @@ class TestInputDataValidationStageService(IsolatedAsyncioTestCase):
         self.assertEqual(
             pc_instance.status,
             PrivateComputationInstanceStatus.INPUT_DATA_VALIDATION_COMPLETED,
-        )
-
-    @patch("fbpcp.service.storage.StorageService")
-    async def test_run_async_fails_when_the_file_does_not_exist(
-        self, mock_storage_service
-    ) -> None:
-        pc_instance = self._pc_instance
-        mock_storage_service.file_exists.return_value = False
-        stage_service = InputDataValidationStageService(mock_storage_service)
-
-        self.assertEqual(
-            pc_instance.status,
-            PrivateComputationInstanceStatus.INPUT_DATA_VALIDATION_STARTED,
-        )
-
-        await stage_service.run_async(pc_instance)
-        self.assertEqual(
-            pc_instance.status,
-            PrivateComputationInstanceStatus.INPUT_DATA_VALIDATION_FAILED,
-        )
-
-    @patch("fbpcp.service.storage.StorageService")
-    async def test_run_async_fails_when_a_value_error_occurs(
-        self, mock_storage_service
-    ) -> None:
-        pc_instance = self._pc_instance
-        mock_storage_service.file_exists.side_effect = ValueError("test error")
-        stage_service = InputDataValidationStageService(mock_storage_service)
-
-        self.assertEqual(
-            pc_instance.status,
-            PrivateComputationInstanceStatus.INPUT_DATA_VALIDATION_STARTED,
-        )
-
-        await stage_service.run_async(pc_instance)
-        self.assertEqual(
-            pc_instance.status,
-            PrivateComputationInstanceStatus.INPUT_DATA_VALIDATION_FAILED,
         )
