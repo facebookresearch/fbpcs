57ff1d4d6a3e5bd1f93b17fa3cec421e92ecc2f8
Yong Tao <taoyong@fb.com>
2022-03-16 11:56:08 -0700

    introduce Validator interface and ValidationReport class (#690)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/690
    
    We decide to make the following refactorings to the start up validator CLI:
    
    1. Validation CLI will be initialized with a list of Validators.
    2. Validation CLI will invoke each and every validator once and consolidate all ValidationReports
    3. Validators are self-contained and encapsulate one or more related validation rules.
    
    Implication: this means the current header_validator, line_ending_validator, and row_validator are not qualified as Validators, because the latter two will only be executed if header_validator does not throw an exception. They will be bundled into 1 top-level validator, which will actually be more desirable/efficient because we will only need to download the input data file once.
    
    {F710945886}
    
    execution plan: https://docs.google.com/document/d/1GgD4VqTL_RdJlOGIedjpkzVwEgAcCzoVNINYmegOr-0/edit#
    
    Differential Revision: D34898072
    
    fbshipit-source-id: 41536c90c1d1024b6dc5443b60283e85f0a676e4

---
+++ fbpcs/input_data_validation/tests/validation_report_test.py
@@ -0,0 +1,60 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from unittest import TestCase
+
+from fbpcs.input_data_validation.enums import ValidationResult
+from fbpcs.input_data_validation.validation_report import ValidationReport
+
+
+class TestValidationReport(TestCase):
+    def test_get_str_for_report_with_details(self) -> None:
+        expected_report_str = """Validation Report: test_validator_name
+Result: success
+Message: test_message
+Details:
+{
+    "test_key_1": 5,
+    "test_key_2": {
+        "test_key_3": {
+            "test_key_4": 1
+        },
+        "test_key_5": {
+            "test_key_6": 1,
+            "test_key_7": 2
+        }
+    }
+}"""
+        report = ValidationReport(
+            validation_result=ValidationResult.SUCCESS,
+            validator_name="test_validator_name",
+            message="test_message",
+            details={
+                "test_key_1": 5,
+                "test_key_2": {
+                    "test_key_3": {
+                        "test_key_4": 1,
+                    },
+                    "test_key_5": {
+                        "test_key_6": 1,
+                        "test_key_7": 2,
+                    },
+                },
+            },
+        )
+        self.assertEqual(expected_report_str, str(report))
+
+    def test_get_str_for_report_without_details(self) -> None:
+        expected_report_str = """Validation Report: test_validator_name
+Result: failed
+Message: test_message"""
+        report = ValidationReport(
+            validation_result=ValidationResult.FAILED,
+            validator_name="test_validator_name",
+            message="test_message",
+        )
+        self.assertEqual(expected_report_str, str(report))
---
+++ fbpcs/input_data_validation/validation_report.py
@@ -0,0 +1,36 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+import json
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
+
+from dataclasses_json import dataclass_json
+from fbpcs.input_data_validation.enums import ValidationResult
+
+
+@dataclass_json
+@dataclass
+class ValidationReport:
+    validation_result: ValidationResult
+    validator_name: str
+    message: str
+    details: Optional[Dict[str, Any]] = None
+
+    def __str__(self) -> str:
+        if self.details:
+            return (
+                f"Validation Report: {self.validator_name}\n"
+                f"Result: {self.validation_result.value}\n"
+                f"Message: {self.message}\n"
+                f"Details:\n{json.dumps(self.details, sort_keys=True, indent=4)}"
+            )
+        else:
+            return (
+                f"Validation Report: {self.validator_name}\n"
+                f"Result: {self.validation_result.value}\n"
+                f"Message: {self.message}"
+            )
