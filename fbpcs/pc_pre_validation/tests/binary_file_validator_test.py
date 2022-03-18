b23c543771f86e806afeabb349ca156f1f61bdf3
Yong Tao <taoyong@fb.com>
2022-03-18 00:39:08 -0700

    Add binary file validator (#717)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/717
    
    Binary file validator will try accessing all PC binaries using the lightweight `file_exist()` method (which under the hood calls s3 head_object()),
    
    Each file_exist() may return 4 types of response:
    1. a boolean value of True: indicate that the file exists
    2. a boolean value of False: indicate that the file does not exists,
    3. a PcpError with "`An error occurred (403) when calling the HeadObject operation: Forbidden`" message: indicate that the access request was denied due to permission reasons.
    4. other unexpected errors
    
    The validator will return Success if and only if all file_exists() requests returns True, return Failed if any request returns False or 403, return Success with Warning if any request returns unexpected error.
    
    Reviewed By: janehwznmeta, marksliva
    
    Differential Revision: D34951703
    
    fbshipit-source-id: 8180b7f5134e917f74bcaee970b9c4a1e23fdd4e

---
+++ fbpcs/pc_pre_validation/tests/binary_file_validator_test.py
@@ -0,0 +1,114 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from fbpcp.error.pcp import PcpError
+from fbpcs.pc_pre_validation.binary_file_validator import BinaryFileValidator
+from fbpcs.pc_pre_validation.constants import BINARY_FILE_VALIDATOR_NAME
+from fbpcs.pc_pre_validation.enums import ValidationResult
+from fbpcs.pc_pre_validation.validation_report import ValidationReport
+
+TEST_REGION = "us-west-2"
+TEST_BINARY_REPO = "https://test.s3.us-west-2.amazonaws.com"
+TEST_BINARY_PATHS = ["path/to/binary/1", "path/to_binary_2", "path/to_binary_3"]
+
+
+class TestBinaryFileValidator(TestCase):
+    @patch("fbpcs.pc_pre_validation.binary_file_validator.S3StorageService")
+    def test_run_validations_success(self, storage_service_mock: Mock) -> None:
+        expected_report = ValidationReport(
+            validation_result=ValidationResult.SUCCESS,
+            validator_name=BINARY_FILE_VALIDATOR_NAME,
+            message="Completed binary accessibility validation successfuly",
+        )
+        storage_service_mock.__init__(return_value=storage_service_mock)
+        storage_service_mock.file_exists.return_value = True
+
+        validator = BinaryFileValidator(
+            TEST_REGION, TEST_BINARY_REPO, TEST_BINARY_PATHS
+        )
+        report = validator.validate()
+
+        self.assertEqual(report, expected_report)
+        self.assertEqual(
+            storage_service_mock.file_exists.call_count, len(TEST_BINARY_PATHS)
+        )
+
+    @patch("fbpcs.pc_pre_validation.binary_file_validator.S3StorageService")
+    def test_run_validations_binary_not_exist(self, storage_service_mock: Mock) -> None:
+        expected_report = ValidationReport(
+            validation_result=ValidationResult.FAILED,
+            validator_name=BINARY_FILE_VALIDATOR_NAME,
+            message="You don't have permission to access some private computation softwares. Please contact your representative at Meta",
+            details={
+                f"{TEST_BINARY_REPO}/{TEST_BINARY_PATHS[0]}": "binary does not exist"
+            },
+        )
+        storage_service_mock.__init__(return_value=storage_service_mock)
+        storage_service_mock.file_exists.side_effect = [False, True, True]
+
+        validator = BinaryFileValidator(
+            TEST_REGION, TEST_BINARY_REPO, TEST_BINARY_PATHS
+        )
+        report = validator.validate()
+
+        self.assertEqual(report, expected_report)
+        self.assertEqual(
+            storage_service_mock.file_exists.call_count, len(TEST_BINARY_PATHS)
+        )
+
+    @patch("fbpcs.pc_pre_validation.binary_file_validator.S3StorageService")
+    def test_run_validations_binary_access_denied(
+        self, storage_service_mock: Mock
+    ) -> None:
+        expected_report = ValidationReport(
+            validation_result=ValidationResult.FAILED,
+            validator_name=BINARY_FILE_VALIDATOR_NAME,
+            message="You don't have permission to access some private computation softwares. Please contact your representative at Meta",
+            details={
+                f"{TEST_BINARY_REPO}/{TEST_BINARY_PATHS[2]}": "An error occurred (403) when calling the HeadObject operation: Forbidden"
+            },
+        )
+        storage_service_mock.__init__(return_value=storage_service_mock)
+        storage_service_mock.file_exists.side_effect = [
+            True,
+            True,
+            PcpError(
+                Exception(
+                    "An error occurred (403) when calling the HeadObject operation: Forbidden"
+                )
+            ),
+        ]
+        validator = BinaryFileValidator(
+            TEST_REGION, TEST_BINARY_REPO, TEST_BINARY_PATHS
+        )
+        report = validator.validate()
+
+        self.assertEqual(report, expected_report)
+        self.assertEqual(
+            storage_service_mock.file_exists.call_count, len(TEST_BINARY_PATHS)
+        )
+
+    @patch("fbpcs.pc_pre_validation.binary_file_validator.S3StorageService")
+    def test_run_validations_unexpected_error(self, storage_service_mock: Mock) -> None:
+        expected_report = ValidationReport(
+            validation_result=ValidationResult.SUCCESS,
+            validator_name=BINARY_FILE_VALIDATOR_NAME,
+            message=f"WARNING: {BINARY_FILE_VALIDATOR_NAME} throws an unexpected error: An internal error occurred (500)",
+        )
+        storage_service_mock.__init__(return_value=storage_service_mock)
+        storage_service_mock.file_exists.side_effect = PcpError(
+            Exception("An internal error occurred (500)")
+        )
+        validator = BinaryFileValidator(
+            TEST_REGION, TEST_BINARY_REPO, TEST_BINARY_PATHS
+        )
+        report = validator.validate()
+
+        self.assertEqual(report, expected_report)
+        self.assertEqual(storage_service_mock.file_exists.call_count, 1)
