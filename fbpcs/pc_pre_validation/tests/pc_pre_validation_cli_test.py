094e422400fb85a8553c5d82107033b0eb6f337c
Mark Sliva <marksliva@fb.com>
2022-03-24 13:30:45 -0700

    print the overall validation result when successful
    Summary: And include the overall result in the error message if validation has failed.
    
    Differential Revision: D35124970
    
    fbshipit-source-id: 624dd56ae0f9a3802f4a7da3b8e24237705f469b

--- fbpcs/pc_pre_validation/tests/pc_pre_validation_cli_test.py
+++ fbpcs/pc_pre_validation/tests/pc_pre_validation_cli_test.py
@@ -109,3 +109,57 @@ class TestPCPreValidationCLI(TestCase):
         run_validators_mock.assert_called_with(
             [input_data_validator_mock(), binary_file_validator_mock()]
         )
+
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.print")
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.InputDataValidator")
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.BinaryFileValidator")
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.run_validators")
+    def test_it_includes_the_overall_result_when_failed(
+        self,
+        run_validators_mock: Mock,
+        binary_file_validator_mock: Mock,
+        input_data_validator_mock: Mock,
+        _print_mock: Mock,
+    ) -> None:
+        aggregated_result = ValidationResult.FAILED
+        aggregated_report = "Aggregated report..."
+        run_validators_mock.side_effect = [[aggregated_result, aggregated_report]]
+        argv = [
+            "--input-file-path=test-path",
+            "--cloud-provider=AWS",
+            "--region=test-region",
+        ]
+        expected_overall_result_str = (
+            f"Overall Validation Result: {aggregated_result.value}"
+        )
+
+        with self.assertRaisesRegex(Exception, expected_overall_result_str):
+            validation_cli.main(argv)
+
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.print")
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.InputDataValidator")
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.BinaryFileValidator")
+    @patch("fbpcs.pc_pre_validation.pc_pre_validation_cli.run_validators")
+    def test_it_includes_the_overall_result_when_success(
+        self,
+        run_validators_mock: Mock,
+        binary_file_validator_mock: Mock,
+        input_data_validator_mock: Mock,
+        print_mock: Mock,
+    ) -> None:
+        aggregated_result = ValidationResult.SUCCESS
+        aggregated_report = "Aggregated report..."
+        run_validators_mock.side_effect = [[aggregated_result, aggregated_report]]
+        argv = [
+            "--input-file-path=test-path",
+            "--cloud-provider=AWS",
+            "--region=test-region",
+        ]
+        expected_overall_result_str = (
+            f"Overall Validation Result: {aggregated_result.value}"
+        )
+
+        validation_cli.main(argv)
+
+        print_str = str(print_mock.call_args[0])
+        self.assertRegex(print_str, expected_overall_result_str)
