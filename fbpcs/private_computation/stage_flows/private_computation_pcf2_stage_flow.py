0eee7a9c19a622a9e283e4924ff70b8708318dff
Jacob Rodal <jrodal@fb.com>
2022-03-31 09:41:36 -0700

    set binary version (#772)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/772
    
    ## What
    
    * set binary version in input_data_validation stage
    * use RunBinaryBaseService instead of calling one docker service directly
    
    ## Why
    
    As ajinkya-ghonge pointed out, we aren't setting the binary version for the input data validation stage, which is going to cause a lot of issues during build and release.
    
    Reviewed By: taoyong-ty
    
    Differential Revision: D35271378
    
    fbshipit-source-id: a46be4594071a3cbc9ce5f4b0969416c4f46f900

--- fbpcs/private_computation/stage_flows/private_computation_pcf2_stage_flow.py
+++ fbpcs/private_computation/stage_flows/private_computation_pcf2_stage_flow.py
@@ -139,7 +139,9 @@ class PrivateComputationPCF2StageFlow(PrivateComputationBaseStageFlow):
             return DummyStageService()
         elif self is self.INPUT_DATA_VALIDATION:
             return InputDataValidationStageService(
-                args.pc_validator_config, args.onedocker_svc
+                args.pc_validator_config,
+                args.onedocker_svc,
+                args.onedocker_binary_config_map,
             )
         elif self is self.ID_MATCH:
             return IdMatchStageService(
