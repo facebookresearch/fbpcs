4271efdeea717dcbaae94db13ee9e2ffe08ee103
Pyre Bot Jr <>
2022-04-27 17:02:22 -0700

    suppress errors in `fbcode/fbpcs` - batch 1
    Differential Revision: D35967961
    
    fbshipit-source-id: 39e749418d63a83b8e25794d35bc8a94386d7322

--- fbpcs/stage_flow/test/dummy_stage_flow.py
+++ fbpcs/stage_flow/test/dummy_stage_flow.py
@@ -12,23 +12,14 @@ from fbpcs.stage_flow.stage_flow import StageFlow, StageFlowData
 
 
 class DummyStageFlowStatus(Enum):
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_1_STARTED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_1_COMPLETED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_1_FAILED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_2_STARTED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_2_COMPLETED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_2_FAILED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_3_STARTED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_3_COMPLETED = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     STAGE_3_FAILED = auto()
 
 
