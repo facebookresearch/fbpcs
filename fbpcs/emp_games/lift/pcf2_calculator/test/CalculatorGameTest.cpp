1bdd40da745f5041ed2ff1a5aeab4e65c9c6e0dc
chualynn <10423511+chualynn@users.noreply.github.com>
2022-05-06 16:44:14 -0700

    Add CalculatorGame correctness tests
    Summary: We adfd correctness tests for the CalculatorGame using the tests cases from https://docs.google.com/spreadsheets/d/1xsIl33YDU_y7lSVD5gsiGV-ubl2x9TDYiTVRyR4dS74/edit?usp=sharing and with different schedulers.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D36025186
    
    fbshipit-source-id: f4d60b1ef51ae7f0ac3277c7b9d674bb6e147790

---
+++ fbpcs/emp_games/lift/pcf2_calculator/test/CalculatorGameTest.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <filesystem>
+#include <memory>
+#include <string>
+#include <thread>
+#include <unordered_map>
+
+#include <gtest/gtest.h>
+#include "folly/Random.h"
+
+#include "fbpcf/engine/communication/InMemoryPartyCommunicationAgentFactory.h"
+#include "fbpcf/engine/communication/test/AgentFactoryCreationHelper.h"
+#include "fbpcf/io/FileManagerUtil.h"
+#include "fbpcf/test/TestHelper.h"
+#include "fbpcs/emp_games/common/Csv.h"
+#include "fbpcs/emp_games/common/TestUtil.h"
+#include "fbpcs/emp_games/common/test/TestUtils.h"
+#include "fbpcs/emp_games/lift/pcf2_calculator/CalculatorGame.h"
+
+namespace private_lift {
+
+template <int schedulerId>
+GroupedLiftMetrics runCalculatorGame(
+    int myId,
+    CalculatorGameConfig config,
+    std::shared_ptr<
+        fbpcf::engine::communication::IPartyCommunicationAgentFactory> factory,
+    fbpcf::SchedulerCreator schedulerCreator) {
+  auto scheduler = schedulerCreator(myId, *factory);
+  auto game = std::make_unique<CalculatorGame<schedulerId>>(
+      myId, std::move(scheduler), std::move(factory));
+  auto output = game->play(config);
+  return GroupedLiftMetrics::fromJson(output);
+}
+
+class CalculatorGameTestFixture
+    : public ::testing::TestWithParam<common::SchedulerType> {
+ public:
+  CalculatorGameConfig getInputData(
+      const std::filesystem::path& inputPath,
+      int numConversionsPerUser) {
+    int64_t epoch = 1546300800;
+    InputData inputData{
+        inputPath,
+        InputData::LiftMPCType::Standard,
+        InputData::LiftGranularityType::Conversion,
+        epoch,
+        numConversionsPerUser};
+    CalculatorGameConfig config = {inputData, true, numConversionsPerUser};
+    return config;
+  }
+
+ protected:
+  GroupedLiftMetrics runGameWithScheduler(
+      fbpcf::SchedulerCreator schedulerCreator,
+      CalculatorGameConfig publisherConfig,
+      CalculatorGameConfig partnerConfig) {
+    auto factories = fbpcf::engine::communication::getInMemoryAgentFactory(2);
+    auto future0 = std::async(
+        runCalculatorGame<0>,
+        0,
+        publisherConfig,
+        std::move(factories[0]),
+        schedulerCreator);
+    auto future1 = std::async(
+        runCalculatorGame<1>,
+        1,
+        partnerConfig,
+        std::move(factories[1]),
+        schedulerCreator);
+
+    GroupedLiftMetrics resFirst = future0.get();
+    GroupedLiftMetrics resSecond = future1.get();
+
+    return resFirst ^ resSecond;
+  }
+};
+
+TEST_P(CalculatorGameTestFixture, TestCorrectness) {
+  int numConversionsPerUser = 2;
+  std::string baseDir =
+      private_measurement::test_util::getBaseDirFromPath(__FILE__);
+  CalculatorGameConfig publisherConfig =
+      CalculatorGameTestFixture::getInputData(
+          baseDir + "../sample_input/publisher_unittest3.csv",
+          numConversionsPerUser);
+  CalculatorGameConfig partnerConfig = CalculatorGameTestFixture::getInputData(
+      baseDir + "../sample_input/partner_2_convs_unittest.csv",
+      numConversionsPerUser);
+  std::string expectedOutputFilename =
+      baseDir + "../sample_input/correctness_output.json";
+
+  // Run calculator game with input files
+  const bool unsafe = true;
+  auto schedulerType = GetParam();
+  fbpcf::SchedulerCreator schedulerCreator =
+      fbpcf::getSchedulerCreator<unsafe>(schedulerType);
+  auto res = runGameWithScheduler(
+      schedulerCreator, std::move(publisherConfig), std::move(partnerConfig));
+
+  // Read expected output from file
+  auto expectedRes =
+      GroupedLiftMetrics::fromJson(fbpcf::io::read(expectedOutputFilename));
+
+  EXPECT_EQ(expectedRes, res);
+}
+
+// Test calculator game with different schedulers
+INSTANTIATE_TEST_SUITE_P(
+    CalculatorGameTest,
+    CalculatorGameTestFixture,
+    ::testing::Values(
+        common::SchedulerType::NetworkPlaintext,
+        common::SchedulerType::Eager,
+        common::SchedulerType::Lazy),
+    [](const testing::TestParamInfo<CalculatorGameTestFixture::ParamType>&
+           info) {
+      auto schedulerType = info.param;
+      return getSchedulerName(schedulerType);
+    });
+
+} // namespace private_lift
