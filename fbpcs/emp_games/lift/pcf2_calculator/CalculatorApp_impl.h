9a3ee8b1a744d2f473a21c72aee3f60a8f065811
Lynn Chua <chualynn@fb.com>
2022-05-09 13:22:24 -0700

    Add CalculatorApp (#891)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/891
    
    We add a CalculatorApp class, which takes input and output file paths and runs the CalculatorGame with a lazy scheduler. We also add correctness tests using socket communication.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D36079612
    
    fbshipit-source-id: dce24bc4b9aea86338422c3e6cc195981f87b116

---
+++ fbpcs/emp_games/lift/pcf2_calculator/CalculatorApp_impl.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <vector>
+
+namespace private_lift {
+
+template <int schedulerId>
+void CalculatorApp<schedulerId>::run() {
+  // Run calculator game sequentially on numFiles files, starting from
+  // startFileIndex
+  auto scheduler = fbpcf::scheduler::createLazySchedulerWithRealEngine(
+      party_, *communicationAgentFactory_);
+  CalculatorGame<schedulerId> game{
+      party_, std::move(scheduler), std::move(communicationAgentFactory_)};
+
+  for (size_t i = startFileIndex_; i < startFileIndex_ + numFiles_; ++i) {
+    try {
+      CHECK_LT(i, inputPaths_.size()) << "File index exceeds number of files.";
+      CalculatorGameConfig config = getInputData(inputPaths_.at(i));
+      auto numRows = config.inputData.getNumRows();
+      XLOG(INFO) << "Have " << numRows << " values in inputData.";
+      auto output = game.play(config);
+      XLOG(INFO) << "done calculating";
+      putOutputData(output, outputPaths_.at(i));
+    } catch (const std::exception& e) {
+      XLOGF(
+          ERR,
+          "Error: Exception caught in CalculatorApp run.\n \t error msg: {} \n \t input shard: {}.",
+          e.what(),
+          inputPaths_.at(i));
+      std::exit(1);
+    }
+  }
+
+  auto gateStatistics =
+      fbpcf::scheduler::SchedulerKeeper<schedulerId>::getGateStatistics();
+  XLOGF(
+      INFO,
+      "Non-free gate count = {}, Free gate count = {}",
+      gateStatistics.first,
+      gateStatistics.second);
+
+  auto trafficStatistics =
+      fbpcf::scheduler::SchedulerKeeper<schedulerId>::getTrafficStatistics();
+  XLOGF(
+      INFO,
+      "Sent network traffic = {}, Received network traffic = {}",
+      trafficStatistics.first,
+      trafficStatistics.second);
+
+  schedulerStatistics_.nonFreeGates = gateStatistics.first;
+  schedulerStatistics_.freeGates = gateStatistics.second;
+  schedulerStatistics_.sentNetwork = trafficStatistics.first;
+  schedulerStatistics_.receivedNetwork = trafficStatistics.second;
+};
+
+template <int schedulerId>
+CalculatorGameConfig CalculatorApp<schedulerId>::getInputData(
+    const std::string& inputPath) {
+  XLOG(INFO) << "Parsing input from " << inputPath;
+
+  InputData inputData{
+      inputPath,
+      InputData::LiftMPCType::Standard,
+      InputData::LiftGranularityType::Conversion,
+      epoch_,
+      numConversionsPerUser_};
+  CalculatorGameConfig config = {inputData, true, numConversionsPerUser_};
+  return config;
+}
+
+template <int schedulerId>
+void CalculatorApp<schedulerId>::putOutputData(
+    const std::string& output,
+    const std::string& outputPath) {
+  XLOG(INFO) << "putting out data...";
+  fbpcf::io::write(outputPath, output);
+}
+} // namespace private_lift
---
+++ fbpcs/emp_games/lift/pcf2_calculator/test/CalculatorAppTest.cpp
@@ -0,0 +1,223 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <filesystem>
+#include <memory>
+#include <string>
+#include <thread>
+#include <unordered_map>
+
+#include <gtest/gtest.h>
+#include "folly/Random.h"
+
+#include "fbpcf/engine/communication/SocketPartyCommunicationAgentFactory.h"
+#include "fbpcf/engine/communication/test/AgentFactoryCreationHelper.h"
+#include "fbpcf/engine/communication/test/TlsCommunicationUtils.h"
+#include "fbpcf/io/FileManagerUtil.h"
+#include "fbpcs/emp_games/common/Csv.h"
+#include "fbpcs/emp_games/common/TestUtil.h"
+#include "fbpcs/emp_games/common/test/TestUtils.h"
+#include "fbpcs/emp_games/lift/pcf2_calculator/CalculatorApp.h"
+#include "fbpcs/emp_games/lift/pcf2_calculator/test/common/GenFakeData.h"
+#include "fbpcs/emp_games/lift/pcf2_calculator/test/common/LiftCalculator.h"
+
+namespace private_lift {
+
+template <int schedulerId>
+void runCalculatorApp(
+    int myId,
+    const int numConversionsPerUser,
+    const int epoch,
+    const std::string& inputPath,
+    const std::string& outputPath,
+    uint16_t port,
+    std::string serverIp,
+    std::string tlsDir,
+    bool useTls) {
+  std::map<
+      int,
+      fbpcf::engine::communication::SocketPartyCommunicationAgentFactory::
+          PartyInfo>
+      partyInfos({{0, {serverIp, port}}, {1, {serverIp, port}}});
+
+  auto communicationAgentFactory = std::make_unique<
+      fbpcf::engine::communication::SocketPartyCommunicationAgentFactory>(
+      myId, partyInfos, useTls, tlsDir);
+
+  auto app = std::make_unique<CalculatorApp<schedulerId>>(
+      myId,
+      std::move(communicationAgentFactory),
+      numConversionsPerUser,
+      epoch,
+      std::vector<std::string>{inputPath},
+      std::vector<std::string>{outputPath});
+  app->run();
+}
+
+class CalculatorAppTestFixture : public ::testing::TestWithParam<bool> {
+ protected:
+  std::string publisherInputPath_;
+  std::string partnerInputPath_;
+  std::string publisherOutputPath_;
+  std::string partnerOutputPath_;
+  uint16_t port_;
+  std::string serverIp_;
+  std::string tlsDir_;
+
+  void SetUp() override {
+    std::string tempDir = std::filesystem::temp_directory_path();
+    publisherInputPath_ = folly::sformat(
+        "{}/publisher_{}.csv", tempDir, folly::Random::secureRand64());
+    partnerInputPath_ = folly::sformat(
+        "{}/partner_{}.csv", tempDir, folly::Random::secureRand64());
+    publisherOutputPath_ = folly::sformat(
+        "{}/res_publisher_{}", tempDir, folly::Random::secureRand64());
+    partnerOutputPath_ = folly::sformat(
+        "{}/res_partner_{}", tempDir, folly::Random::secureRand64());
+
+    port_ = 5000 + folly::Random::rand32() % 1000;
+    serverIp_ = "127.0.0.1";
+    tlsDir_ = fbpcf::engine::communication::setUpTlsFiles();
+  }
+
+  void TearDown() override {
+    std::filesystem::remove(publisherInputPath_);
+    std::filesystem::remove(partnerInputPath_);
+    std::filesystem::remove(publisherOutputPath_);
+    std::filesystem::remove(partnerOutputPath_);
+    fbpcf::engine::communication::deleteTlsFiles(tlsDir_);
+  }
+
+  GroupedLiftMetrics runTest(
+      const std::string& publisherInputPath,
+      const std::string& partnerInputPath,
+      const std::string& publisherOutputPath,
+      const std::string& partnerOutputPath,
+      const int numConversionsPerUser,
+      bool useTls) {
+    int epoch = 1546300800;
+    auto future0 = std::async(
+        runCalculatorApp<0>,
+        0,
+        numConversionsPerUser,
+        epoch,
+        publisherInputPath,
+        publisherOutputPath,
+        port_,
+        serverIp_,
+        tlsDir_,
+        useTls);
+
+    auto future1 = std::async(
+        runCalculatorApp<1>,
+        1,
+        numConversionsPerUser,
+        epoch,
+        partnerInputPath,
+        partnerOutputPath,
+        port_,
+        serverIp_,
+        tlsDir_,
+        useTls);
+
+    future0.get();
+    future1.get();
+    auto publisherResult =
+        GroupedLiftMetrics::fromJson(fbpcf::io::read(publisherOutputPath));
+    auto partnerResult =
+        GroupedLiftMetrics::fromJson(fbpcf::io::read(partnerOutputPath));
+
+    return publisherResult ^ partnerResult;
+  }
+};
+
+TEST_P(CalculatorAppTestFixture, TestCorrectness) {
+  int numConversionsPerUser = 2;
+  std::string baseDir =
+      private_measurement::test_util::getBaseDirFromPath(__FILE__);
+  std::string publisherInputPath =
+      baseDir + "../sample_input/publisher_unittest3.csv";
+  std::string partnerInputPath =
+      baseDir + "../sample_input/partner_2_convs_unittest.csv";
+  std::string expectedOutputPath =
+      baseDir + "../sample_input/correctness_output.json";
+  bool useTls = GetParam();
+
+  auto result = runTest(
+      publisherInputPath,
+      partnerInputPath,
+      publisherOutputPath_,
+      partnerOutputPath_,
+      numConversionsPerUser,
+      useTls);
+
+  auto expectedResult =
+      GroupedLiftMetrics::fromJson(fbpcf::io::read(expectedOutputPath));
+
+  EXPECT_EQ(expectedResult, result);
+}
+
+TEST_P(CalculatorAppTestFixture, TestCorrectnessRandomInput) {
+  // Generate test input files with random data
+  int numConversionsPerUser = 25;
+  GenFakeData testDataGenerator;
+  LiftFakeDataParams params;
+  params.setNumRows(15)
+      .setOpportunityRate(0.5)
+      .setTestRate(0.5)
+      .setPurchaseRate(0.5)
+      .setIncrementalityRate(0.0)
+      .setEpoch(1546300800);
+  testDataGenerator.genFakePublisherInputFile(publisherInputPath_, params);
+  params.setNumConversions(numConversionsPerUser).setOmitValuesColumn(false);
+  testDataGenerator.genFakePartnerInputFile(partnerInputPath_, params);
+
+  // Run calculator app with test input
+  bool useTls = GetParam();
+  auto res = runTest(
+      publisherInputPath_,
+      partnerInputPath_,
+      publisherOutputPath_,
+      partnerOutputPath_,
+      numConversionsPerUser,
+      useTls);
+
+  // Calculate expected results with simple lift calculator
+  LiftCalculator liftCalculator;
+  std::ifstream inFilePublisher{publisherInputPath_};
+  std::ifstream inFilePartner{partnerInputPath_};
+  int32_t tsOffset = 10;
+  std::string linePublisher;
+  std::string linePartner;
+  getline(inFilePublisher, linePublisher);
+  getline(inFilePartner, linePartner);
+  auto headerPublisher =
+      private_measurement::csv::splitByComma(linePublisher, false);
+  auto headerPartner =
+      private_measurement::csv::splitByComma(linePartner, false);
+  std::unordered_map<std::string, int> colNameToIndex =
+      liftCalculator.mapColToIndex(headerPublisher, headerPartner);
+  OutputMetricsData computedResult = liftCalculator.compute(
+      inFilePublisher, inFilePartner, colNameToIndex, tsOffset, false);
+  GroupedLiftMetrics expectedRes;
+  expectedRes.metrics = computedResult.toLiftMetrics();
+
+  EXPECT_EQ(expectedRes, res);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CalculatorAppTest,
+    CalculatorAppTestFixture,
+    ::testing::Bool(),
+    [](const testing::TestParamInfo<CalculatorAppTestFixture::ParamType>&
+           info) {
+      std::string tls = info.param ? "True" : "False";
+      std::string name = "TLS_" + tls;
+      return name;
+    });
+
+} // namespace private_lift
