320fa4dd30addeed2d36def064a9c1ddfdee7492
Ankush Singh <ankushksingh@fb.com>
2022-05-13 09:20:00 -0700

    Creating folder structure to store container logs (#885)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/885
    
    Creating folder structure needed to store the container logs in AWS S3 bucket
    
    This diff is part of the series of diffs which will download logs from partner side AWS account. Following is the list of diff details:
    diff #1 : [D36136606 (https://github.com/facebookresearch/fbpcs/commit/9ba40a9ab1b8c7ecd9ff019376a7d29b44e242f8)]  Fetched container logs from cloudwatch
    diff #2 : [this diff] Creates folder structure in S3 to store fetched logs
    diff #3 : [upcoming] Exports logs from cloudwatch to the folders created in S3
    diff #4 : [upcoming] Let's user download the S3 folders which contains all the logs
    
    More details of the design can be found here: https://docs.google.com/document/d/1PVo20EgB5pD5uATa6aF1Bv56NIX0rWxp2w-5ORCAQDE/edit?usp=sharing
    
    Logging service design: https://docs.google.com/document/d/1vze4JalYZfpLxtXVpQJToB2Tb_LNOeuxJJSAGArZY0k/edit#heading=h.3b1n6r5gq4y0
    
    Differential Revision: D36193153
    
    fbshipit-source-id: f7aef7b2da5e27ec4f2e30612c990ed3d2331e1c

--- fbpcs/infra/logging_service/download_logs/download_logs.py
+++ fbpcs/infra/logging_service/download_logs/download_logs.py
@@ -19,11 +19,12 @@ class AWSContainerLogs(AwsCloud):
 
     LOG_GROUP = "/{}/{}"
     LOG_STREAM = "{}/{}/{}"
-    MAX_LOG_EVENT_LIMIT = 9999
     ARN_PARSE_LENGTH = 6
+    S3_LOGGING_FOLDER = "logging"
 
     def __init__(
         self,
+        tag_name: str,
         aws_access_key_id: str = None,
         aws_secret_access_key: str = None,
         aws_region: str = None,
@@ -33,33 +34,23 @@ class AWSContainerLogs(AwsCloud):
             aws_secret_access_key=aws_secret_access_key,
             aws_region=aws_region,
         )
+        self.tag_name = tag_name
 
-    def get_cloudwatch_logs(self, container_arn: str) -> List[str]:
-        """ """
+    def get_cloudwatch_logs(
+        self, log_group_name: str, log_stream_name: str, container_arn: str = None
+    ) -> List[str]:
+        """
+        Fetches cloudwatch logs from the AWS account for a given log group and log stream
+        Args:
+            log_group_name (string): Name of the log group
+            log_stream_name (string): Name of the log stream
+            container_arn (string): Container arn to get log group and log stream names
+        Returns:
+            List[string]
+        """
 
         messages = []
         message_events = []
-
-        self.log.info(
-            "Getting service name, container name and container ID from continer arn"
-        )
-        service_name, container_name, container_id = self._parse_container_arn(
-            container_arn=container_arn
-        )
-        log_group_name = self.LOG_GROUP.format(service_name, container_name)
-        log_stream_name = self.LOG_STREAM.format(
-            service_name, container_name, container_id
-        )
-
-        if not self._verify_log_group(log_group_name=log_group_name):
-            self.log.error(f"Couldn't find log group {log_group_name} in AWS account.")
-
-        if not self._verify_log_stream(
-            log_group_name=log_group_name, log_stream_name=log_stream_name
-        ):
-            self.log.error(
-                f"Couldn't find log stream {log_stream_name} in log group {log_group_name}"
-            )
 
         try:
             self.log.info(
@@ -91,23 +82,23 @@ class AWSContainerLogs(AwsCloud):
         except ClientError as error:
             error_code = error.response.get("Error", {}).get("Code")
             if error_code == "InvalidParameterException":
-                self.log.error(
-                    f"Couldn't fetch the log events for log group {log_group_name} and log stream {log_stream_name}."
-                    f"Please check if the container arn {container_arn} is correct."
-                    f"{error}"
+                error_message = (
+                    f"Couldn't fetch the log events for log group {log_group_name} and log stream {log_stream_name}.\n"
+                    f"Please check if the container arn {container_arn} is correct.\n"
+                    f"{error}\n"
                 )
             elif error_code == "ResourceNotFoundException":
-                self.log.error(
-                    f"Couldn't find log group name {log_group_name} and log stream {log_stream_name} in AWS account."
-                    f"Please check if the container arn {container_arn} is correct."
-                    f"{error}"
+                error_message = (
+                    f"Couldn't find log group name {log_group_name} and log stream {log_stream_name} in AWS account.\n"
+                    f"Please check if the container arn {container_arn} is correct.\n"
+                    f"{error}\n"
                 )
             else:
-                self.log.error(
-                    f"Unexpected error occured in fetching the log event log group {log_group_name} and log stream {log_stream_name}"
-                    f"{error}"
+                error_message = (
+                    f"Unexpected error occured in fetching the log event log group {log_group_name} and log stream {log_stream_name}\n"
+                    f"{error}\n"
                 )
-            raise Exception("Please resolve the error.")
+            raise Exception(f"{error_message}")
 
         return messages
 
@@ -203,22 +194,22 @@ class AWSContainerLogs(AwsCloud):
         except ClientError as error:
             error_code = error.response.get("Error", {}).get("Code")
             if error_code == "InvalidParameterException":
-                self.log.error(
-                    f"Wrong parameters passed to the API. Please check container arn."
-                    f"Couldn't find log group {log_group_name}"
-                    f"{error}"
+                error_message = (
+                    f"Wrong parameters passed to the API. Please check container arn.\n"
+                    f"Couldn't find log group {log_group_name}\n"
+                    f"{error}\n"
                 )
             elif error_code == "ResourceNotFoundException":
-                self.log.error(
-                    f"Couldn't find log group name {log_group_name} in AWS account."
-                    f"{error}"
+                error_message = (
+                    f"Couldn't find log group name {log_group_name} in AWS account.\n"
+                    f"{error}\n"
                 )
             else:
-                self.log.error(
-                    f"Unexpected error occurred in fetching log group name {log_group_name}."
-                    f"{error}"
+                error_message = (
+                    f"Unexpected error occurred in fetching log group name {log_group_name}.\n"
+                    f"{error}\n"
                 )
-            raise Exception("Please resolve the error.")
+            raise Exception(f"{error_message}")
 
         return len(response.get("logGroups", [])) == 1
 
@@ -242,21 +233,149 @@ class AWSContainerLogs(AwsCloud):
         except ClientError as error:
             error_code = error.response.get("Error", {}).get("Code")
             if error_code == "InvalidParameterException":
-                self.log.error(
-                    f"Wrong parameters passed to the API. Please check container arn."
-                    f"Couldn't find log stream name {log_stream_name} in log group {log_group_name}"
-                    f"{error}"
+                error_message = (
+                    f"Wrong parameters passed to the API. Please check container arn.\n"
+                    f"Couldn't find log stream name {log_stream_name} in log group {log_group_name}\n"
+                    f"{error}\n"
                 )
             elif error_code == "ResourceNotFoundException":
-                self.log.error(
-                    f"Couldn't find log group name {log_group_name} or log stream {log_stream_name} in AWS account"
-                    f"{error}"
+                error_message = (
+                    f"Couldn't find log group name {log_group_name} or log stream {log_stream_name} in AWS account\n"
+                    f"{error}\n"
                 )
             else:
-                self.log.error(
-                    f"Unexpected error occurred in finding log stream name {log_stream_name} in log grpup {log_group_name}"
-                    f"{error}"
+                error_message = (
+                    f"Unexpected error occurred in finding log stream name {log_stream_name} in log grpup {log_group_name}\n"
+                    f"{error}\n"
                 )
-            raise Exception("Please resolve the error.")
+            raise Exception(f"{error_message}")
 
         return len(response.get("logStreams", [])) == 1
+
+    def create_s3_folder(self, bucket_name: str, folder_name: str) -> None:
+        """
+        Creates a folder (Key in boto3 terms) inside the s3 bucket
+        Args:
+            bucket_name (string): Name of the s3 bucket where logs will be stored
+            folder_name (string): Name of folder for which is to be created
+        Returns:
+            None
+        """
+
+        response = self.s3_client.put_object(Bucket=bucket_name, Key=folder_name)
+
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            self.log.info(
+                f"Successfully created folder {folder_name} in S3 bucket {bucket_name}"
+            )
+        else:
+            error_message = (
+                f"Failed to create folder {folder_name} in S3 bucket {bucket_name}\n"
+            )
+            raise Exception(f"{error_message}")
+
+    def ensure_folder_exists(self, bucket_name: str, folder_name: str) -> bool:
+        """
+        Verify if the folder is present in s3 bucket
+        Args:
+            bucket_name (string): Name of the s3 bucket where logs will be stored
+            folder_name (string): Name of folder for which verification is needed
+        Returns:
+            Boolean
+        """
+
+        response = {}
+
+        try:
+            response = self.s3_client.list_objects_v2(
+                Bucket=bucket_name, Prefix=folder_name
+            )
+        except ClientError as error:
+            error_message = (
+                f"folder {folder_name} in S3 bucket {bucket_name} doesn't exist\n"
+                f"{error}\n"
+            )
+            if error.response.get("Error", {}).get("Code") == "NoSuchBucket":
+                error_message = (
+                    f"Couldn't find folder {folder_name} in S3 bucket {bucket_name}\n"
+                    f"{error}\n"
+                )
+            raise Exception(f"{error_message}")
+
+        return "Contents" in response
+
+    def download_logs(self, s3_bucket_name: str, container_arn: str) -> None:
+        """
+        Umbrella function to call other functions for end to end functionality
+
+        Folder structure
+
+        [S3 Bucket]
+            -> [folder name "logging"]
+                -> [folder name in format {tag_name}]
+                    -> [exported logs from each container]
+
+        Args:
+            s3_bucket_name (string): Name of the s3 bucket where logs will be stored
+            container_arn (string): Container arn to get log group and log stream names
+        Returns:
+            None
+        """
+
+        # verify s3 bucket
+        try:
+            self.s3_client.head_bucket(Bucket=s3_bucket_name)
+        except ClientError as error:
+            if error.response.get("Error", {}).get("Code") == "NoSuchBucket":
+                error_message = f"Couldn't find bucket in the AWS account.\n{error}\n"
+            else:
+                error_message = "Couldn't find the S3 bucket in AWS account. Please use the right AWS S3 bucket name\n"
+            raise Exception(f"{error_message}")
+
+        # create logging folder
+        if not self.ensure_folder_exists(
+            bucket_name=s3_bucket_name, folder_name=f"{self.S3_LOGGING_FOLDER}/"
+        ):
+            self.create_s3_folder(
+                bucket_name=s3_bucket_name,
+                folder_name=f"{self.S3_LOGGING_FOLDER}/",
+            )
+
+        # create folder with tag_name passed
+        tag_name_folder = self.tag_name
+        if not self.ensure_folder_exists(
+            bucket_name=s3_bucket_name,
+            folder_name=f"{self.S3_LOGGING_FOLDER}/{tag_name_folder}/",
+        ):
+            self.create_s3_folder(
+                bucket_name=s3_bucket_name,
+                folder_name=f"{self.S3_LOGGING_FOLDER}/{tag_name_folder}/",
+            )
+
+        # fetch logs
+        self.log.info(
+            "Getting service name, container name and container ID from continer arn"
+        )
+        service_name, container_name, container_id = self._parse_container_arn(
+            container_arn=container_arn
+        )
+        log_group_name = self.LOG_GROUP.format(service_name, container_name)
+        log_stream_name = self.LOG_STREAM.format(
+            service_name, container_name, container_id
+        )
+
+        if not self._verify_log_group(log_group_name=log_group_name):
+            raise Exception(f"Couldn't find log group {log_group_name} in AWS account.")
+
+        if not self._verify_log_stream(
+            log_group_name=log_group_name, log_stream_name=log_stream_name
+        ):
+            raise Exception(
+                f"Couldn't find log stream {log_stream_name} in log group {log_group_name}"
+            )
+
+        self.get_cloudwatch_logs(
+            log_group_name=log_group_name,
+            log_stream_name=log_stream_name,
+            container_arn=container_arn,
+        )
