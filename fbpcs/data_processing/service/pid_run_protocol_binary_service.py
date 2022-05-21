48da9dfbe0913cc94a8220c4dca8dc2c3a49ff48
Mao Ye <deborahye@fb.com>
2022-05-20 17:30:52 -0700

    Create PIDRunProtocolBinaryService (#919)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/919
    
    Description:
    This is the second step to migrate PIDProtocolRunStage to StageFlow service. PIDRunProtocolBinaryService is a RunBinaryBaseService, which takes care of building arguments to pass to binary, which will be used to build binaries and to obtain a binary path.
    * The function build_args() which is migrated from _gen_command_args in pid_run_protocol_stage.py
    
    Reviewed By: joe1234wu
    
    Differential Revision: D36470148
    
    fbshipit-source-id: f11eb84e56def0388266be5faca878b60a887a0d

---
+++ fbpcs/data_processing/service/pid_run_protocol_binary_service.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Optional
+
+from fbpcs.onedocker_binary_names import OneDockerBinaryNames
+
+from fbpcs.pid.entity.pid_instance import PIDProtocol
+from fbpcs.private_computation.entity.private_computation_instance import (
+    PrivateComputationRole,
+)
+from fbpcs.private_computation.service.run_binary_base_service import (
+    RunBinaryBaseService,
+)
+
+
+class PIDRunProtocolBinaryService(RunBinaryBaseService):
+    @staticmethod
+    def build_args(
+        input_path: str,
+        output_path: str,
+        port: int,
+        use_row_numbers: bool = False,
+        server_hostname: Optional[str] = None,
+        metric_path: Optional[str] = None,
+    ) -> str:
+
+        cmd_ls = []
+
+        if server_hostname:
+            cmd_ls.append(f"--company {server_hostname}:{port}")
+        else:
+            cmd_ls.append(f"--host 0.0.0.0:{port}")
+
+        cmd_ls.extend(
+            [
+                f"--input {input_path}",
+                f"--output {output_path}",
+            ]
+        )
+
+        if metric_path is not None:
+            cmd_ls.append(f"--metric-path {metric_path}")
+
+        cmd_ls.append("--no-tls")
+
+        if use_row_numbers:
+            cmd_ls.append("--use-row-numbers")
+
+        return " ".join(cmd_ls)
+
+    @staticmethod
+    def get_binary_name(protocol: PIDProtocol, pc_role: PrivateComputationRole):
+        if pc_role is PrivateComputationRole.PARTNER:
+            binary = OneDockerBinaryNames.PID_SERVER.value
+            if protocol is PIDProtocol.UNION_PID_MULTIKEY:
+                binary = OneDockerBinaryNames.PID_MULTI_KEY_SERVER.value
+        elif pc_role is PrivateComputationRole.PUBLISHER:
+            binary = OneDockerBinaryNames.PID_CLIENT.value
+            if protocol is PIDProtocol.UNION_PID_MULTIKEY:
+                binary = OneDockerBinaryNames.PID_MULTI_KEY_CLIENT.value
+        else:
+            raise ValueError(f"Unsupported PrivateComputationRole passed: {pc_role}")
+        return binary
