7fb9d0394a6ce4937d6e0fb8c504116264974bae
Logan Gore <gorel@fb.com>
2022-06-02 09:17:08 -0700

    Add skeleton code to test other methods
    Differential Revision: D36813021
    
    fbshipit-source-id: 54af411e325f302481fd55b533b1e51cf0f3ffc6

--- fbpcs/infra/logging_service/download_logs/test/test_download_logs.py
+++ fbpcs/infra/logging_service/download_logs/test/test_download_logs.py
@@ -14,3 +14,51 @@ class TestDownloadLogs(unittest.TestCase):
     @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
     def test_get_cloudwatch_logs(self, mock_boto3) -> None:
         pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_parse_container_arn(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_parse_log_events(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_get_container_name_id(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_verify_log_group(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_verify_log_stream(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_create_s3_folder(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_ensure_folder_exists(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_get_s3_folder_contents(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_get_s3_folder_path(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_get_files_to_download_logs(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_download_logs(self, mock_boto3) -> None:
+        pass
+
+    @patch("fbpcs.infra.logging_service.download_logs.cloud.aws_cloud.boto3")
+    def test_upload_logs_to_s3_from_cloudwatch(self, mock_boto3) -> None:
+        pass
