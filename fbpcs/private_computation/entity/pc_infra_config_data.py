3d0ba7e783e0e99c983e8ac37855dc0ce67270bb
Eve Chen <evechenc@fb.com>
2022-06-06 11:40:46 -0700

    Add pc_infra_config_data.py (#970)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/970
    
    This is the start of Task: Refactoring config.yml
    
    pc_infra_config_data.py will be used to stored default arguments from config.yml.
    
    Differential Revision: https://www.internalfb.com/diff/D36828606?entry_point=27
    
    fbshipit-source-id: ce0b733e3f4fa21a4296307916a8f1f8ae841182

---
+++ fbpcs/private_computation/entity/pc_infra_config_data.py
@@ -0,0 +1,57 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Set
+
+
+@dataclass(frozen=True)
+class PrivateComputationInfraConfigData:
+    cls_name: str
+    args: Set[str]
+
+
+class PrivateComputationInfraConfigInfo(Enum):
+
+    CONTAINER_SERVICE = PrivateComputationInfraConfigData(
+        "fbpcp.service.container_aws.AWSContainerService",
+        {"region", "cluster", "subnets"},
+    )
+
+    PC_INSTANCE_REPO = PrivateComputationInfraConfigData(
+        "fbpcs.private_computation.repository.private_computation_instance_local.LocalPrivateComputationInstanceRepository",
+        {"base_dir"},
+    )
+
+    STORAGE_SERVICE = PrivateComputationInfraConfigData(
+        "fbpcp.service.storage_s3.S3StorageService",
+        {"region"},
+    )
+
+    PC_VALIDATOR_CONFIG = PrivateComputationInfraConfigData(
+        "fbpcs.private_computation.entity.pc_validator_config.PCValidatorConfig",
+        {"region"},
+    )
+
+    PID_INSTANCE_REPO = PrivateComputationInfraConfigData(
+        "fbpcs.pid.repository.pid_instance_local.LocalPIDInstanceRepository",
+        {"base_dir"},
+    )
+
+    MPC_GAME_SERVICE = PrivateComputationInfraConfigData(
+        "fbpcp.service.mpc_game.MPCGameService",
+        set(),
+    )
+
+    PC_GAME_REPO = PrivateComputationInfraConfigData(
+        "fbpcs.private_computation.repository.private_computation_game.PrivateComputationGameRepository",
+        set(),
+    )
+
+    MPC_INSTANCE_REPO = PrivateComputationInfraConfigData(
+        "fbpcs.common.repository.mpc_instance_local.LocalMPCInstanceRepository",
+        {"base_dir"},
+    )
