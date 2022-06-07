0ae1756cecd9f2c01d21ae2d79240bab98e292e0
Logan Gore <gorel@fb.com>
2022-06-07 13:26:38 -0700

    Make deployment helper pyre-strict
    Differential Revision: D36983092
    
    fbshipit-source-id: f9b053f4618f591bc88a7c72893f094eca3e70a4

--- fbpcs/infra/cloud_bridge/deployment_helper/aws/aws_deployment_helper_tool.py
+++ fbpcs/infra/cloud_bridge/deployment_helper/aws/aws_deployment_helper_tool.py
@@ -3,14 +3,18 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 import argparse
 
-from .aws_deployment_helper import AwsDeploymentHelper
-from .policy_params import PolicyParams
+from fbpcs.infra.cloud_bridge.deployment_helper.aws.aws_deployment_helper import (
+    AwsDeploymentHelper,
+)
+from fbpcs.infra.cloud_bridge.deployment_helper.aws.policy_params import PolicyParams
 
 
 class AwsDeploymentHelperTool:
-    def __init__(self, cli_args: argparse):
+    def __init__(self, cli_args: argparse.Namespace) -> None:
         self.aws_deployment_helper_obj = AwsDeploymentHelper(
             cli_args.access_key,
             cli_args.secret_key,
@@ -19,7 +23,7 @@ class AwsDeploymentHelperTool:
         )
         self.cli_args = cli_args
 
-    def create(self):
+    def create(self) -> None:
         if self.cli_args.add_iam_user:
             if self.cli_args.user_name is None:
                 raise Exception(
@@ -63,7 +67,7 @@ class AwsDeploymentHelperTool:
                 user_name=self.cli_args.iam_user_name,
             )
 
-    def destroy(self):
+    def destroy(self) -> None:
         if self.cli_args.delete_iam_user:
             self.aws_deployment_helper_obj.delete_user_workflow(
                 user_name=self.cli_args.user_name
