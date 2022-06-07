e8a61b0363c275ac10878192af1242e1f1ab5947
Logan Gore <gorel@fb.com>
2022-06-07 13:27:00 -0700

    Add skeleton code to test methods in aws_deployment_helper
    Summary:
    # This stack
    * Add tests to aws_deployment_helper
    # This diff
    * Skeleton file with no methods implemented
    
    Differential Revision: D36983516
    
    fbshipit-source-id: 5bab6f587a72309e01256ab4e7a66fc27bfa59a8

---
+++ fbpcs/infra/cloud_bridge/deployment_helper/aws/test/test_aws_deployment_helper.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import unittest
+
+
+class TestAwsDeploymentHelper(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_create_user(self) -> None:
+        pass
+
+    def test_delete_user(self) -> None:
+        pass
+
+    def test_create_policy(self) -> None:
+        pass
+
+    def test_delete_policy(self) -> None:
+        pass
+
+    def test_attach_user_policy(self) -> None:
+        pass
+
+    def test_detach_user_policy(self) -> None:
+        pass
+
+    def test_list_policies(self) -> None:
+        pass
+
+    def test_list_users(self) -> None:
+        pass
+
+    def test_create_access_key(self) -> None:
+        pass
+
+    def test_delete_access_key(self) -> None:
+        pass
+
+    def test_list_access_keys(self) -> None:
+        pass
+
+    def test_read_json_file(self) -> None:
+        pass
+
+    def test_create_user_workflow(self) -> None:
+        pass
+
+    def test_delete_user_workflow(self) -> None:
+        pass
