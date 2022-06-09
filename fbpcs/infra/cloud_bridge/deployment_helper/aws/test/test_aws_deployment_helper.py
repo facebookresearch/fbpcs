f6c2c4a3069064e31b1170abd446156e25dee3c2
Jesse Cai <jessecai@fb.com>
2022-06-09 09:54:26 -0700

    Add `test_list_policies` to `test_aws_development_helper` (#1025)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1025
    
    Adds in a test for aws_deployment_helper.list_policies()
    
    Differential Revision: D37029288
    
    fbshipit-source-id: 9a2cfd8d43eb87991aa0531fe90ec2fa6d7b69ed

--- fbpcs/infra/cloud_bridge/deployment_helper/aws/test/test_aws_deployment_helper.py
+++ fbpcs/infra/cloud_bridge/deployment_helper/aws/test/test_aws_deployment_helper.py
@@ -9,6 +9,8 @@
 import unittest
 from unittest.mock import patch
 
+from botocore.exceptions import ClientError
+
 from fbpcs.infra.cloud_bridge.deployment_helper.aws.aws_deployment_helper import (
     AwsDeploymentHelper,
 )
@@ -46,8 +48,24 @@ class TestAwsDeploymentHelper(unittest.TestCase):
         pass
 
     def test_list_policies(self) -> None:
-        # T122887235
-        pass
+        self.aws_deployment_helper.iam.list_policies.return_value = {
+            "Policies": [{"PolicyName": "A"}, {"PolicyName": "B"}, {"PolicyName": "C"}]
+        }
+
+        with self.subTest("basic"):
+            expected = ["A", "B", "C"]
+            self.assertEqual(expected, self.aws_deployment_helper.list_policies())
+            self.aws_deployment_helper.iam.list_policies.assert_called_once()
+
+        # Check client error
+        with self.subTest("list_policies.ClientError"):
+            self.aws_deployment_helper.iam.list_policies.reset_mock()
+            self.aws_deployment_helper.iam.list_policies.side_effect = ClientError(
+                error_response={"Error": {}},
+                operation_name="list_policies",
+            )
+            self.assertEqual([], self.aws_deployment_helper.list_policies())
+            self.aws_deployment_helper.iam.list_policies.assert_called_once()
 
     def test_list_users(self) -> None:
         # T122887247
