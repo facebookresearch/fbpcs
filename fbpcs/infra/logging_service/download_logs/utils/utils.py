6ca84796e7292e10b008633bdef8dd92716e5840
Ankush Singh <ankushksingh@fb.com>
2022-06-09 10:06:34 -0700

    Uploading all the containers in a compressed folder to S3 (#967)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/967
    
    Changing download code flow
    From:
    -> cloudwatch -> local (in memory) -> S3 -> Download -> compress
    
    To:
    
    -> Cloudwatch -> local -> compress -> S3
    
    User can directly download zipped folder with all the logs.
    
    This diff will be followed by adding threading in fetching multiple container logs.
    
    Reviewed By: gorel, gitfish77
    
    Differential Revision: D36796082
    
    fbshipit-source-id: 9bb7133ac63c52ff15cb3bcf347c7e78b5469703

--- fbpcs/infra/logging_service/download_logs/utils/utils.py
+++ fbpcs/infra/logging_service/download_logs/utils/utils.py
@@ -5,11 +5,54 @@
 
 # pyre-strict
 
+import io
 import os
 import shutil
+from typing import List
 
 
 class Utils:
+    def create_file(self, file_location: str, content: List[str]) -> None:
+        """
+        Create file in the file location with content.
+        Args:
+            file_location (str): Full path of the file location Eg: /tmp/xyz.txt
+            content (list): Content to be written in file
+        Returns:
+            None
+        """
+        content = content or []
+
+        try:
+            # write to a file, if it already exists
+            with open(file_location, "w") as file_object:
+                self.write_to_file(file_object, content)
+        except FileNotFoundError as error:
+            # T122918736 - for better execption messages
+            raise Exception(
+                f"Please check if this is the right file path. Error: {error}"
+            )
+
+    @classmethod
+    def write_to_file(
+        cls,
+        file_object: io.TextIOWrapper,
+        contents: List[str],
+        append_newline: bool = True,
+    ) -> None:
+        """
+        Write content to the file.
+        Args:
+            file_object (IO): Object of file to read/write
+            contents (list): Content to be written in file
+        Returns:
+            None
+        """
+        for content in contents:
+            if append_newline:
+                content = content + "\n"
+            file_object.write(content)
+
     @staticmethod
     def create_folder(folder_location: str) -> None:
         """
@@ -32,8 +75,9 @@ class Utils:
             folder_location (str): Complete folder path Eg /tmp/folder1
         """
         if os.path.isdir(folder_location):
-            shutil.make_archive(f"{folder_location}_zipped", "zip", folder_location)
+            shutil.make_archive(folder_location, "zip", folder_location)
         else:
+            # T122918736 - for better exception messages
             raise Exception(
                 f"Couldn't find folder {folder_location}."
                 f"Please check if folder exists.\nAborting folder compression."
