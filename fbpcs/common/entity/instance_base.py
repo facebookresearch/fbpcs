beeee52dee2b0910850ea45ecaea07aec547e74a
Eve Chen <evechenc@fb.com>
2022-06-10 11:22:06 -0700

    freeze individual fields for instance_base and subclass
    Summary:
    I am trying to freeze individual field of InstanceBase and its subclass.
    
    We store the mutability info of each field in the matched Field object as a dictionary.
    
    If a filed is mutable, we can always change it.
    If a filed is immutable, we cannot change it after it has been initialized. An immutable field can be None, empty. It can be initialized after creating the object.
    
    For now, if a immutable field is a reference type, we can still modify its content.
    
    Differential Revision: D36947666
    
    fbshipit-source-id: 24e855048fd32bf7efbd13850a2f830fa6d0d9e4

--- fbpcs/common/entity/instance_base.py
+++ fbpcs/common/entity/instance_base.py
@@ -7,14 +7,33 @@
 # pyre-strict
 
 import abc
-from typing import Type, TypeVar
+from dataclasses import dataclass, field
+from functools import partial
+from typing import Any, Type, TypeVar
 
 from dataclasses_json import DataClassJsonMixin
+from fbpcs.common.entity.exceptions import InstanceFrozenFieldError
+from fbpcs.common.entity.instance_base_config import (
+    InstanceBaseMetadata,
+    IS_FROZEN_FIELD,
+)
 
 T = TypeVar("T", bound="InstanceBase")
 
+# pyre-ignore Missing parameter annotation [4]
+mutable_field = partial(field, metadata=InstanceBaseMetadata.MUTABLE.value)
+# pyre-ignore Missing parameter annotation [4]
+immutable_field = partial(field, metadata=InstanceBaseMetadata.IMMUTABLE.value)
 
+
+@dataclass
 class InstanceBase(DataClassJsonMixin):
+    # this boolean will be set to True after an obj initialization
+    initialized: bool = field(default=False, init=False)
+
+    def __post_init__(self) -> None:
+        self.initialized = True
+
     @abc.abstractmethod
     def get_instance_id(self) -> str:
         pass
@@ -28,3 +47,23 @@ class InstanceBase(DataClassJsonMixin):
     @classmethod
     def loads_schema(cls: Type[T], json_schema_str: str) -> T:
         return cls.schema().loads(json_schema_str, many=None)
+
+    # pyre-ignore Missing parameter annotation [2]
+    def __setattr__(self, name: str, value: Any) -> None:
+        # if setattr is called after initialization
+        if self.initialized:
+            # if we cannot find it, this field has not been initialized yet
+            try:
+                self.__getattribute__(name)
+            except AttributeError:
+                super().__setattr__(name, value)
+            else:
+                # if this field has been initialized and it is immutable
+                # pyre-fixme Undefined attribute [16]: InstanceBase has no attribute __dataclass_fields__
+                if self.__dataclass_fields__[name].metadata.get(IS_FROZEN_FIELD, False):
+                    raise InstanceFrozenFieldError(name)
+                else:
+                    super().__setattr__(name, value)
+        else:
+            # if setattr is called during initialization
+            super().__setattr__(name, value)
