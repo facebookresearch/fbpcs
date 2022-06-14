a85c7ad5e6af5112006199224b1feceefaf2f121
Mao Ye <deborahye@fb.com>
2022-06-14 11:28:28 -0700

    Created PIDPrepareBinaryService (#1048)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1048
    
    In this diff, we created a new RunBinaryBaseService named PIDPrepareBinaryService, which takes care of building arguments to pass to binary. The static methods in this class will be called in PIDPrepareStageService.
    
    Reviewed By: jrodal98
    
    Differential Revision: D36917930
    
    fbshipit-source-id: 718438e919189bf01813ad0c1a56713a11d036ab

---
+++ fbpcs/data_processing/service/pid_prepare_binary_service.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from fbpcs.onedocker_binary_names import OneDockerBinaryNames
+from fbpcs.private_computation.service.run_binary_base_service import (
+    RunBinaryBaseService,
+)
+
+
+class PIDPrepareBinaryService(RunBinaryBaseService):
+    @staticmethod
+    def build_args(
+        input_path: str,
+        output_path: str,
+        tmp_directory: str = "/tmp/",
+        max_column_count: int = 1,
+    ) -> str:
+        cmd_args = " ".join(
+            [
+                f"--input_path={input_path}",
+                f"--output_path={output_path}",
+                f"--tmp_directory={tmp_directory}",
+                f"--max_column_cnt={max_column_count}",
+            ]
+        )
+        return cmd_args
+
+    @staticmethod
+    def get_binary_name() -> str:
+        return OneDockerBinaryNames.UNION_PID_PREPARER.value
