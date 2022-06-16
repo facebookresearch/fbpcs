4f8a9476aad481c2dbffafa0c86bd08cd0ac4b40
Eve Chen <evechenc@fb.com>
2022-06-15 17:21:52 -0700

    add range_hook.py
    Summary: This hook gives a field range of valid values. An error will raise when an unvalid value is given to this field.
    
    Differential Revision: D37099777
    
    fbshipit-source-id: 3fcaafa4e91569a5f5324edb8fec23be295dc9cd

--- fbpcs/common/entity/exceptions.py
+++ fbpcs/common/entity/exceptions.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Any
+
 
 class InstanceBaseError(Exception):
     pass
@@ -13,4 +15,22 @@ class InstanceFrozenFieldError(RuntimeError, InstanceBaseError):
         msg = (
             f"Cannot change value of {name} because it is marked as an immutable field."
         )
+        super().__init__(msg)
+
+
+class HookError(Exception):
+    pass
+
+
+class OutOfRangeHookError(RuntimeError, HookError):
+    def __init__(
+        self, name: str, value: Any, min_value: float, max_value: float
+    ) -> None:
+        msg = f"Cannot change {name} with value: {value} because it is beyond range of [{min_value},  {max_value}]"
+        super().__init__(msg)
+
+
+class MissingRangeHookError(RuntimeError, HookError):
+    def __init__(self, name: str) -> None:
+        msg = f"Cannot create a range hook of {name} because min and max value are both missing."
         super().__init__(msg)
