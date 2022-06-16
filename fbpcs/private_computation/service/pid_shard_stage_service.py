b0eea8ee07e87a2a9931c9f13c835254956b8705
Mao Ye <deborahye@fb.com>
2022-06-15 21:52:52 -0700

    Create PIDShardStageService skeleton
    Summary: In this diff, we created new empty PrivateComputationStageService named PIDShardStageService, which will replace the PIDStageService that calls the PIDShardStage downstream soon.
    
    Reviewed By: joe1234wu
    
    Differential Revision: D37128027
    
    fbshipit-source-id: 059acd1ba3635b12b94b4769bb60c0023c38ff81

---
+++ fbpcs/private_computation/service/pid_shard_stage_service.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+from typing import DefaultDict, List, Optional
+
+from fbpcp.service.onedocker import OneDockerService
+from fbpcp.service.storage import StorageService
+from fbpcs.common.entity.stage_state_instance import StageStateInstance
+from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
+from fbpcs.private_computation.entity.private_computation_instance import (
+    PrivateComputationInstance,
+    PrivateComputationInstanceStatus,
+)
+from fbpcs.private_computation.service.private_computation_stage_service import (
+    PrivateComputationStageService,
+)
+from fbpcs.private_computation.service.utils import (
+    DEFAULT_CONTAINER_TIMEOUT_IN_SEC,
+    get_pc_status_from_stage_state,
+)
+
+
+class PIDShardStageService(PrivateComputationStageService):
+    """Handles business logic for the PID_SHARD stage
+
+    Private attributes:
+        _storage_svc: used to read/write files during private computation runs
+        _onedocker_svc: used to spin up containers that run binaries in the cloud
+        _onedocker_binary_config: stores OneDocker information
+        _containter_timeout: customed timeout for container
+    """
+
+    def __init__(
+        self,
+        storage_svc: StorageService,
+        onedocker_svc: OneDockerService,
+        onedocker_binary_config_map: DefaultDict[str, OneDockerBinaryConfig],
+        container_timeout: Optional[int] = DEFAULT_CONTAINER_TIMEOUT_IN_SEC,
+    ) -> None:
+        self._storage_svc = storage_svc
+        self._onedocker_svc = onedocker_svc
+        self._onedocker_binary_config_map = onedocker_binary_config_map
+        self._container_timeout = container_timeout
+        self._logger: logging.Logger = logging.getLogger(__name__)
+
+    async def run_async(
+        self,
+        pc_instance: PrivateComputationInstance,
+        server_ips: Optional[List[str]] = None,
+    ) -> PrivateComputationInstance:
+        """Runs the PID shard stage service
+        Args:
+            pc_instance: the private computation instance to start pid shard stage service
+            server_ips: No need in this stage.
+        Returns:
+            An updated version of pc_instance
+        """
+        self._logger.info(f"[{self}] Starting PIDShardStageService")
+        container_instances = []
+
+        self._logger.info("PIDShardStageService finished")
+        stage_state = StageStateInstance(
+            pc_instance.instance_id,
+            pc_instance.current_stage.name,
+            containers=container_instances,
+        )
+        pc_instance.instances.append(stage_state)
+        return pc_instance
+
+    def get_status(
+        self,
+        pc_instance: PrivateComputationInstance,
+    ) -> PrivateComputationInstanceStatus:
+        """Gets the latest PrivateComputationInstance status.
+
+        Arguments:
+            pc_instance: The private computation instance that is being updated
+
+        Returns:
+            The latest status for private computation instance
+        """
+        return get_pc_status_from_stage_state(pc_instance, self._onedocker_svc)
