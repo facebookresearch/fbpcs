628d206e58df4cfccb86ee1c47d7e549297604c3
Calvin Ma <calvinma@fb.com>
2022-06-20 08:27:02 -0700

    Add tests for AwsDeploymentHelperTool destroy (#1097)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1097
    
    Added unit tests for the destroy method
    
    Differential Revision: D37247273
    
    fbshipit-source-id: 9a85402f4b687382699444f61eafa7f6cca60523

--- fbpcs/infra/cloud_bridge/deployment_helper/aws/test/test_aws_deployment_helper_tool.py
+++ fbpcs/infra/cloud_bridge/deployment_helper/aws/test/test_aws_deployment_helper_tool.py
@@ -26,6 +26,57 @@ class TestAwsDeploymentHelperTool(unittest.TestCase):
         # T123419617
         pass
 
-    def test_destroy(self) -> None:
-        # T123420230
+    @patch(
+        "fbpcs.infra.cloud_bridge.deployment_helper.aws.aws_deployment_helper_tool.AwsDeploymentHelper"
+    )
+    @patch.object(AwsDeploymentHelperTool, "aws_deployment_helper_obj", create=True)
+    def test_destroy(
+        self,
+        mock_aws_deployment_helper: MagicMock,
+        mock_aws_deployment_helper_obj: MagicMock,
+    ) -> None:
+        with self.subTest("should delete resources correctly"):
+            mock_cli_args = MagicMock()
+            mock_cli_args.user_name = "test_user"
+            mock_cli_args.policy_name = "test_policy"
+            mock_cli_args.iam_policy_name = "test_iam_policy"
+            mock_cli_args.iam_user_name = "test_iam_user"
+            aws_deployment_helper_tool = AwsDeploymentHelperTool(mock_cli_args)
+
+            aws_deployment_helper_tool.destroy()
+            aws_deployment_helper_tool.aws_deployment_helper_obj.delete_user_workflow.assert_called_once_with(
+                user_name=mock_cli_args.user_name
+            )
+            aws_deployment_helper_tool.aws_deployment_helper_obj.delete_policy.assert_called_once_with(
+                policy_name=mock_cli_args.policy_name
+            )
+            aws_deployment_helper_tool.aws_deployment_helper_obj.detach_user_policy.assert_called_once_with(
+                user_name=mock_cli_args.iam_user_name,
+                policy_name=mock_cli_args.iam_policy_name,
+            )
+
+        with self.subTest("should throw when iam_policy_name is None"):
+            mock_cli_args = MagicMock()
+            mock_cli_args.iam_policy_name = None
+            mock_cli_args.iam_user_name = "test_iam_user"
+            aws_deployment_helper_tool = AwsDeploymentHelperTool(mock_cli_args)
+            with self.assertRaisesRegex(
+                Exception,
+                "Need username and policy_name to detach policy to user. Please use"
+                " --user_name and --policy_name arguments in cli.py",
+            ):
+                aws_deployment_helper_tool.destroy()
+
+        with self.subTest("should throw when iam_user_name is None"):
+            mock_cli_args = MagicMock()
+            mock_cli_args.iam_policy_name = "test_iam_policy"
+            mock_cli_args.iam_user_name = None
+            aws_deployment_helper_tool = AwsDeploymentHelperTool(mock_cli_args)
+            with self.assertRaisesRegex(
+                Exception,
+                "Need username and policy_name to detach policy to user. Please use"
+                " --user_name and --policy_name arguments in cli.py",
+            ):
+                aws_deployment_helper_tool.destroy()
+
         pass
