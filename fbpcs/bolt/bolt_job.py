8ddf96bab376eee3c65587eed41fdba72a4da8bd
Andrew Zhuang <azhuang0@fb.com>
2022-06-22 12:21:38 -0700

    Implement Bolt config parsing
    Differential Revision: D37214367
    
    fbshipit-source-id: 2e0734587bdacddba069f1a101afedd8fd86d9ba

--- fbpcs/bolt/bolt_job.py
+++ fbpcs/bolt/bolt_job.py
@@ -7,9 +7,11 @@
 # pyre-strict
 
 from abc import ABC
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from typing import Optional, Type
+
+from dataclasses_json import config, DataClassJsonMixin
 
 from fbpcs.private_computation.stage_flows.private_computation_base_stage_flow import (
     PrivateComputationBaseStageFlow,
@@ -28,8 +30,22 @@ class BoltPlayerArgs:
 
 
 @dataclass
-class BoltJob:
+class BoltJob(DataClassJsonMixin):
     job_name: str
     publisher_bolt_args: BoltPlayerArgs
     partner_bolt_args: BoltPlayerArgs
-    stage_flow: Type[PrivateComputationBaseStageFlow]
+    stage_flow: Type[PrivateComputationBaseStageFlow] = field(
+        metadata={
+            **config(
+                # the enum will be represented as a list of its members, so we can
+                # use the first enum member to get the class name
+                encoder=lambda x: x[0].get_cls_name(),
+                # if no value is provided in the yaml file, then the dataclass json
+                # library will return the default stage flow. Otherwise, if it was
+                # provided in the yaml file, we should decode the string.
+                decoder=lambda x: x
+                if x is PrivateComputationBaseStageFlow
+                else PrivateComputationBaseStageFlow.cls_name_to_cls(x),
+            )
+        },
+    )
