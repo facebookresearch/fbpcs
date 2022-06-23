3bf7bd3c9356de2295c80adbfe4bab4a50b163ac
Ankush Singh <ankushksingh@fb.com>
2022-06-23 10:23:07 -0700

    Adding skeleton unittests for utils.py in logging service downloadlogs (#1120)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1120
    
    Adding skeleton unittests for utils.py in logging service downloadlogs
    
    Reviewed By: gorel, marksliva
    
    Differential Revision: D37363999
    
    fbshipit-source-id: e993fbf14747cab94fb47e0021d83bd3466ed939

---
+++ fbpcs/infra/logging_service/download_logs/utils/test/test_utils.py
@@ -0,0 +1,28 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_create_file(self) -> None:
+        pass
+
+    def test_write_to_file(self) -> None:
+        pass
+
+    def test_create_folder(self) -> None:
+        pass
+
+    def test_compress_downloaded_logs(self) -> None:
+        pass
+
+    def test_copy_file(self) -> None:
+        pass
