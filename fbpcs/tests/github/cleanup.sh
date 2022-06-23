fffed36ff0362c7bb10f852bd1997b822ff34e06
Andrew Zhuang <azhuang0@fb.com>
2022-06-23 16:12:42 -0700

    onboard fbpcs tests to Bolt (#1129)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1129
    
    ## What
    * replaced old Onedocker publish workflow with call to Bolt
    * deleted old shell scripts
    
    ## Why
    * Bolt is faster and easier to use (test took ~36 minutes compared to ~78 previously)
    * we don't have to maintain the old scripts anymore
    
    Reviewed By: gorel, ajinkya-ghonge
    
    Differential Revision: D37361465
    
    fbshipit-source-id: b5a53b5509ba91597012f26ee786d94ebc288efc

--- fbpcs/tests/github/cleanup.sh
+++ fbpcs/tests/github/cleanup.sh
@@ -7,18 +7,15 @@
 # Usage:Remove ECS tasks and aggregation outputs in S3 bucket
 set -e
 
-# shellcheck disable=SC1091,SC1090
-source "$(dirname "${BASH_SOURCE[0]}")/config.sh" || exit 1
-
 # Remove ECS tasks
-RUNNING_TASKS=$(aws ecs list-tasks --cluster "$E2E_CLUSTER_NAME" --desired-status RUNNING --region us-west-2 | grep -E "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
+RUNNING_TASKS=$(aws ecs list-tasks --cluster "fbpcs-github-cicd" --desired-status RUNNING --region us-west-2 | grep -E "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
 IFS=', ' read -r -a array <<< "$RUNNING_TASKS"
 for task in "${array[@]}"
 do
-    aws ecs stop-task --cluster "${E2E_CLUSTER_NAME}" --task "${task}" --region us-west-2 > /dev/null
+    aws ecs stop-task --cluster "fbpcs-github-cicd" --task "${task}" --region us-west-2 > /dev/null
     echo "Task:${task} is stopped"
 done
 
 # Remove all the outputs from previous run
-aws s3 rm --recursive "$LIFT_OUTPUT_PATH"
-aws s3 rm --recursive "$ATTRIBUTION_OUTPUT_PATH"
+aws s3 rm --recursive "s3://fbpcs-github-e2e/lift/outputs"
+aws s3 rm --recursive "s3://fbpcs-github-e2e/attribution/outputs"
