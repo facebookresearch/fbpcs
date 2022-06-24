0c5424a5453d5329a6fd70f60882db82ecc7b0ba
Joe Wu <joewu@fb.com>
2022-06-24 13:59:08 -0700

    raise custom exception on run_next when all stages has been completed. (#1138)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1138
    
    ## Why
    raise custom exception on run_next when all stages have been completed
    T122438804
    in Sev S274601 - www codemod leads to PCService DDoS
    according to tupperware log
    https://www.internalfb.com/tupperware/job/tasks?handle=tsp_global%2Fprivate_lift%2FPrivateComputationServiceRC&task_id=1&task_tab=TASK_LOGS
    www keep spamming request to call run_next even all stages are completed.
    This Diff is from PCS to raise custom exception run_next if we already know all stage flow are completed to avoid error happened which lead thrift server unhealthy.
    ## What
    * raise PrivateComputationServiceInvalidStageError on run_next request if pc instance know last stage is marked as completed.
    * in Thrift server, catch PrivateComputationServiceInvalidStageError and raise InvalidRequestT to www
    
    ## Next
    follow this wiki instruction - SETUP DIFFERENT EXCEPTIONS FOR USER ERRORS AND APPLICATION ERROR
    https://www.internalfb.com/intern/wiki/ServiceRouter/User_Guide/Monitoring/#difference-between-fatal
    which we still want to raise the InvalidRequestT exception, but don't affect how SR monitors thrift server
    
    Reviewed By: jrodal98
    
    Differential Revision: D37424441
    
    fbshipit-source-id: 057cdc704339564bdbb359214e7459219d7573b0

--- fbpcs/private_computation/service/errors.py
+++ fbpcs/private_computation/service/errors.py
@@ -5,7 +5,19 @@
 # LICENSE file in the root directory of this source tree.
 
 
-class PrivateComputationServiceValidationError(ValueError):
+class PrivateComputationServiceBaseException(Exception):
+    pass
+
+
+class PrivateComputationServiceValidationError(
+    PrivateComputationServiceBaseException, ValueError
+):
     """
     Error raised when private_computation_service.validate_metrics found the aggregated results doesn't match the expected results.
     """
+
+
+class PrivateComputationServiceInvalidStageError(
+    PrivateComputationServiceBaseException, RuntimeError
+):
+    pass
