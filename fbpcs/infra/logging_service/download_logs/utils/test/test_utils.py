f1851ffbbfb80efdb202b97f66caede56f8240eb
Ankush Singh <ankushksingh@fb.com>
2022-06-24 15:09:48 -0700

    Unittest for create_file in utils.py
    Differential Revision: D37389252
    
    fbshipit-source-id: 3aca6c31d61c8549ea0451713388b70f234d4e67

--- fbpcs/infra/logging_service/download_logs/utils/test/test_utils.py
+++ fbpcs/infra/logging_service/download_logs/utils/test/test_utils.py
@@ -6,14 +6,28 @@
 # pyre-strict
 
 import unittest
+from unittest.mock import mock_open, patch
+
+from fbpcs.infra.logging_service.download_logs.utils.utils import Utils
 
 
 class TestUtils(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.utils = Utils()
 
     def test_create_file(self) -> None:
-        pass
+        with self.subTest("basic"):
+            fake_file_path = "fake/file/path"
+            content_list = ["This is test string"]
+            with patch(
+                "fbpcs.infra.logging_service.download_logs.utils.utils.open",
+                mock_open(),
+            ) as mocked_file:
+                self.utils.create_file(
+                    file_location=fake_file_path, content=content_list
+                )
+                mocked_file.assert_called_once_with(fake_file_path, "w")
+                mocked_file().write.assert_called_once_with(content_list[0] + "\n")
 
     def test_write_to_file(self) -> None:
         pass
