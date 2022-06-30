fc79d45040f2baa86582b9442616bc50dd08f08b
Eve Chen <evechenc@fb.com>
2022-06-30 13:40:32 -0700

    delete is_validating
    Summary:
    # This Stack: T119253842
    This stack will decouple product and infra instance data in private_computation_instance.py.
    
    We will ship the whole stack after each diff is accepted.
    
    # 1st Part: Delete deprecated fields: T124135944
    ## deprecated fields:
    is_validating,
    is_test,
    fail_fast,
    synthetic_shard_path,
    partial_container_retry_enabled
    
    # This Diff
    delete is_validating
    
    Differential Revision: D37369756
    
    fbshipit-source-id: aae1db1e944980afb6084e490861311916cb3301

--- fbpcs/private_computation/service/aggregate_shards_stage_service.py
+++ fbpcs/private_computation/service/aggregate_shards_stage_service.py
@@ -37,7 +37,6 @@ class AggregateShardsStageService(PrivateComputationStageService):
     Private attributes:
         _onedocker_binary_config_map: Stores a mapping from mpc game to OneDockerBinaryConfig (binary version and tmp directory)
         _mpc_svc: creates and runs MPC instances
-        _is_validating: TODO
         _log_cost_to_s3: TODO
         _container_timeout: optional duration in seconds before cloud containers timeout
     """
@@ -46,13 +45,11 @@ class AggregateShardsStageService(PrivateComputationStageService):
         self,
         onedocker_binary_config_map: DefaultDict[str, OneDockerBinaryConfig],
         mpc_service: MPCService,
-        is_validating: bool = False,
         log_cost_to_s3: bool = DEFAULT_LOG_COST_TO_S3,
         container_timeout: Optional[int] = None,
     ) -> None:
         self._onedocker_binary_config_map = onedocker_binary_config_map
         self._mpc_service = mpc_service
-        self._is_validating = is_validating
         self._log_cost_to_s3 = log_cost_to_s3
         self._container_timeout = container_timeout
 
@@ -116,96 +113,38 @@ class AggregateShardsStageService(PrivateComputationStageService):
         else:
             run_name = ""
 
-        if self._is_validating:
-            # num_containers_real_data is the number of containers processing real data
-            # synthetic data is processed by a dedicated extra container, and this container is always the last container,
-            # hence synthetic_data_shard_start_index = num_real_data_shards
-            # each of the containers, processing real or synthetic data, processes the same number of shards due to our resharding mechanism
-            # num_shards representing the total number of shards which is equal to num_real_data_shards + num_synthetic_data_shards
-            # hence, when num_containers_real_data and num_shards are given, num_synthetic_data_shards = num_shards / (num_containers_real_data + 1)
-            num_containers_real_data = pc_instance.num_pid_containers
-            if num_containers_real_data is None:
-                raise ValueError("num_containers_real_data is None")
-            num_synthetic_data_shards = num_shards // (num_containers_real_data + 1)
-            num_real_data_shards = num_shards - num_synthetic_data_shards
-            synthetic_data_shard_start_index = num_real_data_shards
+        # Create and start MPC instance
+        game_args = [
+            {
+                "input_base_path": input_stage_path,
+                "metrics_format_type": metrics_format_type,
+                "num_shards": num_shards,
+                "output_path": pc_instance.shard_aggregate_stage_output_path,
+                "threshold": pc_instance.k_anonymity_threshold,
+                "run_name": run_name,
+                "log_cost": self._log_cost_to_s3,
+            },
+        ]
+        # We should only export visibility to scribe when it's set
+        if pc_instance.result_visibility is not ResultVisibility.PUBLIC:
+            result_visibility = int(pc_instance.result_visibility)
+            for arg in game_args:
+                arg["visibility"] = result_visibility
 
-            # Create and start MPC instance for real data shards and synthetic data shards
-            game_args = [
-                {
-                    "input_base_path": input_stage_path,
-                    "num_shards": num_real_data_shards,
-                    "metrics_format_type": metrics_format_type,
-                    "output_path": pc_instance.shard_aggregate_stage_output_path,
-                    "first_shard_index": 0,
-                    "threshold": pc_instance.k_anonymity_threshold,
-                    "run_name": run_name,
-                    "log_cost": self._log_cost_to_s3,
-                },
-                {
-                    "input_base_path": input_stage_path,
-                    "num_shards": num_synthetic_data_shards,
-                    "metrics_format_type": metrics_format_type,
-                    "output_path": pc_instance.shard_aggregate_stage_output_path
-                    + "_synthetic_data_shards",
-                    "first_shard_index": synthetic_data_shard_start_index,
-                    "threshold": pc_instance.k_anonymity_threshold,
-                    "run_name": run_name,
-                    "log_cost": self._log_cost_to_s3,
-                },
-            ]
-            # We should only export visibility to scribe when it's set
-            if pc_instance.result_visibility is not ResultVisibility.PUBLIC:
-                result_visibility = int(pc_instance.result_visibility)
-                for arg in game_args:
-                    arg["visibility"] = result_visibility
-
-            mpc_instance = await create_and_start_mpc_instance(
-                mpc_svc=self._mpc_service,
-                instance_id=pc_instance.instance_id
-                + "_aggregate_shards"
-                + str(pc_instance.retry_counter),
-                game_name=GameNames.SHARD_AGGREGATOR.value,
-                mpc_party=map_private_computation_role_to_mpc_party(pc_instance.role),
-                num_containers=2,
-                binary_version=binary_config.binary_version,
-                server_ips=server_ips,
-                game_args=game_args,
-                container_timeout=self._container_timeout,
-            )
-        else:
-            # Create and start MPC instance
-            game_args = [
-                {
-                    "input_base_path": input_stage_path,
-                    "metrics_format_type": metrics_format_type,
-                    "num_shards": num_shards,
-                    "output_path": pc_instance.shard_aggregate_stage_output_path,
-                    "threshold": pc_instance.k_anonymity_threshold,
-                    "run_name": run_name,
-                    "log_cost": self._log_cost_to_s3,
-                },
-            ]
-            # We should only export visibility to scribe when it's set
-            if pc_instance.result_visibility is not ResultVisibility.PUBLIC:
-                result_visibility = int(pc_instance.result_visibility)
-                for arg in game_args:
-                    arg["visibility"] = result_visibility
-
-            mpc_instance = await create_and_start_mpc_instance(
-                mpc_svc=self._mpc_service,
-                instance_id=pc_instance.instance_id
-                + "_aggregate_shards"
-                + str(pc_instance.retry_counter),
-                game_name=GameNames.SHARD_AGGREGATOR.value,
-                mpc_party=map_private_computation_role_to_mpc_party(pc_instance.role),
-                num_containers=1,
-                binary_version=binary_config.binary_version,
-                server_ips=server_ips,
-                game_args=game_args,
-                container_timeout=self._container_timeout,
-                repository_path=binary_config.repository_path,
-            )
+        mpc_instance = await create_and_start_mpc_instance(
+            mpc_svc=self._mpc_service,
+            instance_id=pc_instance.instance_id
+            + "_aggregate_shards"
+            + str(pc_instance.retry_counter),
+            game_name=GameNames.SHARD_AGGREGATOR.value,
+            mpc_party=map_private_computation_role_to_mpc_party(pc_instance.role),
+            num_containers=1,
+            binary_version=binary_config.binary_version,
+            server_ips=server_ips,
+            game_args=game_args,
+            container_timeout=self._container_timeout,
+            repository_path=binary_config.repository_path,
+        )
         # Push MPC instance to PrivateComputationInstance.instances and update PL Instance status
         pc_instance.instances.append(PCSMPCInstance.from_mpc_instance(mpc_instance))
         return pc_instance
