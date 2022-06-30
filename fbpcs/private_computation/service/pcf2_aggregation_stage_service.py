b322b17ecce9b77ce12985d69c561afd99f14c91
Eve Chen <evechenc@fb.com>
2022-06-30 13:27:57 -0700

    put instance[UnionedPCInstance] in infra_config
    Summary:
    # This Stack: T119253842
    This stack will decouple product and infra instance data in private_computation_instance.py.
    We will ship the whole stack after each diff is accepted.
    ## 2nd Part: Implement infra_config: T124215036
    # This Diff
    put instance[UnionedPCInstance] in infra_config
    
    Differential Revision: https://www.internalfb.com/diff/D37397269?entry_point=27
    
    fbshipit-source-id: ffda4b74b81887f6984533dd9e0e65da4407f43b

--- fbpcs/private_computation/service/pcf2_aggregation_stage_service.py
+++ fbpcs/private_computation/service/pcf2_aggregation_stage_service.py
@@ -117,7 +117,9 @@ class PCF2AggregationStageService(PrivateComputationStageService):
         logging.info("MPC instance started running for pcf2.0 based aggregation stage.")
 
         # Push MPC instance to PrivateComputationInstance.instances and update PL Instance status
-        pc_instance.instances.append(PCSMPCInstance.from_mpc_instance(mpc_instance))
+        pc_instance.infra_config.instances.append(
+            PCSMPCInstance.from_mpc_instance(mpc_instance)
+        )
         return pc_instance
 
     # For now, only passing the attribution game arguments, as this game is currently only used for PA.
