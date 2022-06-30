f86caf7287bf81c6859f5c6bca94c0bdc95bd743
Eve Chen <evechenc@fb.com>
2022-06-30 13:27:47 -0700

    delete is_test
    Summary:
    # This Stack
     This stack will delete the deprecated fields in private_computation_instance.py
    
    # This Diff
    delete is_test
    
    Differential Revision: D37369771
    
    fbshipit-source-id: 291c4b71b433db09e462c2b455955a8e636b99ba

--- fbpcs/private_computation/entity/private_computation_instance.py
+++ fbpcs/private_computation/entity/private_computation_instance.py
@@ -139,7 +139,7 @@ class PrivateComputationInstance(InstanceBase):
 
     breakdown_key: Optional[BreakdownKey] = None
     pce_config: Optional[PCEConfig] = None
-    is_test: Optional[bool] = False  # set to be true for testing account ID
+
     # stored as a string because the enum was refusing to serialize to json, no matter what I tried.
     # TODO(T103299005): [BE] Figure out how to serialize StageFlow objects to json instead of using their class name
     _stage_flow_cls_name: str = "PrivateComputationStageFlow"
