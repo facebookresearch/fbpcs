79a98822146a6ddbe8372080c1574adad40d0011
Ankush Singh <ankushksingh@fb.com>
2022-06-30 09:55:43 -0700

    Adding detailed unitests for check_s3_buckets_exists (#1166)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1166
    
    Adding detailed unitests for check_s3_buckets_exists.
    
    This diff addresses review comments on D37446115 (https://github.com/facebookresearch/fbpcs/commit/0c28929bcd163f3fbea5cbb2d8bfa823ba80f161)
    
    Reviewed By: marksliva
    
    Differential Revision: D37479056
    
    fbshipit-source-id: f48ffeed8ee331ace96ec1b562c1fe00e65b85d7

--- fbpcs/infra/pce_deployment_library/test/test_aws.py
+++ fbpcs/infra/pce_deployment_library/test/test_aws.py
@@ -7,7 +7,7 @@
 # pyre-strict
 
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import create_autospec
 
 from botocore.exceptions import ClientError
 
@@ -21,29 +21,49 @@ from fbpcs.infra.pce_deployment_library.errors_library.aws_errors import (
 class TestAws(unittest.TestCase):
     def setUp(self) -> None:
         self.aws = AWS()
-        self.aws.sts.get_caller_identity = MagicMock(return_value=None)
+        self.aws.sts.get_caller_identity = create_autospec(
+            self.aws.sts.get_caller_identity
+        )
 
     def test_check_s3_buckets_exists(self) -> None:
         s3_bucket_name = "fake_bucket"
-        self.aws.s3_client.head_bucket = MagicMock(return_value=None)
+        self.aws.s3_client.head_bucket = create_autospec(self.aws.s3_client.head_bucket)
 
         with self.subTest("basic"):
-            self.assertIsNone(
-                self.aws.check_s3_buckets_exists(s3_bucket_name=s3_bucket_name)
-            )
+            with self.assertLogs() as captured:
+                self.aws.check_s3_buckets_exists(
+                    s3_bucket_name=s3_bucket_name, bucket_version=False
+                )
+                self.assertEqual(len(captured.records), 2)
+                self.assertEqual(
+                    captured.records[1].getMessage(),
+                    f"S3 bucket {s3_bucket_name} already exists in the AWS account.",
+                )
 
         with self.subTest("BucketNotFound"):
-            self.aws.s3_client.create_bucket = MagicMock(return_value=None)
-            self.aws.s3_client.put_bucket_versionin = MagicMock(return_value=None)
+            self.aws.s3_client.create_bucket = create_autospec(
+                self.aws.s3_client.create_bucket
+            )
+            self.aws.s3_client.put_bucket_versioning = create_autospec(
+                self.aws.s3_client.put_bucket_versioning
+            )
             self.aws.s3_client.head_bucket.side_effect = ClientError(
                 error_response={"Error": {"Code": "404"}},
                 operation_name="head_bucket",
             )
-            self.assertIsNone(
+            with self.assertLogs() as captured:
                 self.aws.check_s3_buckets_exists(
                     s3_bucket_name=s3_bucket_name, bucket_version=False
                 )
-            )
+                self.assertEqual(len(captured.records), 4)
+                self.assertEqual(
+                    captured.records[2].getMessage(),
+                    f"Creating new S3 bucket {s3_bucket_name}",
+                )
+                self.assertEqual(
+                    captured.records[3].getMessage(),
+                    f"Create S3 bucket {s3_bucket_name} operation was successful.",
+                )
 
         with self.subTest("AccessDenied"):
             self.aws.s3_client.head_bucket.side_effect = ClientError(
