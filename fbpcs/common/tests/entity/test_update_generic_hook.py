daaf90340a8113e61046d18bab8216632222746b
Eve Chen <evechenc@fb.com>
2022-07-12 11:53:42 -0700

    implement update_generic_hook.py (#1260)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1260
    
    Finish implement of update_generic_hook.py and apply tests.
    
    Reviewed By: joe1234wu
    
    Differential Revision: D37618895
    
    fbshipit-source-id: cb44205d732ba48f8357e60354d5d3875a2e34d7

--- fbpcs/common/tests/entity/test_update_generic_hook.py
+++ fbpcs/common/tests/entity/test_update_generic_hook.py
@@ -6,22 +6,60 @@
 # pyer-strict
 
 import unittest
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-from fbpcs.common.entity.instance_base import InstanceBase
+from fbpcs.common.entity.dataclasses_hooks import DataclassHookMixin
+from fbpcs.common.entity.dataclasses_mutability import MutabilityMetadata, mutable_field
+
+from fbpcs.common.entity.update_generic_hook import UpdateGenericHook
+
+
+def after_update_input(obj: "DummyInstance"):
+    obj.output_path = obj.input_path + ":output"
+    obj.storage = obj.input_path + ":storage"
+
+
+# create a hook obj
+# update output_path and storage in one hook when input_path is changed
+input_updated_hook: UpdateGenericHook = UpdateGenericHook(after_update_input)
 
 
 @dataclass
-class DummyInstance(InstanceBase):
+class DummyInstance(DataclassHookMixin):
     """
     Dummy instance class to be used in unit tests.
 
     """
 
+    input_path: str = field(
+        metadata={
+            **DataclassHookMixin.get_metadata(input_updated_hook),
+            **MutabilityMetadata.MUTABLE.value,
+        }
+    )
+
+    output_path: str = mutable_field(init=False)
+    storage: str = mutable_field(init=False)
+
 
 class TestUpdateGenericHook(unittest.TestCase):
     def test_init_event_update_generic_hook(self) -> None:
-        pass
+        # create an obj
+        dummy_obj = DummyInstance("//fbsource")
+
+        # assert output_path will be automatically initialized by instance_id hook
+        self.assertEqual(dummy_obj.output_path, "//fbsource:output")
+        # assert storage will be automatically initialized by id hook
+        self.assertEqual(dummy_obj.storage, "//fbsource:storage")
 
     def test_update_event_update_generic_hook(self) -> None:
-        pass
+        # create an obj
+        dummy_obj = DummyInstance("//fbsource")
+
+        # change the input path
+        dummy_obj.input_path = "//www"
+
+        # assert output have been changed by hook
+        self.assertEqual(dummy_obj.output_path, "//www:output")
+        # assert storage have been changed by hook
+        self.assertEqual(dummy_obj.storage, "//www:storage")
