98117d39b15a99c477ec2efea3a56dae1120f1e7
Ankush Singh <ankushksingh@fb.com>
2022-07-12 17:55:02 -0700

    Adding terraform init implementation for terraform CLI init
    Differential Revision: D37635866
    
    fbshipit-source-id: c50483ec4427b63263acb7d9a7073cca45859815

--- fbpcs/infra/pce_deployment_library/deploy_library/terraform_library/terraform.py
+++ fbpcs/infra/pce_deployment_library/deploy_library/terraform_library/terraform.py
@@ -8,13 +8,17 @@
 import logging
 import sys
 from subprocess import PIPE, Popen
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional
 
 from fbpcs.infra.pce_deployment_library.deploy_library.deploy_base.deploy_base import (
     DeployBase,
 )
 
-from fbpcs.infra.pce_deployment_library.deploy_library.models import RunCommandReturn
+from fbpcs.infra.pce_deployment_library.deploy_library.models import (
+    RunCommandReturn,
+    TerraformCliOptions,
+    TerraformCommands,
+)
 from fbpcs.infra.pce_deployment_library.deploy_library.terraform_library.terraform_utils import (
     TerraformUtils,
 )
@@ -60,20 +64,49 @@ class Terraform(DeployBase):
     def destroy(self) -> None:
         pass
 
-    def init(self) -> None:
-        pass
+    def terraform_init(
+        self,
+        backend_config: Optional[Dict[str, str]] = None,
+        reconfigure: bool = False,
+        **kwargs: Dict[str, Any],
+    ) -> RunCommandReturn:
+        """
+        Implements `terraform init` of terraform CLI.
+        `terraform init` command is used to initialize a working directory containing Terraform configuration files.
+
+        More information: https://www.terraform.io/cli/commands/init
+
+        backend_config:
+            Provides backend config information using key-value pairs.
+            Usage:
+                terraform = Terraform()
+                terraform.terraform_init(backend_config = {"bucket": s3_bucket,"region": region})
+            More info: https://www.terraform.io/language/settings/backends/configuration#command-line-key-value-pairs
+
+        reconfigure:
+            in `terraform init` reconfigure disregards any existing configuration, preventing migration of any existing state.
+            More info: https://www.terraform.io/cli/commands/init#backend-initialization
+        """
+        options: Dict[str, Any] = kwargs.copy()
+        options.update(
+            {
+                TerraformCliOptions.backend_config: backend_config,
+                TerraformCliOptions.reconfigure: reconfigure,
+            }
+        )
+        options = self.utils.get_default_options(TerraformCommands.init, options)
+        return self.run_command("terraform init", **options)
 
     def plan(self) -> None:
         pass
 
     def run_command(
-        self,
-        command: str,
-        capture_output: bool = True,
+        self, command: str, capture_output: bool = True, **kwargs: Any
     ) -> RunCommandReturn:
         """
         Executes Terraform CLIs apply/destroy/init/plan
         """
+        options: Dict[str, Any] = kwargs.copy()
 
         if capture_output:
             stderr = PIPE
@@ -82,7 +115,7 @@ class Terraform(DeployBase):
             stderr = sys.stderr
             stdout = sys.stdout
 
-        command_list = self.utils.get_command_list(command)
+        command_list = self.utils.get_command_list(command, **options)
         command_str = " ".join(command_list)
         self.log.info(f"Command: {command_str}")
         out, err = None, None
