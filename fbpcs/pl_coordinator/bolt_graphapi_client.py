bd40e145faa3e57f26505e689f6dd797924334e8
Andrew Zhuang <azhuang0@fb.com>
2022-07-18 12:01:35 -0700

    Introducing Bolt GraphAPI client skeletons (#1294)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1294
    
    ## What
    * skeleton for Bolt GraphApi clients
    
    ## Why
    * to align on interface
    
    Reviewed By: jrodal98
    
    Differential Revision: D37838350
    
    fbshipit-source-id: f97cc42bc2e1cdbef93d2f6d1e74d703d7580201

---
+++ fbpcs/pl_coordinator/bolt_graphapi_client.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+from fbpcs.bolt.bolt_job import BoltCreateInstanceArgs
+from fbpcs.bolt.bolt_runner import BoltClient, BoltState
+from fbpcs.private_computation.stage_flows.private_computation_base_stage_flow import (
+    PrivateComputationBaseStageFlow,
+)
+
+
+@dataclass
+class BoltPLGraphAPICreateInstanceArgs(BoltCreateInstanceArgs):
+    study_id: str
+    breakdown_key: Dict[str, str]
+    instance_id: str  # used for temporary resuming solution
+
+
+@dataclass
+class BoltPAGraphPICreateInstanceArgs(BoltCreateInstanceArgs):
+    dataset_id: str
+    timestamp: str
+    attribution_rule: str
+    num_containers: str
+    instance_id: str  # used for temporary resuming solution
+
+
+class BoltGraphAPIClient(BoltClient):
+    def __init__(
+        self, access_token: str, logger: Optional[logging.Logger] = None
+    ) -> None:
+        pass
+
+    async def create_instance(self, instance_args: BoltCreateInstanceArgs) -> str:
+        pass
+
+    async def run_stage(
+        self,
+        instance_id: str,
+        stage: PrivateComputationBaseStageFlow,
+        server_ips: Optional[List[str]] = None,
+    ) -> None:
+        pass
+
+    async def update_instance(self, instance_id: str) -> BoltState:
+        pass
+
+    async def validate_results(
+        self, instance_id: str, expected_result_path: Optional[str] = None
+    ) -> bool:
+        pass
---
+++ fbpcs/pl_coordinator/tests/test_bolt_graphapi_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+from fbpcs.pl_coordinator.bolt_graphapi_client import BoltGraphAPIClient
+
+
+class TestBoltGraphAPIClient(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        pass
+
+    async def test_create_instance(self) -> None:
+        pass
+
+    async def test_run_stage(self) -> None:
+        pass
+
+    async def test_update_instance(self) -> None:
+        pass
+
+    async def test_validate_results(self) -> None:
+        pass
