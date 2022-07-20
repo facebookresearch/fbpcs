d28b72d5b260e844b053b23f3776fc6a44a3d16f
Shashank Ganapathi Hegde <sgh@fb.com>
2022-07-19 18:45:21 -0700

    Add implementations to AggMetrics and Tests (#1320)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1320
    
    # Context
    This commit contains the implementation of AggMetrics for shard combiner.
    
    # Changes in the diff
    1. Add new AggMetrics_imph.h which contains implementation of metrics formation and accumulation APIs
    2. Test file that checks reading of the json files and populating the metrics.
    3. added corresponding test and compile TARGETs
    
    ShardValidator to follow.
    
    Reviewed By: chualynn
    
    Differential Revision: D37571014
    
    fbshipit-source-id: dcac1551cc2ea9a47ea727c8c37bcfd2fec4b177

---
+++ fbpcs/emp_games/pcf2_shard_combiner/AggMetricsTest.cpp
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include <fbpcf/exception/exceptions.h>
+#include <fbpcs/emp_games/common/Constants.h>
+#include <fbpcs/emp_games/common/TestUtil.h>
+#include <fbpcs/emp_games/pcf2_shard_combiner/AggMetrics.h>
+#include <fbpcs/emp_games/pcf2_shard_combiner/AggMetrics_impl.h>
+using namespace ::testing;
+
+namespace shard_combiner {
+
+class AggMetricsTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    std::string filePath = __FILE__;
+    baseDir_ = filePath.substr(0, filePath.rfind("/")) + "/test/";
+  }
+  std::string baseDir_;
+};
+
+TEST_F(AggMetricsTest, TestParseAttribution) {
+  auto inputPath =
+      baseDir_ + "shard_validation_test/valid_measurement_shard.json";
+
+  auto parsedInput = folly::parseJson(fbpcf::io::read(inputPath));
+
+  constexpr int schedulerId = 0;
+  constexpr bool usingBatch = false;
+  constexpr common::InputEncryption inputEncryption =
+      common::InputEncryption::Plaintext;
+
+  auto metrics =
+      AggMetrics<schedulerId, usingBatch, inputEncryption>::fromJson(inputPath);
+
+  XLOG(INFO) << metrics;
+  EXPECT_EQ(
+      metrics->getAtKey("last_click_1d")
+          ->getAtKey("measurement")
+          ->getAtKey("1")
+          ->getAtKey("convs")
+          ->getValue(),
+      -831273128088263600);
+  EXPECT_EQ(
+      metrics->getAtKey("last_click_1d")
+          ->getAtKey("measurement")
+          ->getAtKey("1")
+          ->getAtKey("sales")
+          ->getValue(),
+      339959610281870460);
+  EXPECT_EQ(
+      metrics->getAtKey("last_touch_1d")
+          ->getAtKey("measurement")
+          ->getAtKey("1")
+          ->getAtKey("convs")
+          ->getValue(),
+      -4250297646419635700);
+  EXPECT_EQ(
+      metrics->getAtKey("last_touch_1d")
+          ->getAtKey("measurement")
+          ->getAtKey("1")
+          ->getAtKey("sales")
+          ->getValue(),
+      -572762462605311500);
+  EXPECT_EQ(metrics->toDynamic(), parsedInput);
+}
+
+TEST_F(AggMetricsTest, TestParseLift) {
+  auto inputPath = baseDir_ + "shard_validation_test/valid_lift_input.json";
+  auto parsedInput = folly::parseJson(fbpcf::io::read(inputPath));
+
+  constexpr int schedulerId = 0;
+  constexpr bool usingBatch = false;
+  constexpr common::InputEncryption inputEncryption =
+      common::InputEncryption::Plaintext;
+
+  auto metrics =
+      AggMetrics<schedulerId, usingBatch, inputEncryption>::fromJson(inputPath);
+  XLOG(INFO) << metrics;
+
+  ASSERT_EQ(metrics->getAtKey("cohortMetrics")->getAsList().size(), 2);
+  ASSERT_EQ(metrics->getAtKey("publisherBreakdowns")->getAsList().size(), 2);
+  ASSERT_EQ(metrics->getAtKey("metrics")->getAsDict().size(), 28);
+
+  // check a few values
+  EXPECT_EQ(
+      metrics->getAtKey("cohortMetrics")
+          ->getAtIndex(0)
+          ->getAtKey("controlValueSquared")
+          ->getValue(),
+      2988483738);
+  EXPECT_EQ(
+      metrics->getAtKey("cohortMetrics")
+          ->getAtIndex(0)
+          ->getAtKey("reachedValue")
+          ->getValue(),
+      1957171223);
+  EXPECT_EQ(
+      metrics->getAtKey("cohortMetrics")
+          ->getAtIndex(1)
+          ->getAtKey("controlValueSquared")
+          ->getValue(),
+      1825398531);
+  EXPECT_EQ(
+      metrics->getAtKey("cohortMetrics")
+          ->getAtIndex(1)
+          ->getAtKey("reachedValue")
+          ->getValue(),
+      2368649346);
+  EXPECT_EQ(
+      metrics->getAtKey("publisherBreakdowns")
+          ->getAtIndex(0)
+          ->getAtKey("controlValueSquared")
+          ->getValue(),
+      2988483738);
+  EXPECT_EQ(
+      metrics->getAtKey("publisherBreakdowns")
+          ->getAtIndex(0)
+          ->getAtKey("reachedValue")
+          ->getValue(),
+      1957171223);
+  EXPECT_EQ(
+      metrics->getAtKey("publisherBreakdowns")
+          ->getAtIndex(1)
+          ->getAtKey("controlValueSquared")
+          ->getValue(),
+      1825398531);
+  EXPECT_EQ(
+      metrics->getAtKey("publisherBreakdowns")
+          ->getAtIndex(1)
+          ->getAtKey("reachedValue")
+          ->getValue(),
+      2368649346);
+  EXPECT_EQ(
+      metrics->getAtKey("metrics")->getAtKey("controlValueSquared")->getValue(),
+      405497006);
+  EXPECT_EQ(metrics->toDynamic(), parsedInput);
+}
+
+TEST_F(AggMetricsTest, TestParseInvalidMap) {
+  auto inputPath = baseDir_ + "test_new_parser/invalid_map.json";
+
+  EXPECT_THROW(
+      AggMetrics<>::fromJson(inputPath),
+      common::exceptions::NotImplementedError);
+}
+
+} // namespace shard_combiner
---
+++ fbpcs/emp_games/pcf2_shard_combiner/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <folly/logging/xlog.h>
+#include "fbpcs/emp_games/pcf2_shard_combiner/AggMetrics.h"
+
+using namespace shard_combiner;
+
+int main() {
+  XLOG(INFO)
+      << "Place holder for the target file(to ensure crt1's entry to main).";
+  return 0;
+}
