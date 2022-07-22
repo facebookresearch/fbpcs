5debcc881b295bf7e1bfc9d8698eeca5c5065d67
Chenyu Liu <chenyuliu9402@fb.com>
2022-07-21 23:17:42 -0700

    Temporary commit at 6/29/2022, 6:22:11 PM
    Differential Revision: D37541926
    
    fbshipit-source-id: 69799bba24b60c7e020deb1efa4def1c26322a1d

--- fbpcs/private_computation/service/pcf2_attribution_stage_service.py
+++ fbpcs/private_computation/service/pcf2_attribution_stage_service.py
@@ -184,6 +184,7 @@ class PCF2AttributionStageService(PrivateComputationStageService):
             "attribution_rules": attribution_rule.value,
             "use_xor_encryption": True,
             "use_postfix": True,
+            "use_new_output_format": False,
         }
 
         game_args = [
