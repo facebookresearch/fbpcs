a157abcee0312dde443731d5cbc60b823b296369
Shashank Ganapathi Hegde <sgh@fb.com>
2022-07-22 17:17:21 -0700

    Add Shard validator and Tests (#1333)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1333
    
    # Context
    This diff adds the shard validator, basically, the purpose of this to check whether the Json Shards adhere to a said Schema. Since it templatized now, newer formats are easy to add. Also, code is more readable because we know which validator is used
    ```
    ShardValidator<ShardSchemaType::GourpedLiftMetric> v;
    v.validate(obj);
    ```
    for instance is more readable than,
    ```
    ShardValidator v;
    v.validate(obj, "ad_object_fmt");
    ```
    # Changes in this diff
    1. Adds to new ShardValidator
     a. currently we support 2 formats AdObjectFormat and GroupedLiftMetric format.
     b. I added `VALIDATE_OR_THROW` macro, so that the code looks compact, readable. I'm not using glog checkers because we can't make them throw the exception that we like.
    2. Adds ShadValidator tests.
    
    Reviewed By: anthonyzhang25
    
    Differential Revision: D37571697
    
    fbshipit-source-id: bd27055eee93e7dce00c940b88e4e37c59e2ba63

---
+++ fbpcs/emp_games/pcf2_shard_combiner/ShardValidator.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <fbpcs/emp_games/common/Constants.h>
+#include <fbpcs/emp_games/pcf2_shard_combiner/AggMetrics.h>
+
+namespace shard_combiner {
+
+enum class ShardSchemaType { kAdObjFormat, kGroupedLiftMetrics };
+
+template <
+    ShardSchemaType shardSchemaType,
+    int schedulerId,
+    bool usingBatch,
+    common::InputEncryption inputEncryption>
+void validateShardSchema(
+    const AggMetrics<schedulerId, usingBatch, inputEncryption>& metrics);
+
+} // namespace shard_combiner
