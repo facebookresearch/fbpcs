03be1a08b6721ceb98df7d3dbfab445fa885cfd2
Logan Gore <gorel@fb.com>
2022-08-02 14:34:04 -0700

    Create SimpleTraceLoggingService for OSS (#1380)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1380
    
    # What
    * Title
    # Why
    * The low bar for OSS code is that trace logging just writes messages out via the logger. This mimics the behavior we implemented for the metric service in diff 2 of this stack
    
    Reviewed By: joe1234wu
    
    Differential Revision: D38266949
    
    fbshipit-source-id: 3faff82cec8d88efd6f4a89528eeabcf70272f5f

---
+++ fbpcs/common/service/test/test_simple_trace_logging_service.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import json
+import logging
+from unittest import mock, TestCase
+
+from fbpcs.common.service.simple_trace_logging_service import SimpleTraceLoggingService
+
+from fbpcs.common.service.trace_logging_service import CheckpointStatus
+
+
+class TestSimpleTraceLoggingService(TestCase):
+    def setUp(self) -> None:
+        self.logger = mock.create_autospec(logging.Logger)
+        self.svc = SimpleTraceLoggingService()
+        self.svc.logger = self.logger
+
+    def test_write_checkpoint_simple(self) -> None:
+        # Arrange
+        expected_dump = json.dumps(
+            {
+                "operation": "write_checkpoint",
+                "run_id": "run123",
+                "instance_id": "instance456",
+                "checkpoint_name": "foo",
+                "status": str(CheckpointStatus.STARTED),
+            }
+        )
+
+        # Act
+        self.svc.write_checkpoint(
+            run_id="run123",
+            instance_id="instance456",
+            checkpoint_name="foo",
+            status=CheckpointStatus.STARTED,
+        )
+
+        # Assert
+        self.logger.info.assert_called_once_with(expected_dump)
+
+    def test_write_checkpoint_custom_data(self) -> None:
+        # Arrange
+        data = {"bar": "baz", "quux": "quuz"}
+        expected_dump = json.dumps(
+            {
+                "operation": "write_checkpoint",
+                "run_id": "run123",
+                "instance_id": "instance456",
+                "checkpoint_name": "foo",
+                "status": str(CheckpointStatus.STARTED),
+                "checkpoint_data": json.dumps(data),
+            }
+        )
+
+        # Act
+        self.svc.write_checkpoint(
+            run_id="run123",
+            instance_id="instance456",
+            checkpoint_name="foo",
+            status=CheckpointStatus.STARTED,
+            checkpoint_data=data,
+        )
+
+        # Assert
+        self.logger.info.assert_called_once_with(expected_dump)
