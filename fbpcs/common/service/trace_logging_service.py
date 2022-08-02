0230371c17f6a2c264a339b64fc9f069c8fcfcb8
Logan Gore <gorel@fb.com>
2022-08-02 16:14:38 -0700

    Create Trace Logging interface (#1379)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1379
    
    # What
    * Title
    # Why
    * https://docs.google.com/document/d/1X-iRE4L-_xHqr-Xvllz7SFofAA-F4DxE-iclhlUvSNA/edit#
    
    Reviewed By: joe1234wu
    
    Differential Revision: D38266613
    
    fbshipit-source-id: 5d27c287a5b2d7d8b3989d5b363035a0fc25d5ef

---
+++ fbpcs/common/service/trace_logging_service.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+import abc
+import logging
+from enum import auto, Enum
+from typing import Dict, Optional
+
+
+class CheckpointStatus(Enum):
+    STARTED = auto()
+    COMPLETED = auto()
+    FAILED = auto()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TraceLoggingService(abc.ABC):
+    def __init__(self) -> None:
+        self.logger: logging.Logger = logging.getLogger(__name__)
+
+    @abc.abstractmethod
+    def write_checkpoint(
+        self,
+        run_id: str,
+        instance_id: str,
+        checkpoint_name: str,
+        status: CheckpointStatus,
+        checkpoint_data: Optional[Dict[str, str]] = None,
+    ) -> None:
+        pass
