a5e0e2413f16f530bb96b7c0cdce85b1b560855a
Ajinkya Ghonge <ajinkyaghonge@fb.com>
2022-08-12 14:33:26 -0700

    Adding run_id to PC cli during PL instance creation. (#1383)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1383
    
    # Context
    For Centralized Logging, we will use a unique run_id to identify a PL/PA run, this run_id will be generated on the partner side and will be consumed by publisher side during instance creation. This run_id will be included in all the multiple ENT instances involved in the run and will also be shared with all the backend components (thrift, PCS, PCA binaries).
    # Design Doc
    https://docs.google.com/document/d/1vZNOq8GUT1D_7-MjSh2yNpUKVEsvpsYeFx1PjceyelQ/edit?usp=sharing
    RunID Format
    RunId will be in UUID format. Ex - 2621fda2-0eca-11ed-861d-0242ac120002
    # This Stack
    Add field run_id to private computation instance pattern.
    Add field run_id to PL instance creation endpoint.
    Add field run_id to PA instance creation endpoint.
    Add logic in PCS to get run_id using private computation cli for PL run.
    Add logic in PCS to pass run_id to PL creation API.
    Add logic in PCS to get run_id using private computation cli for PA run.
    Add logic in PCS to pass run_id to PA creation API.
    Add logic to pass run_id from www to thrift tier and save it in thrift entity.
    Add logic to pass run_id from thrift tier to PID/PCA binaries.
    # This diff
    Add logic in PCS to get run_id using private computation cli for PL run.
    
    Reviewed By: joe1234wu
    
    Differential Revision: D38304530
    
    fbshipit-source-id: 38a5f62a93eefc15eb6eaf5d168d1341c5d3fd85

--- fbpcs/private_computation/service/private_computation.py
+++ fbpcs/private_computation/service/private_computation.py
@@ -169,6 +169,7 @@ class PrivateComputationService:
         post_processing_data_optional: Optional[PostProcessingData] = None,
         pid_configs: Optional[Dict[str, Any]] = None,
         pcs_features: Optional[List[str]] = None,
+        run_id: Optional[str] = None,
     ) -> PrivateComputationInstance:
         self.logger.info(f"Creating instance: {instance_id}")
         self.metric_svc.bump_entity_key(PCSERVICE_ENTITY_NAME, "create_instance")
@@ -209,6 +210,7 @@ class PrivateComputationService:
             ),
             mpc_compute_concurrency=concurrency or DEFAULT_CONCURRENCY,
             status_updates=[],
+            run_id=run_id,
         )
         multikey_enabled = True
         if pid_configs and "multikey_enabled" in pid_configs.keys():
