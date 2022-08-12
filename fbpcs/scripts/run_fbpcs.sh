da3c7e302787babd05ea1db3aca81694a730c217
Shangguang Wu <shangg@fb.com>
2022-08-12 16:13:38 -0700

    not removing the docker image after study_run

--- fbpcs/scripts/run_fbpcs.sh
+++ fbpcs/scripts/run_fbpcs.sh
@@ -142,14 +142,14 @@ function run_fbpcs() {
     aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin "$FBPCS_CONTAINER_REPO_URL"
   fi
   docker pull "$docker_image"
-  docker run -e FBPCS_GRAPH_API_TOKEN --rm \
+  docker run -e FBPCS_GRAPH_API_TOKEN \
     -v "$real_config_path":"$DOCKER_CONFIG_PATH" \
     -v "$REAL_INSTANCE_REPO":"$DOCKER_INSTANCE_REPO" \
     -v "$REAL_CREDENTIALS_PATH":"$DOCKER_CREDENTIALS_PATH" \
     "${docker_image}" "${docker_cmd[@]}"
 
   echo "Checking the folder /fbpcs_instances"
-  docker run -e FBPCS_GRAPH_API_TOKEN --rm \
+  docker run -e FBPCS_GRAPH_API_TOKEN \
     -v "$real_config_path":"$DOCKER_CONFIG_PATH" \
     -v "$REAL_INSTANCE_REPO":"$DOCKER_INSTANCE_REPO" \
     -v "$REAL_CREDENTIALS_PATH":"$DOCKER_CREDENTIALS_PATH" \
