8e4c91ca7001ca12dedced3cfbf9b8db0e6cd1ac
Ankush Singh <ankushksingh@fb.com>
2022-08-15 09:51:44 -0700

    Adding unittest for function `get_command_list` (#1434)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1434
    
    Adding unittest for function `get_command_list` in class `TerraformDeploymentUtils`
    
    Code link for `get_command_list`: https://www.internalfb.com/code/fbsource/fbcode/fbpcs/infra/pce_deployment_library/deploy_library/terraform_library/terraform_deployment_utils.py?lines=62
    
    Reviewed By: gorel
    
    Differential Revision: D38668448
    
    fbshipit-source-id: cc991eb82465ae9ae661ac48cbc7f992f0a4a608

--- fbpcs/infra/pce_deployment_library/test/test_terraform_deployment_utils.py
+++ fbpcs/infra/pce_deployment_library/test/test_terraform_deployment_utils.py
@@ -6,6 +6,9 @@
 # pyre-strict
 
 import unittest
+from typing import Any, Dict
+
+from fbpcs.infra.pce_deployment_library.deploy_library.models import FlaggedOption
 
 from fbpcs.infra.pce_deployment_library.deploy_library.terraform_library.terraform_deployment_utils import (
     TerraformDeploymentUtils,
@@ -21,8 +24,73 @@ class TestTerraformDeploymentUtils(unittest.TestCase):
         pass
 
     def test_get_command_list(self) -> None:
-        # T125643785
-        pass
+        command: str = "terraform apply"
+        with self.subTest("OptionTypeDict"):
+            kwargs: Dict[str, Any] = {
+                "backend-config": {
+                    "region": "fake_region",
+                    "access_key": "fake_access_key",
+                }
+            }
+            expected_value = [
+                "terraform",
+                "apply",
+                '-backend-config "region=fake_region"',
+                '-backend-config "access_key=fake_access_key"',
+            ]
+            return_value = self.terraform_deployment_utils.get_command_list(
+                command, **kwargs
+            )
+            self.assertEqual(expected_value, return_value)
+
+        with self.subTest("OptionTypeList"):
+            kwargs: Dict[str, Any] = {"target": ["fake_region", "fake_access_key"]}
+            expected_value = [
+                "terraform",
+                "apply",
+                '-target="fake_region"',
+                '-target="fake_access_key"',
+            ]
+            return_value = self.terraform_deployment_utils.get_command_list(
+                command, **kwargs
+            )
+            self.assertEqual(expected_value, return_value)
+
+        with self.subTest("OptionTypeBool"):
+            kwargs: Dict[str, Any] = {"input": False}
+            expected_value = ["terraform", "apply", "-input=false"]
+            return_value = self.terraform_deployment_utils.get_command_list(
+                command, **kwargs
+            )
+            self.assertEqual(expected_value, return_value)
+
+        with self.subTest("OptionTypeFlaggedOption"):
+            kwargs: Dict[str, Any] = {"reconfigure": FlaggedOption}
+            expected_value = ["terraform", "apply", "-reconfigure"]
+            return_value = self.terraform_deployment_utils.get_command_list(
+                command, **kwargs
+            )
+            self.assertEqual(expected_value, return_value)
+
+        with self.subTest("OptionTypeDictWithArgs"):
+            kwargs: Dict[str, Any] = {
+                "backend-config": {
+                    "region": "fake_region",
+                    "access_key": "fake_access_key",
+                }
+            }
+            args = ("test_test",)
+            expected_value = [
+                "terraform",
+                "apply",
+                '-backend-config "region=fake_region"',
+                '-backend-config "access_key=fake_access_key"',
+                "test_test",
+            ]
+            return_value = self.terraform_deployment_utils.get_command_list(
+                command, *args, **kwargs
+            )
+            self.assertEqual(expected_value, return_value)
 
     def test_add_dict_options(self) -> None:
         pass
