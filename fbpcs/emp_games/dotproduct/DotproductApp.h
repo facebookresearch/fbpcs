2150007d7405af6cf32fcb1222940be70cde8a48
Tarek Elgamal <telgamal@fb.com>
2022-09-11 22:51:59 -0700

    Read raw Input in DotproductApp (#1543)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1543
    
    This diff reads the feature and labels to prepare for dotproduct computation. Both publisher and partner read csv files, the publisher file has features and labels while the partner has labels only.
    
    The code goes as follows:
     - Read csv input
     - for each line in the csv file find the features array and read it in a vector<double>, and find the label column and read it in a vector<bool>
    - Transpose the labels (this will be helpful in subsequent diffs when we do an OR function for all labels)
    
    Reviewed By: chualynn
    
    Differential Revision: D39163241
    
    fbshipit-source-id: 77302bcf389c2ad37980e404358713e8e23c75eb

--- fbpcs/emp_games/dotproduct/DotproductApp.h
+++ fbpcs/emp_games/dotproduct/DotproductApp.h
@@ -44,11 +44,88 @@ class DotproductApp {
     auto scheduler = fbpcf::scheduler::createLazySchedulerWithRealEngine(
         MY_ROLE, *communicationAgentFactory_);
 
-    XLOG(INFO) << "Starting Dotproduct App with role = " << MY_ROLE;
+    XLOG(INFO) << "Start Reading input file ";
+    auto inputTuple = readCSVInput(inputFilePath_, labelWidth_, numFeatures_);
+    XLOG(INFO) << "Finished Reading input file ";
+
+    XLOG(INFO) << "Number of feature rows " << std::get<0>(inputTuple).size();
+
+    XLOG(INFO) << "Number of label rows " << std::get<1>(inputTuple).size();
+    XLOG(INFO) << "Number of label columns "
+               << std::get<1>(inputTuple).at(0).size();
   }
 
   common::SchedulerStatistics getSchedulerStatistics() {
     return schedulerStatistics_;
+  }
+
+ protected:
+  std::tuple<std::vector<std::vector<double>>, std::vector<std::vector<bool>>>
+  readCSVInput(std::string inputPath, int labelWidth, int numFeatures) {
+    std::vector<std::vector<double>> allFeatures;
+    std::vector<std::vector<bool>> allLabels;
+    auto lineNo = 0;
+
+    bool success = private_measurement::csv::readCsv(
+        inputPath,
+        [&](const std::vector<std::string>& header,
+            const std::vector<std::string>& parts) {
+          if (lineNo == 0) {
+            XLOGF(DBG, "{}", common::vecToString(header));
+          }
+
+          auto [features, labels] =
+              parseLine(lineNo, header, parts, labelWidth, numFeatures);
+
+          // empty feature vector is not added for partner
+          if (features.size() != 0)
+            allFeatures.push_back(features);
+          allLabels.push_back(labels);
+          lineNo++;
+        });
+
+    return {allFeatures, transposeLabels(allLabels, labelWidth)};
+  }
+
+  std::tuple<std::vector<double>, std::vector<bool>> parseLine(
+      const int lineNo,
+      const std::vector<std::string>& header,
+      const std::vector<std::string>& parts,
+      int labelWidth,
+      int numFeatures) {
+    std::vector<double> features;
+    std::vector<bool> labels(labelWidth);
+
+    for (auto i = 0; i < header.size(); ++i) {
+      auto column = header[i];
+
+      if (column == "float_features") {
+        if (i < parts.size()) {
+          auto value = parts[i];
+          features = common::getInnerArray<double>(value);
+        } else {
+          features = std::vector<double>(numFeatures);
+        }
+
+      } else if (column == "label_secret_share") {
+        auto value = parts[i];
+        for (int j = 0; j < value.size(); j++) {
+          labels[j] = (value[j] == '1');
+        }
+      }
+    }
+    return {features, labels};
+  }
+
+  inline std::vector<std::vector<bool>> transposeLabels(
+      std::vector<std::vector<bool>> labels,
+      int labelWidth) {
+    std::vector<std::vector<bool>> transposedLabels(
+        labelWidth, std::vector<bool>(labels.size()));
+    for (int i = 0; i < labels.size(); i++)
+      for (int j = 0; j < labelWidth; j++)
+        transposedLabels[j][i] = labels[i][j];
+    return transposedLabels;
   }
 
  private:
