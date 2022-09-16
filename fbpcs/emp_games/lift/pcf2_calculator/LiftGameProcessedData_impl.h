e86c1cce327c9f82ae5a5efe7e7e7bf64d42484e
Tal Davidi <tdavidi@fb.com>
2022-09-16 08:21:39 -0700

    Serialize and Deserialize LiftGameProcessedData array values
    Differential Revision: https://internalfb.com/D39547913
    
    fbshipit-source-id: 015846b3b3afc3f28c4943aeb36647e428ef9aa0

--- fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData_impl.h
+++ fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData_impl.h
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <algorithm>
+#include <iterator>
 #include <string>
 
 #include "fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData.h"
@@ -27,6 +29,33 @@ void LiftGameProcessedData<schedulerId>::writeToCSV(
 
   private_measurement::csv::writeCsv(
       globalParamsOutputPath, GLOBAL_PARAMS_HEADER, globalParams);
+
+  std::vector<std::vector<std::string>> secretShares(numRows);
+
+  std::vector<uint64_t> opportunityTimestampsShares =
+      opportunityTimestamps.extractIntShare().getValue();
+  std::vector<bool> isValidOpportunityTimestampShares =
+      isValidOpportunityTimestamp.extractBit().getValue();
+  std::vector<bool> anyValidPurchaseTimestampShares =
+      anyValidPurchaseTimestamp.extractBit().getValue();
+  std::vector<bool> testReachShares = testReach.extractBit().getValue();
+
+  for (size_t i = 0; i < numRows; i++) {
+    secretShares[i] = std::vector<std::string>();
+    secretShares[i].reserve(SECRET_SHARES_HEADER.size());
+
+    // id_ column
+    secretShares[i].push_back(std::to_string(i));
+    secretShares[i].push_back(std::to_string(opportunityTimestampsShares[i]));
+    secretShares[i].push_back(
+        std::to_string(isValidOpportunityTimestampShares[i]));
+    secretShares[i].push_back(
+        std::to_string(anyValidPurchaseTimestampShares[i]));
+    secretShares[i].push_back(std::to_string(testReachShares[i]));
+  }
+
+  private_measurement::csv::writeCsv(
+      secretSharesOutputPath, SECRET_SHARES_HEADER, secretShares);
 }
 
 template <int schedulerId>
@@ -62,6 +91,53 @@ LiftGameProcessedData<schedulerId>::readFromCSV(
           }
         }
       });
+  std::vector<uint64_t> opportunityTimestampsShares;
+  std::vector<bool> isValidOpportunityTimestampShares;
+  std::vector<bool> anyValidPurchaseTimestampShares;
+  std::vector<bool> testReachShares;
+
+  private_measurement::csv::readCsv(
+      secretSharesInputPath,
+      [&result,
+       &opportunityTimestampsShares,
+       &isValidOpportunityTimestampShares,
+       &anyValidPurchaseTimestampShares,
+       &testReachShares](
+          const std::vector<std::string>& header,
+          const std::vector<std::string>& parts) {
+        result.numRows++;
+        for (size_t i = 0; i < header.size(); i++) {
+          auto column = header[i];
+          auto value = parts[i];
+          if (column == "opportunityTimestamps") {
+            opportunityTimestampsShares.push_back(std::stoull(value));
+          } else if (column == "isValidOpportunityTimestamp") {
+            isValidOpportunityTimestampShares.push_back(std::stoul(value));
+          } else if (column == "anyValidPurchaseTimestamp") {
+            anyValidPurchaseTimestampShares.push_back(std::stoul(value));
+          } else if (column == "testReach") {
+            testReachShares.push_back(std::stoul(value));
+          } else if (column != "id_") {
+            LOG(WARNING) << "Warning: Unknown column in csv: " << column;
+          }
+        }
+      });
+
+  if (result.numRows == 0) {
+    XLOG(FATAL, "Lift Game shares file was empty");
+  }
+
+  result.opportunityTimestamps = SecTimestamp<schedulerId>(
+      typename SecTimestamp<schedulerId>::ExtractedInt(
+          opportunityTimestampsShares));
+  result.isValidOpportunityTimestamp =
+      SecBit<schedulerId>(typename SecBit<schedulerId>::ExtractedBit(
+          isValidOpportunityTimestampShares));
+  result.anyValidPurchaseTimestamp =
+      SecBit<schedulerId>(typename SecBit<schedulerId>::ExtractedBit(
+          anyValidPurchaseTimestampShares));
+  result.testReach = SecBit<schedulerId>(
+      typename SecBit<schedulerId>::ExtractedBit(testReachShares));
 
   return result;
 }
