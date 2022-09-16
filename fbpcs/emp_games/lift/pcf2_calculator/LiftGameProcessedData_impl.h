c08b67339c49bf8903ad1cba542f1545be60cfbf
Tal Davidi <tdavidi@fb.com>
2022-09-16 08:21:39 -0700

    Serialize and Deserialize LiftGameProcessedData global params
    Differential Revision: https://internalfb.com/D39443227
    
    fbshipit-source-id: 9226daee3e295dd97b0dc3b96f0235b18571d427

--- fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData_impl.h
+++ fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData_impl.h
@@ -7,7 +7,8 @@
 
 #pragma once
 
-#include <stdexcept>
+#include <string>
+
 #include "fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData.h"
 
 namespace private_lift {
@@ -16,7 +17,16 @@ template <int schedulerId>
 void LiftGameProcessedData<schedulerId>::writeToCSV(
     const std::string& globalParamsOutputPath,
     const std::string& secretSharesOutputPath) const {
-  throw std::runtime_error("Unimplemented");
+  std::vector<std::vector<std::string>> globalParams = {
+      {std::to_string(numPartnerCohorts),
+       std::to_string(numPublisherBreakdowns),
+       std::to_string(numGroups),
+       std::to_string(numTestGroups),
+       std::to_string(valueBits),
+       std::to_string(valueSquaredBits)}};
+
+  private_measurement::csv::writeCsv(
+      globalParamsOutputPath, GLOBAL_PARAMS_HEADER, globalParams);
 }
 
 template <int schedulerId>
@@ -24,6 +34,35 @@ LiftGameProcessedData<schedulerId>
 LiftGameProcessedData<schedulerId>::readFromCSV(
     const std::string& globalParamsInputPath,
     const std::string& secretSharesInputPath) {
-  throw std::runtime_error("Unimplemented");
+  LiftGameProcessedData<schedulerId> result;
+  result.numRows = 0;
+
+  private_measurement::csv::readCsv(
+      globalParamsInputPath,
+      [&result](
+          const std::vector<std::string>& header,
+          const std::vector<std::string>& parts) {
+        for (size_t i = 0; i < header.size(); i++) {
+          auto column = header[i];
+          auto value = parts[i];
+          if (column == "numPartnerCohorts") {
+            result.numPartnerCohorts = std::stoul(value);
+          } else if (column == "numPublisherBreakdowns") {
+            result.numPublisherBreakdowns = std::stoul(value);
+          } else if (column == "numGroups") {
+            result.numGroups = std::stoul(value);
+          } else if (column == "numTestGroups") {
+            result.numTestGroups = std::stoul(value);
+          } else if (column == "valueBits") {
+            result.valueBits = std::stoul(value);
+          } else if (column == "valueSquaredBits") {
+            result.valueSquaredBits = std::stoul(value);
+          } else {
+            LOG(WARNING) << "Warning: Unknown column in csv: " << column;
+          }
+        }
+      });
+
+  return result;
 }
 } // namespace private_lift
