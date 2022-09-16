19c40345e7ce695151b35b567328d0a139f55b74
Tal Davidi <tdavidi@fb.com>
2022-09-16 08:22:12 -0700

    Serialize and Deserialize LiftGameProcessedData nested array values (#1583)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1583
    
    This diff adds serialization and deserialization of the following values in LiftGameProcessedData:
    
      std::vector<SecTimestamp<schedulerId>> purchaseTimestamps;
      std::vector<SecTimestamp<schedulerId>> thresholdTimestamps;
      std::vector<SecValue<schedulerId>> purchaseValues;
      std::vector<SecValueSquared<schedulerId>> purchaseValueSquared;
    
    The output format for each row will be a array of shares (e.g.)
    
    "[12315,3241,61234,-1541,12341,1231,5341]" for each share. Timestamps are read as unsigned values and purchase values as signed.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D39547912
    
    fbshipit-source-id: 6cad32c909563350c24c7d23fc6c42793f370c78

--- fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData_impl.h
+++ fbpcs/emp_games/lift/pcf2_calculator/LiftGameProcessedData_impl.h
@@ -36,8 +36,40 @@ void LiftGameProcessedData<schedulerId>::writeToCSV(
       opportunityTimestamps.extractIntShare().getValue();
   std::vector<bool> isValidOpportunityTimestampShares =
       isValidOpportunityTimestamp.extractBit().getValue();
+  std::vector<std::vector<uint64_t>> purchaseTimestampShares;
+  std::transform(
+      purchaseTimestamps.begin(),
+      purchaseTimestamps.end(),
+      std::back_inserter(purchaseTimestampShares),
+      [](const SecTimestamp<schedulerId>& purchaseTimestamp) {
+        return purchaseTimestamp.extractIntShare().getValue();
+      });
+  std::vector<std::vector<uint64_t>> thresholdTimestampShares;
+  std::transform(
+      thresholdTimestamps.begin(),
+      thresholdTimestamps.end(),
+      std::back_inserter(thresholdTimestampShares),
+      [](const SecTimestamp<schedulerId>& thresholdTimestamp) {
+        return thresholdTimestamp.extractIntShare().getValue();
+      });
   std::vector<bool> anyValidPurchaseTimestampShares =
       anyValidPurchaseTimestamp.extractBit().getValue();
+  std::vector<std::vector<int64_t>> purchaseValueShares;
+  std::transform(
+      purchaseValues.begin(),
+      purchaseValues.end(),
+      std::back_inserter(purchaseValueShares),
+      [](const SecValue<schedulerId>& purchaseValue) {
+        return purchaseValue.extractIntShare().getValue();
+      });
+  std::vector<std::vector<int64_t>> purchaseValueSquaredShares;
+  std::transform(
+      purchaseValueSquared.begin(),
+      purchaseValueSquared.end(),
+      std::back_inserter(purchaseValueSquaredShares),
+      [](const SecValueSquared<schedulerId>& purchaseValueSquared) {
+        return purchaseValueSquared.extractIntShare().getValue();
+      });
   std::vector<bool> testReachShares = testReach.extractBit().getValue();
 
   for (size_t i = 0; i < numRows; i++) {
@@ -49,13 +81,50 @@ void LiftGameProcessedData<schedulerId>::writeToCSV(
     secretShares[i].push_back(std::to_string(opportunityTimestampsShares[i]));
     secretShares[i].push_back(
         std::to_string(isValidOpportunityTimestampShares[i]));
+    secretShares[i].push_back(joinColumn(purchaseTimestampShares, i));
+    secretShares[i].push_back(joinColumn(thresholdTimestampShares, i));
     secretShares[i].push_back(
         std::to_string(anyValidPurchaseTimestampShares[i]));
+    secretShares[i].push_back(joinColumn(purchaseValueShares, i));
+    secretShares[i].push_back(joinColumn(purchaseValueSquaredShares, i));
     secretShares[i].push_back(std::to_string(testReachShares[i]));
   }
 
   private_measurement::csv::writeCsv(
       secretSharesOutputPath, SECRET_SHARES_HEADER, secretShares);
+}
+
+template <int schedulerId>
+template <typename T>
+std::string LiftGameProcessedData<schedulerId>::joinColumn(
+    const std::vector<std::vector<T>>& data,
+    size_t columnIndex) {
+  if (data.size() == 0) {
+    return "[]";
+  } else if (data.size() == 1) {
+    return "[" + std::to_string(data[0][columnIndex]) + "]";
+  } else {
+    std::string result = "[";
+    for (size_t row = 0; row < data.size() - 1; row++) {
+      result += std::to_string(data[row][columnIndex]) + ",";
+    }
+
+    result += std::to_string(data[data.size() - 1][columnIndex]) + "]";
+    return result;
+  }
+}
+
+template <int schedulerId>
+template <typename T>
+std::vector<T> LiftGameProcessedData<schedulerId>::extractColumn(
+    const std::vector<std::vector<T>>& data,
+    size_t columnIndex) {
+  std::vector<T> result;
+  result.reserve(data.size());
+  for (size_t row = 0; row < data.size(); row++) {
+    result.push_back(data[row][columnIndex]);
+  }
+  return result;
 }
 
 template <int schedulerId>
@@ -93,7 +162,11 @@ LiftGameProcessedData<schedulerId>::readFromCSV(
       });
   std::vector<uint64_t> opportunityTimestampsShares;
   std::vector<bool> isValidOpportunityTimestampShares;
+  std::vector<std::vector<uint64_t>> purchaseTimestampShares;
+  std::vector<std::vector<uint64_t>> thresholdTimestampShares;
   std::vector<bool> anyValidPurchaseTimestampShares;
+  std::vector<std::vector<int64_t>> purchaseValueShares;
+  std::vector<std::vector<int64_t>> purchaseValueSquaredShares;
   std::vector<bool> testReachShares;
 
   private_measurement::csv::readCsv(
@@ -101,7 +174,11 @@ LiftGameProcessedData<schedulerId>::readFromCSV(
       [&result,
        &opportunityTimestampsShares,
        &isValidOpportunityTimestampShares,
+       &purchaseTimestampShares,
+       &thresholdTimestampShares,
        &anyValidPurchaseTimestampShares,
+       &purchaseValueShares,
+       &purchaseValueSquaredShares,
        &testReachShares](
           const std::vector<std::string>& header,
           const std::vector<std::string>& parts) {
@@ -113,8 +190,33 @@ LiftGameProcessedData<schedulerId>::readFromCSV(
             opportunityTimestampsShares.push_back(std::stoull(value));
           } else if (column == "isValidOpportunityTimestamp") {
             isValidOpportunityTimestampShares.push_back(std::stoul(value));
+          } else if (column == "purchaseTimestamps") {
+            purchaseTimestampShares.emplace_back();
+            for (const auto& purchaseTimestampShare : splitValueArray(value)) {
+              purchaseTimestampShares.back().push_back(
+                  std::stoull(purchaseTimestampShare));
+            }
+          } else if (column == "thresholdTimestamps") {
+            thresholdTimestampShares.emplace_back();
+            for (const auto& thresholdTimestampShare : splitValueArray(value)) {
+              thresholdTimestampShares.back().push_back(
+                  std::stoull(thresholdTimestampShare));
+            }
           } else if (column == "anyValidPurchaseTimestamp") {
             anyValidPurchaseTimestampShares.push_back(std::stoul(value));
+          } else if (column == "purchaseValues") {
+            purchaseValueShares.emplace_back();
+            for (const auto& purchaseValueShare : splitValueArray(value)) {
+              purchaseValueShares.back().push_back(
+                  std::stoll(purchaseValueShare));
+            }
+          } else if (column == "purchaseValueSquared") {
+            purchaseValueSquaredShares.emplace_back();
+            for (const auto& purchaseValueSquaredShare :
+                 splitValueArray(value)) {
+              purchaseValueSquaredShares.back().push_back(
+                  std::stoll(purchaseValueSquaredShare));
+            }
           } else if (column == "testReach") {
             testReachShares.push_back(std::stoul(value));
           } else if (column != "id_") {
@@ -133,12 +235,55 @@ LiftGameProcessedData<schedulerId>::readFromCSV(
   result.isValidOpportunityTimestamp =
       SecBit<schedulerId>(typename SecBit<schedulerId>::ExtractedBit(
           isValidOpportunityTimestampShares));
+
+  result.purchaseTimestamps = std::vector<SecTimestamp<schedulerId>>();
+  result.purchaseTimestamps.reserve(purchaseTimestampShares[0].size());
+  for (size_t i = 0; i < purchaseTimestampShares[0].size(); i++) {
+    result.purchaseTimestamps.push_back(SecTimestamp<schedulerId>(
+        typename SecTimestamp<schedulerId>::ExtractedInt(
+            extractColumn(purchaseTimestampShares, i))));
+  }
+
+  result.thresholdTimestamps = std::vector<SecTimestamp<schedulerId>>();
+  result.thresholdTimestamps.reserve(thresholdTimestampShares[0].size());
+  for (size_t i = 0; i < thresholdTimestampShares[0].size(); i++) {
+    result.thresholdTimestamps.push_back(SecTimestamp<schedulerId>(
+        typename SecTimestamp<schedulerId>::ExtractedInt(
+            extractColumn(thresholdTimestampShares, i))));
+  }
+
   result.anyValidPurchaseTimestamp =
       SecBit<schedulerId>(typename SecBit<schedulerId>::ExtractedBit(
           anyValidPurchaseTimestampShares));
+
+  result.purchaseValues = std::vector<SecValue<schedulerId>>();
+  result.purchaseValues.reserve(purchaseValueShares[0].size());
+  for (size_t i = 0; i < purchaseValueShares[0].size(); i++) {
+    result.purchaseValues.push_back(
+        SecValue<schedulerId>(typename SecValue<schedulerId>::ExtractedInt(
+            extractColumn(purchaseValueShares, i))));
+  }
+
+  result.purchaseValueSquared = std::vector<SecValueSquared<schedulerId>>();
+  result.purchaseValueSquared.reserve(purchaseValueSquaredShares[0].size());
+  for (size_t i = 0; i < purchaseValueSquaredShares[0].size(); i++) {
+    result.purchaseValueSquared.push_back(SecValueSquared<schedulerId>(
+        typename SecValueSquared<schedulerId>::ExtractedInt(
+            extractColumn(purchaseValueSquaredShares, i))));
+  }
+
   result.testReach = SecBit<schedulerId>(
       typename SecBit<schedulerId>::ExtractedBit(testReachShares));
 
   return result;
+}
+
+template <int schedulerId>
+std::vector<std::string> LiftGameProcessedData<schedulerId>::splitValueArray(
+    const std::string& str) {
+  auto innerString = str.substr(1, str.size() - 1);
+  std::vector<std::string> values =
+      private_measurement::csv::splitByComma(innerString, false);
+  return values;
 }
 } // namespace private_lift
