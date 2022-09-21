2225a38d712b50a2a1344411b0ffebe1c6d1a2d6
Tal Davidi <tdavidi@fb.com>
2022-09-21 10:04:46 -0700

    Add SS -> SS Input processor (#1602)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1602
    
    Adds an explicit Input Processor for reading secret shares locally. This will replace the current InputProcessor in the existing lift stage in the UDP flow
    
    Reviewed By: mdas7, RuiyuZhu
    
    Differential Revision: D39638698
    
    fbshipit-source-id: 3f0e302c91a1687155fb1aba3f48e53a2a2ffe88

---
+++ fbpcs/emp_games/lift/pcf2_calculator/SecretShareInputProcessor.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "fbpcs/emp_games/lift/pcf2_calculator/IInputProcessor.h"
+
+#pragma once
+
+namespace private_lift {
+
+template <int schedulerId>
+class SecretShareInputProcessor : public IInputProcessor<schedulerId> {
+ public:
+  SecretShareInputProcessor(
+      const std::string& globalParamsPath,
+      const std::string& secretSharePath)
+      : liftGameProcessedData_{LiftGameProcessedData<schedulerId>::readFromCSV(
+            globalParamsPath,
+            secretSharePath)} {}
+
+  SecretShareInputProcessor() {}
+
+  const LiftGameProcessedData<schedulerId>& getLiftGameProcessedData() const {
+    return liftGameProcessedData_;
+  }
+
+ private:
+  LiftGameProcessedData<schedulerId> liftGameProcessedData_;
+};
+
+} // namespace private_lift
