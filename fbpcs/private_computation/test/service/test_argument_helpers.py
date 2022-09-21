ca4bb0e95503ff61f82d39d812f38383c7843032
Aditya Shastri <adshastri@fb.com>
2022-09-21 13:26:56 -0700

    Add helper function for compiling TLS arguments
    Summary:
    All PCF2 stage services will need to put together TLS arguments. We define a simple helper function that can be reused by all stage services
    
    input - if the TLS feature is enabled
    output - the argument dict
    
    Reviewed By: danbunnell
    
    Differential Revision: D39636352
    
    fbshipit-source-id: 204db23148eb253a5662dd52241aedc53caba80e

---
+++ fbpcs/private_computation/test/service/test_argument_helpers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env fbpython
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from unittest import TestCase
+
+from fbpcs.private_computation.service.argument_helper import get_tls_arguments
+from fbpcs.private_computation.service.constants import (
+    CA_CERT_PATH,
+    PRIVATE_KEY_PATH,
+    SERVER_CERT_PATH,
+)
+
+
+class TestArgumentHelpers(TestCase):
+    def test_get_tls_arguments_no_feature(self):
+        tls_arguments = get_tls_arguments(False)
+
+        self.assertFalse(tls_arguments["use_tls"])
+        self.assertEqual(tls_arguments["ca_cert_path"], "")
+        self.assertEqual(tls_arguments["server_cert_path"], "")
+        self.assertEqual(tls_arguments["private_key_path"], "")
+
+    def test_get_tls_arguments_with_feature(self):
+        tls_arguments = get_tls_arguments(True)
+
+        self.assertTrue(tls_arguments["use_tls"])
+        self.assertEqual(tls_arguments["ca_cert_path"], CA_CERT_PATH)
+        self.assertEqual(tls_arguments["server_cert_path"], SERVER_CERT_PATH)
+        self.assertEqual(tls_arguments["private_key_path"], PRIVATE_KEY_PATH)
