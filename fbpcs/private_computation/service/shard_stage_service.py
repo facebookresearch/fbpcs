a6da90c78d842234f6203db4e20bbf97f329288e
Tal Davidi <tdavidi@fb.com>
2022-10-03 16:18:35 -0700

    Delete prepare_data_stage_service (#1668)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1668
    
    PrepareDataStageService has been split into two stages (IdSpineCombinerService and ShardDataStageService). This was done in D34976330 (https://github.com/facebookresearch/fbpcs/commit/36bb58b74540a00930f082abe292667cb149617f) and D35060817 (https://github.com/facebookresearch/fbpcs/commit/a9f45b59fad14a9ff56f6a460ca483c4c0b8d80b). I need to change the logic in the utils file, so I am moving them back into the respective stages.
    
    Reviewed By: joe1234wu
    
    Differential Revision: D39931315
    
    fbshipit-source-id: 5f4cc0153b07193736f27c0e68922c41bfa0ab18

--- fbpcs/private_computation/service/shard_stage_service.py
+++ fbpcs/private_computation/service/shard_stage_service.py
@@ -8,23 +8,29 @@
 
 
 import logging
+import math
 from typing import DefaultDict, List, Optional
+
+from fbpcp.entity.container_instance import ContainerInstance
 
 from fbpcp.service.onedocker import OneDockerService
 from fbpcs.common.entity.stage_state_instance import StageStateInstance
-from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
+from fbpcs.data_processing.service.sharding_service import ShardingService, ShardType
+from fbpcs.onedocker_binary_config import (
+    ONEDOCKER_REPOSITORY_PATH,
+    OneDockerBinaryConfig,
+)
+from fbpcs.onedocker_binary_names import OneDockerBinaryNames
 from fbpcs.private_computation.entity.private_computation_instance import (
     PrivateComputationInstance,
     PrivateComputationInstanceStatus,
     PrivateComputationRole,
 )
+from fbpcs.private_computation.service.pid_utils import get_sharded_filepath
 from fbpcs.private_computation.service.private_computation_stage_service import (
     PrivateComputationStageService,
 )
-from fbpcs.private_computation.service.utils import (
-    get_pc_status_from_stage_state,
-    start_sharder_service,
-)
+from fbpcs.private_computation.service.utils import get_pc_status_from_stage_state
 
 
 class ShardStageService(PrivateComputationStageService):
@@ -71,7 +77,7 @@ class ShardStageService(PrivateComputationStageService):
         should_wait_spin_up: bool = (
             pc_instance.infra_config.role is PrivateComputationRole.PARTNER
         )
-        container_instances = await start_sharder_service(
+        container_instances = await self._start_sharder_service(
             pc_instance,
             self._onedocker_svc,
             self._onedocker_binary_config_map,
@@ -102,3 +108,75 @@ class ShardStageService(PrivateComputationStageService):
             The latest status for private_computation_instance
         """
         return get_pc_status_from_stage_state(pc_instance, self._onedocker_svc)
+
+    async def _start_sharder_service(
+        self,
+        private_computation_instance: PrivateComputationInstance,
+        onedocker_svc: OneDockerService,
+        onedocker_binary_config_map: DefaultDict[str, OneDockerBinaryConfig],
+        combine_output_path: str,
+        wait_for_containers: bool = False,
+        wait_for_containers_to_start_up: bool = True,
+    ) -> List[ContainerInstance]:
+        """Run combiner service and return those container instances
+
+        Args:
+            private_computation_instance: The PC instance to run sharder service with
+            onedocker_svc: Spins up containers that run binaries in the cloud
+            onedocker_binary_config_map: Stores a mapping from mpc game to OneDockerBinaryConfig (binary version and tmp directory)
+            combine_output_path: out put path for the combine result
+            wait_for_containers: block until containers to finish running, default False
+
+        Returns:
+            return: list of container instances running combiner service
+        """
+        sharder = ShardingService()
+        logging.info("Instantiated sharder")
+
+        args_list = []
+        for shard_index in range(
+            private_computation_instance.infra_config.num_pid_containers
+        ):
+            path_to_shard = get_sharded_filepath(combine_output_path, shard_index)
+            logging.info(f"Input path to sharder: {path_to_shard}")
+
+            shards_per_file = math.ceil(
+                (
+                    private_computation_instance.infra_config.num_mpc_containers
+                    / private_computation_instance.infra_config.num_pid_containers
+                )
+                * private_computation_instance.infra_config.num_files_per_mpc_container
+            )
+            shard_index_offset = shard_index * shards_per_file
+            logging.info(
+                f"Output base path to sharder: {private_computation_instance.data_processing_output_path}, {shard_index_offset=}"
+            )
+
+            binary_config = onedocker_binary_config_map[
+                OneDockerBinaryNames.SHARDER.value
+            ]
+            args_per_shard = sharder.build_args(
+                filepath=path_to_shard,
+                output_base_path=private_computation_instance.data_processing_output_path,
+                file_start_index=shard_index_offset,
+                num_output_files=shards_per_file,
+                tmp_directory=binary_config.tmp_directory,
+            )
+            args_list.append(args_per_shard)
+
+        binary_name = sharder.get_binary_name(ShardType.ROUND_ROBIN)
+        env_vars = {}
+        if binary_config.repository_path:
+            env_vars[ONEDOCKER_REPOSITORY_PATH] = binary_config.repository_path
+
+        return await sharder.start_containers(
+            cmd_args_list=args_list,
+            onedocker_svc=onedocker_svc,
+            binary_version=binary_config.binary_version,
+            binary_name=binary_name,
+            timeout=None,
+            wait_for_containers_to_finish=wait_for_containers,
+            env_vars=env_vars,
+            wait_for_containers_to_start_up=wait_for_containers_to_start_up,
+            existing_containers=private_computation_instance.get_existing_containers_for_retry(),
+        )
