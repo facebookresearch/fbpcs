0f30bd5fdea101924d0a5e4d543cf5d1cd01639a
Tal Davidi <tdavidi@fb.com>
2022-10-04 13:54:28 -0700

    Add new PCS stage service to run metadata compaction (Lift UDP) binary
    Differential Revision: https://www.internalfb.com/diff/D39747757?entry_point=27
    
    fbshipit-source-id: c55b9b4ae17f3533af6a323e33868fef85afa121

--- fbpcs/private_computation/service/private_computation_service_data.py
+++ fbpcs/private_computation/service/private_computation_service_data.py
@@ -109,6 +109,14 @@ class PrivateComputationServiceData:
         service=None,
     )
 
+    PCF2_LIFT_METADATA_COMPACTION_DATA: StageData = StageData(
+        binary_name=OneDockerBinaryNames.PCF2_LIFT_METADATA_COMPACTION.value,
+        game_name=BINARY_NAME_TO_GAME_NAME[
+            OneDockerBinaryNames.PCF2_LIFT_METADATA_COMPACTION.value
+        ],
+        service=None,
+    )
+
     PCF2_SHARD_COMBINE_STAGE_DATA: StageData = StageData(
         binary_name=OneDockerBinaryNames.PCF2_SHARD_COMBINER.value,
         game_name=BINARY_NAME_TO_GAME_NAME[
