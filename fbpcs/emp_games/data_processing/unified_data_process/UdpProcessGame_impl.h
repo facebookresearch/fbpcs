6ca5b350afd24afbed3e192e6b3406f6146e25d5
Chen Yuan <chenyc@meta.com>
2022-10-12 14:29:14 -0700

    Add binary of UDP library (#1715)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1715
    
    This diff added the binary of UDP library as an emp game under ``//fbpcs/emp_games/data_processing/unified_data_process``.
     1. adds `UdpProcessGame.h` and `UdpProcessGame_impl.h` to implement a game for udp adapter and data processor.
    2. adds `UdpProcessGameFactory.h` to create `UdpProcessGame`.
    3. adds `UdpProcessApp.h` and `UdpProcessApp_impl.h` to wrap the `UdpProcessGame`.
    4. adds a test file to test the `UdpProcessApp`
    
    This game mainly expect four input parameters: ``party`` to indicate whether running the game as publisher or partner; ``row_number`` represents the size of ``unionMap`` from PID stage. ``row_size`` reoresents the number of bytes in a row of metadata. ``intersection`` represents the number of instersection in the ``unionMap``.
    
    The ``UdpProcessApp`` will generate some random data based on the above input parameters and feed them into the ``UdpProcessGame``. In more detail, the ``dataGeneration()`` methods will generate a ``unionMap`` to simulate the results from PID stage and ``metadata`` to simulate each party's meta data.
    
    ``unionMap`` will be an integer vector with size ``row_number``. On publisher side, the first ``intersection`` rows in the ``unionMap`` will be positive indexes and the rest will be 0. On the partner side, the first ``intersection`` rows in the ``unionMap`` will be positive indexes and the rest will be -1. In this way, the top ``intersection`` rows in the ``unionMap`` represents the intersection.
    
    Reviewed By: robotal
    
    Differential Revision: D39939986
    
    fbshipit-source-id: e7fe3209e251e9bc78b3604e6f505b8454af2764

---
+++ fbpcs/emp_games/data_processing/unified_data_process/UdpProcessGame_impl.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <vector>
+
+#include "fbpcs/data_processing/unified_data_process/adapter/Adapter.h"
+#include "fbpcs/data_processing/unified_data_process/adapter/Adapter_impl.h"
+#include "fbpcs/data_processing/unified_data_process/adapter/IAdapter.h"
+#include "fbpcs/data_processing/unified_data_process/data_processor/DataProcessor.h"
+#include "fbpcs/data_processing/unified_data_process/data_processor/DataProcessor_impl.h"
+#include "fbpcs/data_processing/unified_data_process/data_processor/IDataProcessor.h"
+#include "fbpcs/emp_games/data_processing/unified_data_process/UdpProcessGame.h"
+
+namespace unified_data_process {
+
+template <int schedulerId>
+std::vector<int64_t> UdpProcessGame<schedulerId>::playAdapter(
+    const std::vector<int64_t>& unionMap) {
+  auto adapter = adapterFactory_->create();
+  return adapter->adapt(unionMap);
+}
+
+template <int schedulerId>
+std::tuple<std::vector<std::vector<bool>>, std::vector<std::vector<bool>>>
+UdpProcessGame<schedulerId>::playDataProcessor(
+    const std::vector<std::vector<unsigned char>>& metaData,
+    const std::vector<int64_t>& indexes,
+    size_t peersDataSize,
+    size_t peersDataWidth) {
+  size_t intersectionSize = indexes.size();
+  auto dataProcessor = dataProcessorFactory_->create();
+  typename UdpProcessGame<schedulerId>::SecString publisherShares;
+  typename UdpProcessGame<schedulerId>::SecString advertiserShares;
+  if (myId_ == common::PUBLISHER) {
+    XLOG(INFO) << "Start to process my data...";
+    publisherShares = dataProcessor->processMyData(metaData, intersectionSize);
+    XLOG(INFO) << "Start to process peer's data...";
+    advertiserShares =
+        dataProcessor->processPeersData(peersDataSize, indexes, peersDataWidth);
+  } else {
+    XLOG(INFO) << "Start to process peer's data...";
+    publisherShares =
+        dataProcessor->processPeersData(peersDataSize, indexes, peersDataWidth);
+    XLOG(INFO) << "Start to process my data...";
+    advertiserShares = dataProcessor->processMyData(metaData, intersectionSize);
+  }
+  std::vector<std::vector<bool>> publisherRawShare(
+      publisherShares.size(),
+      std::vector<bool>(publisherShares.getBatchSize()));
+  std::vector<std::vector<bool>> advertiserRawShare(
+      advertiserShares.size(),
+      std::vector<bool>(advertiserShares.getBatchSize()));
+
+  auto extractPublisherString = publisherShares.extractStringShare();
+  auto extractAdvertiserString = advertiserShares.extractStringShare();
+
+  for (size_t i = 0; i < extractPublisherString.size(); ++i) {
+    publisherRawShare[i] = extractPublisherString[i].getValue();
+  }
+  for (size_t i = 0; i < extractAdvertiserString.size(); ++i) {
+    advertiserRawShare[i] = extractAdvertiserString[i].getValue();
+  }
+
+  return {publisherRawShare, advertiserRawShare};
+}
+} // namespace unified_data_process
---
+++ fbpcs/emp_games/data_processing/unified_data_process/test/UdpProcessAppTest.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+#include <filesystem>
+#include <memory>
+#include "folly/Format.h"
+#include "folly/Random.h"
+
+#include "fbpcf/engine/communication/test/AgentFactoryCreationHelper.h"
+#include "fbpcf/scheduler/ISchedulerFactory.h"
+#include "fbpcf/test/TestHelper.h"
+#include "fbpcs/emp_games/data_processing/unified_data_process/UdpProcessApp.h"
+#include "fbpcs/emp_games/data_processing/unified_data_process/UdpProcessGameFactory.h"
+
+namespace unified_data_process {
+template <int schedulerId>
+std::tuple<std::vector<std::vector<bool>>, std::vector<std::vector<bool>>>
+runUdpProcessApp(
+    int myId,
+    int64_t rowNumber,
+    int64_t rowSize,
+    int64_t intersectionSize,
+    std::shared_ptr<
+        fbpcf::engine::communication::IPartyCommunicationAgentFactory>
+        communicationAgentFactory,
+    std::shared_ptr<fbpcf::util::MetricCollector> metricCollector,
+    std::unique_ptr<UdpProcessGameFactory<schedulerId>> udpGameFactory) {
+  UdpProcessApp<schedulerId> app(
+      myId,
+      std::move(communicationAgentFactory),
+      std::move(metricCollector),
+      std::move(udpGameFactory),
+      rowNumber,
+      rowSize,
+      intersectionSize);
+  return app.run();
+}
+
+std::vector<std::vector<uint8_t>> reconstructResults(
+    std::vector<std::vector<bool>>& booleanShares0,
+    std::vector<std::vector<bool>>& booleanShares1) {
+  std::vector<std::vector<bool>> booleanShares(
+      booleanShares0.size(), std::vector<bool>(booleanShares0.at(0).size()));
+  for (size_t i = 0; i < booleanShares0.size(); ++i) {
+    for (size_t j = 0; j < booleanShares0.at(0).size(); ++j) {
+      booleanShares[i][j] = booleanShares0[i][j] ^ booleanShares1[i][j];
+    }
+  }
+  std::vector<std::vector<uint8_t>> reconstructedData(
+      booleanShares.at(0).size(),
+      std::vector<uint8_t>(booleanShares.size() / 8));
+
+  for (size_t i = 0; i < booleanShares.size() / 8; i++) {
+    for (uint8_t j = 0; j < 8; j++) {
+      for (size_t k = 0; k < booleanShares.at(0).size(); k++) {
+        reconstructedData[k][i] += (booleanShares.at(i * 8 + j).at(k) << j);
+      }
+    }
+  }
+
+  return reconstructedData;
+}
+
+void checkOutput(
+    const std::vector<std::vector<uint8_t>>& publisherData,
+    const std::vector<std::vector<uint8_t>>& partnerData,
+    int64_t row_size,
+    int64_t intersectionSize) {
+  ASSERT_EQ(publisherData.size(), intersectionSize);
+  ASSERT_EQ(partnerData.size(), intersectionSize);
+  ASSERT_EQ(publisherData.at(0).size(), row_size);
+  ASSERT_EQ(partnerData.at(0).size(), row_size);
+
+  // The intersected meta data on both
+  // party were set to be the same for the ease of
+  // correctness verification.
+  EXPECT_EQ(publisherData, partnerData);
+}
+
+TEST(UdpProcessApp, testUdpProcessApp) {
+  std::random_device rd;
+  std::mt19937_64 e(rd());
+  std::uniform_int_distribution<int32_t> randomRowNum(100, 0xFF);
+  std::uniform_int_distribution<int32_t> randomRowSize(64, 80);
+  std::uniform_int_distribution<uint8_t> randomRate(1, 20);
+  int64_t rowNumber = randomRowNum(e);
+  int64_t rowSize = randomRowSize(e);
+  double intsectionRate = randomRate(e);
+  int64_t intersectionSize = (intsectionRate / 100) * rowNumber;
+
+  auto agentFactories =
+      fbpcf::engine::communication::getInMemoryAgentFactory(2);
+  fbpcf::setupRealBackend<0, 1>(*agentFactories[0], *agentFactories[1]);
+  auto udpGameFactory0 =
+      std::make_unique<UdpProcessGameFactory<0>>(0, *agentFactories[0]);
+  auto udpGameFactory1 =
+      std::make_unique<UdpProcessGameFactory<1>>(1, *agentFactories[1]);
+
+  auto metricCollector0 =
+      std::make_shared<fbpcf::util::MetricCollector>("attribution_test_0");
+
+  auto metricCollector1 =
+      std::make_shared<fbpcf::util::MetricCollector>("attribution_test_1");
+
+  auto future0 = std::async(
+      runUdpProcessApp<0>,
+      0,
+      rowNumber,
+      rowSize,
+      intersectionSize,
+      std::move(agentFactories[0]),
+      std::move(metricCollector0),
+      std::move(udpGameFactory0));
+  auto future1 = std::async(
+      runUdpProcessApp<1>,
+      1,
+      rowNumber,
+      rowSize,
+      intersectionSize,
+      std::move(agentFactories[1]),
+      std::move(metricCollector1),
+      std::move(udpGameFactory1));
+
+  auto sharesOutput0 = future0.get();
+  auto sharesOutput1 = future1.get();
+
+  auto& publisherDataShares0 = std::get<0>(sharesOutput0);
+  auto& partnerDataShares0 = std::get<1>(sharesOutput0);
+
+  auto& publisherDataShares1 = std::get<0>(sharesOutput1);
+  auto& partnerDataShares1 = std::get<1>(sharesOutput1);
+
+  auto publisherData =
+      reconstructResults(publisherDataShares0, publisherDataShares1);
+  auto partnerData = reconstructResults(partnerDataShares0, partnerDataShares1);
+  checkOutput(publisherData, partnerData, rowSize, intersectionSize);
+}
+
+} // namespace unified_data_process
