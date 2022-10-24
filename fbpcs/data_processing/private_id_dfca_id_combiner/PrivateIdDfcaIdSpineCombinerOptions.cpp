066799ee1b13ccc88f182cbb5b9ba07125aba57f
ma9o <ma9o@fb.com>
2022-10-24 01:57:32 -0700

    Add spine combiner binary
    Differential Revision: D39926640
    
    fbshipit-source-id: bc5279ad9040c740e4d895ba348aa76d7d2bfb30

---
+++ fbpcs/data_processing/private_id_dfca_id_combiner/PrivateIdDfcaIdSpineCombinerOptions.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gflags/gflags.h>
+
+#include "fbpcs/data_processing/private_id_dfca_id_combiner/PrivateIdDfcaIdSpineCombinerOptions.h"
+
+DEFINE_string(spine_path, "", "File path which contains the identity spine");
+DEFINE_string(data_path, "", "File path which contains the data file");
+DEFINE_string(
+    output_path,
+    "",
+    "File path with combined output from the identity spine");
+DEFINE_string(
+    tmp_directory,
+    "/tmp/",
+    "Directory where temporary files should be saved before final write");
+DEFINE_string(run_name, "", "User given name used to write cost info in S3");
+DEFINE_string(
+    sort_strategy,
+    "sort",
+    "Sorting strategy selected for the output data - options: (sort|keep_original)");
+DEFINE_bool(
+    log_cost,
+    false,
+    "Log cost info into cloud which will be used for dashboard");
+DEFINE_int32(max_id_column_cnt, 1, "Maximum number of id columns to use as id");
+DEFINE_string(log_cost_s3_bucket, "cost-estimation-logs", "s3 bucket name");
+DEFINE_string(
+    log_cost_s3_region,
+    ".s3.us-west-2.amazonaws.com/",
+    "s3 region name");
+DEFINE_string(protocol_type, "PID", "protocol type");
+DEFINE_string(
+    run_id,
+    "",
+    "A run_id used to identify all the logs in a PL/PA run.");
