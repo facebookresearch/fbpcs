05e0fe92cf5775098570e44f9cfacfb89dfe7171
Giovanni Magoga <ma9o@meta.com>
2022-10-26 10:13:17 -0700

    Add Private ID DFCA aggregation stage binary
    Summary:
    # Context
    **Internal:** [Private ID DFCA one-pager](https://docs.google.com/document/d/1mwImSRCljqU66uXWJS7jL-dQOrYAR4aBZ3bpJ8quiJw) Advertisers want to be able to target their customers on our platform in a privacy-safe manner. We utilise Private ID with the existing DFCA solution to provide this.
    
    We're adding a new game type `PRIVATE_ID_DFCA` whose inputs for both parties are csvs with the columns `user_pii, user_id_{party}`, and the output is a csv of `publisher_user_id, partner_user_id` which can then be fed to our existing DFCA APIs.
    After running the existing PID stages, we need to run two new binaries to get the output in its final form:
    - private_id_dfca_spine_combiner
    - private_id_dfca_aggregator
    
    # This Diff
    Adds the private_id_dfca_aggregator binary to be invoked by the `PRIVATE_ID_DFCA_AGGREGATION` stage defined later in the stack.
    The binary takes the spine combiner results as inputs from both parties and outputs the final csv result of form `publisher_user_id, partner_user_id`, by sequentially computing the joint set on the publisher side.
    We assume the input shards are locally sorted by `id_`.
    The parties communicate via a `SocketPartyCommunicationAgent` with a message length of `MSG_SIZE`.
    
    # This Stack
    - Add spine combiner binary
    - **Add aggregation binary**
    - Add aggregation stage
    - Add Stageflow
    - Add game type
    
    Reviewed By: adshastri
    
    Differential Revision: D39846000
    
    fbshipit-source-id: c99f96b3dee0412c5570cc1b9bdfa4576561264e

---
+++ fbpcs/emp_games/private_id_dfca_aggregator/PrivateIdDfcaAggregatorOptions.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gflags/gflags.h>
+
+#include "fbpcs/emp_games/private_id_dfca_aggregator/PrivateIdDfcaAggregatorOptions.h"
+
+DEFINE_int32(party, 1, "1 = publisher, 2 = partner");
+DEFINE_string(server_ip, "", "Server's IP address");
+DEFINE_int32(port, 15200, "Server's port");
+DEFINE_string(input_path, "", "Input path where input file is located");
+DEFINE_string(output_path, "", "Output path where output file is located");
+DEFINE_string(run_name, "", "User given name used to write cost info in S3");
+DEFINE_bool(
+    log_cost,
+    false,
+    "Log cost info into cloud which will be used for dashboard");
+DEFINE_string(log_cost_s3_bucket, "cost-estimation-logs", "s3 bucket name");
+DEFINE_string(
+    log_cost_s3_region,
+    ".s3.us-west-2.amazonaws.com/",
+    "s3 region name");
+
+DEFINE_bool(
+    use_tls,
+    false,
+    "Whether to use TLS when communicating with other parties.");
+DEFINE_string(
+    ca_cert_path,
+    "",
+    "Relative file path where root CA cert is stored. It will be prefixed with $HOME.");
+DEFINE_string(
+    server_cert_path,
+    "",
+    "Relative file path where server cert is stored. It will be prefixed with $HOME.");
+DEFINE_string(
+    private_key_path,
+    "",
+    "Relative file path where private key is stored. It will be prefixed with $HOME.");
