d688588408e55796faf4b8a8e40af728cc2ca282
Tal Davidi <tdavidi@fb.com>
2022-11-01 15:27:51 -0700

    Implementation of UDP input processor
    Differential Revision: https://internalfb.com/D40402984
    
    fbshipit-source-id: 7b3ab86f17f4d61f9046513922028fc393f86cb3

--- fbpcs/emp_games/lift/pcf2_calculator/input_processing/CompactionBasedInputProcessor.h
+++ fbpcs/emp_games/lift/pcf2_calculator/input_processing/CompactionBasedInputProcessor.h
@@ -9,10 +9,14 @@
 
 #include <stdexcept>
 #include "fbpcf/engine/util/IPrg.h"
+#include "folly/logging/xlog.h"
+
 #include "fbpcs/data_processing/unified_data_process/adapter/IAdapter.h"
 #include "fbpcs/data_processing/unified_data_process/data_processor/IDataProcessor.h"
 #include "fbpcs/emp_games/common/Constants.h"
 #include "fbpcs/emp_games/common/Util.h"
+#include "fbpcs/emp_games/lift/pcf2_calculator/input_processing/Constants.h"
+#include "fbpcs/emp_games/lift/pcf2_calculator/input_processing/GlobalSharingUtils.h"
 #include "fbpcs/emp_games/lift/pcf2_calculator/input_processing/IInputProcessor.h"
 #include "fbpcs/emp_games/lift/pcf2_calculator/input_processing/InputData.h"
 
@@ -47,9 +51,17 @@ class CompactionBasedInputProcessor : public IInputProcessor<schedulerId> {
       throw std::invalid_argument("Tried to process dataset with no rows");
     }
 
-    auto unionMap = shuffleAndGetUnionMap();
-    auto intersectionMap = getIntersectionMap(unionMap);
+    liftGameProcessedData_.numRows = inputData.getNumRows();
 
+    input_processing::validateNumRowsStep(myRole_, liftGameProcessedData_);
+    input_processing::shareNumGroupsStep(
+        myRole_, inputData_, liftGameProcessedData_);
+    input_processing::shareBitsForValuesStep(
+        myRole_, inputData_, liftGameProcessedData_);
+
+    auto unionMap = shuffleAndGetUnionMap();
+
+    auto intersectionMap = getIntersectionMap(unionMap);
     auto plaintextData = preparePlaintextData(unionMap);
 
     auto publisherPartnerJointMetadataShares =
@@ -58,7 +70,18 @@ class CompactionBasedInputProcessor : public IInputProcessor<schedulerId> {
     extractCompactedData(
         std::get<0>(publisherPartnerJointMetadataShares),
         std::get<1>(publisherPartnerJointMetadataShares));
+
+    input_processing::computeIndexSharesAndSetTestGroupIds(
+        liftGameProcessedData_,
+        cohortGroupIds_,
+        controlPopulation_,
+        breakdownGroupIds_,
+        testGroupIds_);
+    input_processing::computeTestIndexShares(
+        liftGameProcessedData_, controlPopulation_, testGroupIds_);
   }
+
+  // CompactionBasedInputProcessor();
 
   const LiftGameProcessedData<schedulerId>& getLiftGameProcessedData()
       const override {
@@ -119,6 +142,59 @@ class CompactionBasedInputProcessor : public IInputProcessor<schedulerId> {
       const SecString& publisherDataShares,
       const SecString& partnerDataShares);
 
+  std::tuple<
+      std::vector<PartnerRow>,
+      std::vector<std::vector<PartnerConversionRow>>,
+      std::vector<PublisherRow>>
+  deserializeSecretSharedData(
+      const SecString& publisherDataShares,
+      const SecString& partnerDataShares);
+
+  void extractPartnerValues(const std::vector<PartnerRow>& partnerRows);
+
+  void extractPartnerConversionValues(
+      const std::vector<std::vector<PartnerConversionRow>>&
+          partnerConversionRows);
+
+  void extractPublisherValues(const std::vector<PublisherRow>& publisherRows);
+
+  template <typename T>
+  unsigned char extractByte(T val, size_t byte) {
+    if (byte < 0 || byte >= sizeof(T)) {
+      throw std::invalid_argument("Not enough bytes in type");
+    }
+
+    return (uint8_t)(val >> 8 * byte);
+  }
+
+  template <typename T>
+  T reconstructFromBytes(unsigned char* data) {
+    T val = 0;
+    for (size_t i = 0; i < sizeof(T); i++) {
+      val |= ((T) * (data + i)) << (i * 8);
+    }
+    return val;
+  }
+
+  std::vector<unsigned char> convertFromBits(std::vector<bool> data) {
+    std::vector<unsigned char> rst;
+    rst.reserve(data.size() / 8);
+
+    size_t i = 0;
+
+    while (i < data.size()) {
+      unsigned char val = 0;
+      size_t bitsLeft = data.size() - i > 8 ? 8 : data.size() - i;
+      for (auto j = 0; j < bitsLeft; j++) {
+        val |= (data[i] << j);
+        ++i;
+      }
+      rst.push_back(val);
+    }
+
+    return rst;
+  }
+
   int32_t myRole_;
 
   std::unique_ptr<unified_data_process::adapter::IAdapter> adapter_;
@@ -128,6 +204,11 @@ class CompactionBasedInputProcessor : public IInputProcessor<schedulerId> {
   std::unique_ptr<fbpcf::engine::util::IPrg> prg_;
   InputData inputData_;
   int32_t numConversionsPerUser_;
+
+  SecBit<schedulerId> controlPopulation_;
+  SecGroup<schedulerId> cohortGroupIds_;
+  SecBit<schedulerId> breakdownGroupIds_;
+  SecGroup<schedulerId> testGroupIds_;
 
   LiftGameProcessedData<schedulerId> liftGameProcessedData_;
 };
