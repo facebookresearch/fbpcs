993f28df6c74be4a7d983b850c725457c5289735
Jian Cao <jiancao0421@meta.com>
2022-11-08 12:10:55 -0800

    Replace the old getShardFor function with deterministic hash algorithm (#1796)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/1796
    
    # What
    * use std:hash to do the get the hash value of the id.
    * Previously, the hash function only take at most 4 leftmost characters from the key string to calculate the hash value.
    * When testing the 100m data, it can save time about 26 mins if we use the new hash function
    
    command:
    ```
    buck run //identity/private_aggregation/mpc_aem/experimentation_platform:mpc_a
    em_exp_platform_runner -- sharder_with_changes_101 --publisher_input="https://mpc-aem-exp-platform-input.s3.us-west-2.amazonaws.com/kosaraju/private_lift_annotated_opportunities_keyed_hashed_pii_prod-opportunities_539290649537_656344465471616_668002787733330._2022-07-30.csv" --partner_input="https://mpc-aem-exp-platform-input.s3.us-west-2.amazonaws.com/adshastri/partner_lift_100m_rows.csv" --num_pid_containers=32 --num_mpc_containers=32 --log_file=sharder_with_changes_101.txt --game=lift --binary_version=jiancao0421_test 2>e 1>o &
    ```
    
    ```
    The distribution of rows by using previous hash function
    0:1
    1:4961780
    2:4960580
    3:4964629
    4:4964350
    5:4962068
    6:4964724
    7:4961107
    8:4958195
    9:4960164
    10:4961260
    11:7441012
    12:4957133
    13:4962667
    14:4956636
    15:7442630
    16:7447949
    17:7441557
    18:7442156
    19:7440335
    20:7443927
    21:7440724
    22:7442462
    23:7438491
    24:7443643
    25:7441319
    26:4960049
    ```
    
    ```
    The distribution of rows by using std:hash
    0:4962763
    1:4959759
    2:4964154
    3:4957506
    4:4962218
    5:4958317
    6:4961223
    7:4958107
    8:4961709
    9:4962701
    10:4960879
    11:4961483
    12:4960521
    13:4959062
    14:4960935
    15:4964092
    16:4962239
    17:4963060
    18:4959130
    19:4965413
    20:4962280
    21:4958296
    22:4964033
    23:4959741
    24:4959561
    25:4961894
    26:4963698
    27:4958637
    28:4961302
    29:4961936
    30:4961599
    31:4963300
    
    ```
    
    design doc: https://docs.google.com/document/d/1O9uZ5FZDnA6RrAO86pbV80lBv0QEpbgK1XsBfdvcJFM/edit
    
    # Why
    * unbalanced shards in pid_shard stage
    
    Reviewed By: gorel
    
    Differential Revision: D39939071
    
    fbshipit-source-id: a10b7ba2695a4e9eff5842e4dfac65cfd67f5151

--- fbpcs/data_processing/sharding/test/HashBasedSharderTest.cpp
+++ fbpcs/data_processing/sharding/test/HashBasedSharderTest.cpp
@@ -17,49 +17,10 @@
 #include "fbpcs/data_processing/test_utils/FileIOTestUtils.h"
 
 namespace data_processing::sharder {
-TEST(HashBasedSharderTest, TestToBytes) {
-  std::string key = "abcd";
-  std::vector<unsigned char> expected{
-      static_cast<unsigned char>('a'),
-      static_cast<unsigned char>('b'),
-      static_cast<unsigned char>('c'),
-      static_cast<unsigned char>('d')};
-  EXPECT_EQ(detail::toBytes(key), expected);
-}
-
-TEST(HashBasedSharderTest, TestBytesToIntSimple) {
-  // First a very simple test (but still important for endianness correctness!)
-  std::vector<unsigned char> bytes{0, 0, 0, 1};
-  EXPECT_EQ(1, detail::bytesToInt(bytes));
-
-  // Assuming network byte order, big-endian 0x1 | 0x0 | 0x0 | 0x0
-  // is equivalent to integer 16777216 (2^24). In binary, we recognize this
-  // number as 0b 0000 0001 0000 0000 0000 0000 0000 0000
-  std::vector<unsigned char> bytes2{1, 0, 0, 0};
-  EXPECT_EQ(1 << 24, detail::bytesToInt(bytes2));
-}
-
-TEST(HashBasedSharderTest, TestBytesToIntAdvanced) {
-  // Don't throw std::out_of_range if bytes is empty
-  std::vector<unsigned char> bytes{};
-  EXPECT_EQ(0, detail::bytesToInt(bytes));
-
-  // If bytes are missing, we still copy the bytes array from the "start" so
-  // this is equivalent to the test in TestBytesToIntSimple. In other words,
-  // this is like copying [0b 0000 0001 0000 0000 [implicit 0000 0000 0000 0000]
-  // Hopefully this is clear -- the lower two bytes were never "overridden" so
-  // they still contain zero.
-  std::vector<unsigned char> bytes2{1, 0};
-  EXPECT_EQ(1 << 24, detail::bytesToInt(bytes2));
-}
-
 TEST(HashBasedSharderTest, TestGetShardFor) {
-  // Assuming toBytes and bytesToInt have been tested elsewhere, this is a
-  // straightforward modulo operation.
   HashBasedSharder sharder{"unused", {/* unused */}, 123, ""};
   std::string key = "abcd";
-  auto integerValue = detail::bytesToInt(detail::toBytes(key));
-  EXPECT_EQ(sharder.getShardFor(key, 123), integerValue % 123);
+  EXPECT_EQ(sharder.getShardFor(key, 123), 25);
   // Anything % 1 should be zero
   EXPECT_EQ(sharder.getShardFor(key, 1), 0);
 }
@@ -124,9 +85,9 @@ TEST(HashBasedSharderTest, TestShardLineWithHmacKey) {
   streams.at(1)->close();
 
   // We didn't write headers, so we expect to *just* have the written line
-  std::vector<std::string> expected0{};
-  std::vector<std::string> expected1{
+  std::vector<std::string> expected0{
       "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,1,2,3"};
+  std::vector<std::string> expected1{};
 
   data_processing::test_utils::expectFileRowsEqual(
       outputPaths.at(0), expected0);
@@ -160,9 +121,9 @@ TEST(HashBasedSharderTest, TestShardMultiKeyLineWithHmacKey) {
   streams.at(1)->close();
 
   // We didn't write headers, so we expect to *just* have the written line
-  std::vector<std::string> expected0{};
-  std::vector<std::string> expected1{
+  std::vector<std::string> expected0{
       "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,1,2,3"};
+  std::vector<std::string> expected1{};
 
   data_processing::test_utils::expectFileRowsEqual(
       outputPaths.at(0), expected0);
@@ -195,11 +156,11 @@ TEST(HashBasedSharderTest, TestShardNoHmacKey) {
       "id_,a,b,c",
       "abcd,1,2,3",
       "abcd,4,5,6",
+      "defg,7,8,9",
+      "hijk,0,0,0",
   };
   std::vector<std::string> expected1{
       "id_,a,b,c",
-      "defg,7,8,9",
-      "hijk,0,0,0",
   };
   data_processing::test_utils::expectFileRowsEqual(
       outputPaths.at(0), expected0);
@@ -234,14 +195,16 @@ TEST(HashBasedSharderTest, TestShardWithHmacKey) {
   // good way to generate more of these given our I/O specification.
   std::vector<std::string> expected0{
       "id_,a,b,c",
-      "bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,7,8,9", // defg line
-      "ZGCVov/c63+N2Swslf6pY6pWsNzS1IkXKVi+lmAD6yU=,0,0,0", // hijk line
-  };
-  std::vector<std::string> expected1{
-      "id_,a,b,c",
       "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,1,2,3", // first abcd line
       "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,4,5,6", // second abcd line
   };
+
+  std::vector<std::string> expected1{
+      "id_,a,b,c",
+      "bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,7,8,9", // defg line
+      "ZGCVov/c63+N2Swslf6pY6pWsNzS1IkXKVi+lmAD6yU=,0,0,0", // hijk line
+  };
+
   data_processing::test_utils::expectFileRowsEqual(
       outputPaths.at(0), expected0);
   data_processing::test_utils::expectFileRowsEqual(
@@ -275,13 +238,14 @@ TEST(HashBasedSharderTest, TestShardMultiKeyWithHmacKey) {
   // good way to generate more of these given our I/O specification.
   std::vector<std::string> expected0{
       "id_email,id_phone,a,b,c",
-      ",bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,7,8,9", // ,defg line
+      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,,1,2,3",
+      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,ZGCVov/c63+N2Swslf6pY6pWsNzS1IkXKVi+lmAD6yU=,4,5,6",
   };
   std::vector<std::string> expected1{
       "id_email,id_phone,a,b,c",
-      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,,1,2,3", // abcd, line
-      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,ZGCVov/c63+N2Swslf6pY6pWsNzS1IkXKVi+lmAD6yU=,4,5,6", // abcd,hijk line
+      ",bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,7,8,9",
   };
+
   data_processing::test_utils::expectFileRowsEqual(
       outputPaths.at(0), expected0);
   data_processing::test_utils::expectFileRowsEqual(
@@ -315,12 +279,12 @@ TEST(HashBasedSharderTest, TestShardMultiKeyWithNullsQuotes) {
   // good way to generate more of these given our I/O specification.
   std::vector<std::string> expected0{
       "id_email,id_phone,a,b,c",
-      ",bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,7,8,9", // ,defg line
+      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,,1,2,3",
+      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,ZGCVov/c63+N2Swslf6pY6pWsNzS1IkXKVi+lmAD6yU=,4,5,6",
   };
   std::vector<std::string> expected1{
       "id_email,id_phone,a,b,c",
-      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,,1,2,3", // abcd, line
-      "9BX9ClsYtFj3L8N023K3mJnw1vemIGqenY5vfAY0/cg=,ZGCVov/c63+N2Swslf6pY6pWsNzS1IkXKVi+lmAD6yU=,4,5,6", // abcd,hijk line
+      ",bSRNJ92+ML97JRfp1lEvqssXNCX+lI2T/HQtHRTkBk4=,7,8,9",
   };
   data_processing::test_utils::expectFileRowsEqual(
       outputPaths.at(0), expected0);
