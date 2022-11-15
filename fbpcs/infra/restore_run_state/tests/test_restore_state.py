8afa7a97350306594b8989a5b742c23b38e16d42
Ramesh Chandran <rameshc@meta.com>
2022-11-15 14:17:04 -0800

    Use regex in restore script & add unit tests
    Summary:
    Followups from earlier diff:
      Add UT for restore script
      Use regex
    
    Reviewed By: ztlbells
    
    Differential Revision: D41314279
    
    fbshipit-source-id: 228ec983d90818009910b5489dcee86211d17784

---
+++ fbpcs/infra/restore_run_state/tests/test_restore_state.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env fbpython
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from unittest import mock, TestCase
+from unittest.mock import ANY
+
+from fbpcs.infra.restore_run_state.restore_state import RestoreRunState
+
+
+class TestRestoreState(TestCase):
+    def setUp(self) -> None:
+        self.restore_state = RestoreRunState()
+
+    def test_split_path(self) -> None:
+        with self.subTest("Valid path"):
+            self.assertEqual(
+                ("fb-pc-data-bucket-wnm6", "logging/logs_20220928T201923/last"),
+                self.restore_state._split_path(
+                    "s3://fb-pc-data-bucket-wnm6/logging/logs_20220928T201923/last"
+                ),
+            )
+        with self.subTest("Valid path 2"):
+            self.assertEqual(
+                ("fb-pc-data-bucket-wnm6", "last"),
+                self.restore_state._split_path("s3://fb-pc-data-bucket-wnm6/last"),
+            )
+        with self.subTest("Invalid path"):
+            self.assertIsNone(
+                self.restore_state._split_path("s3://fb-pc-data-bucket-wnm6"),
+            )
+        with self.subTest("Invalid path 2"):
+            self.assertIsNone(
+                self.restore_state._split_path(
+                    "fb-pc-data-bucket-wnm6/logging/logs_20220928T201923/last"
+                ),
+            )
+
+    def test_copy_files(self) -> None:
+        objects = [
+            mock.Mock(key="key1"),
+            mock.Mock(key="key2/"),
+        ]
+        self.restore_state.s3 = mock.MagicMock()
+        bucket = mock.MagicMock()
+        bucket.objects = mock.MagicMock()
+        bucket.objects.filter = mock.MagicMock(return_value=objects)
+        self.restore_state.s3.Bucket = mock.MagicMock(return_value=bucket)
+
+        self.restore_state._copy_files(
+            "s3://fb-pc-data-bucket-wnm6/logging/last", "/tmp"
+        )
+
+        bucket.download_file.assert_called_once_with("key1", ANY)
