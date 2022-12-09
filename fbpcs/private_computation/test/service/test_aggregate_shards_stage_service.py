2bb3f42d1a70521b98bd58741c2846914c24bb36
joewu <joewu@fb.com>
2022-12-09 15:44:46 -0800

    Migrate AGGREGATE_SHARD stage to use StageStateInstance
    Differential Revision: https://internalfb.com/D41640927
    
    fbshipit-source-id: 0fa971760d16fbbe138474d9d4f4d340c5d5bbdb

--- fbpcs/private_computation/test/service/test_aggregate_shards_stage_service.py
+++ fbpcs/private_computation/test/service/test_aggregate_shards_stage_service.py
@@ -6,9 +6,9 @@
 
 from collections import defaultdict
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock
 
-from fbpcs.common.entity.pcs_mpc_instance import PCSMPCInstance
+from fbpcp.entity.container_instance import ContainerInstance, ContainerInstanceStatus
 from fbpcs.infra.certificate.null_certificate_provider import NullCertificateProvider
 from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
 from fbpcs.private_computation.entity.infra_config import (
@@ -22,26 +22,21 @@ from fbpcs.private_computation.entity.private_computation_instance import (
     PrivateComputationRole,
 )
 from fbpcs.private_computation.entity.product_config import (
-    AttributionConfig,
     CommonProductConfig,
     LiftConfig,
     ProductConfig,
 )
-from fbpcs.private_computation.repository.private_computation_game import GameNames
 from fbpcs.private_computation.service.aggregate_shards_stage_service import (
     AggregateShardsStageService,
 )
 from fbpcs.private_computation.service.constants import NUM_NEW_SHARDS_PER_FILE
-
-from fbpcs.private_computation.service.mpc.entity.mpc_instance import MPCParty
+from fbpcs.private_computation.service.mpc.mpc import MPCService
 
 
 class TestAggregateShardsStageService(IsolatedAsyncioTestCase):
-    @patch("fbpcs.private_computation.service.mpc.mpc.MPCService")
-    def setUp(self, mock_mpc_svc) -> None:
-        self.mock_mpc_svc = mock_mpc_svc
-        self.mock_mpc_svc.get_instance = MagicMock(side_effect=Exception())
-        self.mock_mpc_svc.create_instance = MagicMock()
+    def setUp(self) -> None:
+        self.mock_mpc_svc = MagicMock(spec=MPCService)
+        self.mock_mpc_svc.onedocker_svc = MagicMock()
         self.run_id = "681ba82c-16d9-11ed-861d-0242ac120002"
 
         onedocker_binary_config_map = defaultdict(
@@ -56,21 +51,24 @@ class TestAggregateShardsStageService(IsolatedAsyncioTestCase):
         )
 
     async def test_aggregate_shards(self) -> None:
+        containers = [
+            ContainerInstance(
+                instance_id="test_container_id", status=ContainerInstanceStatus.STARTED
+            )
+        ]
+        self.mock_mpc_svc.start_containers.return_value = containers
         private_computation_instance = self._create_pc_instance()
-        mpc_instance = PCSMPCInstance.create_instance(
-            instance_id=private_computation_instance.infra_config.instance_id
-            + "_aggregate_metrics",
-            game_name=GameNames.LIFT.value,
-            mpc_party=MPCParty.CLIENT,
-            num_workers=private_computation_instance.infra_config.num_mpc_containers,
-        )
-
-        self.mock_mpc_svc.start_instance_async = AsyncMock(return_value=mpc_instance)
-
+        binary_name = "private_attribution/shard-aggregator"
         test_server_ips = [
             f"192.0.2.{i}"
             for i in range(private_computation_instance.infra_config.num_mpc_containers)
         ]
+        self.mock_mpc_svc.convert_cmd_args_list.return_value = (
+            binary_name,
+            ["cmd_1", "cmd_2"],
+        )
+
+        # act
         await self.stage_svc.run_async(
             private_computation_instance,
             NullCertificateProvider(),
@@ -79,47 +77,34 @@ class TestAggregateShardsStageService(IsolatedAsyncioTestCase):
             "",
             test_server_ips,
         )
-        test_game_args = [
-            {
-                "input_base_path": private_computation_instance.compute_stage_output_base_path,
-                "metrics_format_type": "lift",
-                "num_shards": private_computation_instance.infra_config.num_mpc_containers
-                * NUM_NEW_SHARDS_PER_FILE,
-                "output_path": private_computation_instance.shard_aggregate_stage_output_path,
-                "threshold": 0
-                if isinstance(
-                    private_computation_instance.product_config, AttributionConfig
-                )
-                # pyre-ignore Undefined attribute [16]
-                else private_computation_instance.product_config.k_anonymity_threshold,
-                "run_name": private_computation_instance.infra_config.instance_id
-                if self.stage_svc._log_cost_to_s3
-                else "",
-                "log_cost": True,
-                "run_id": self.run_id,
-                "pc_feature_flags": private_computation_instance.feature_flags,
-                "log_cost_s3_bucket": private_computation_instance.infra_config.log_cost_bucket,
-            }
-        ]
 
-        self.assertEqual(
-            GameNames.SHARD_AGGREGATOR.value,
-            self.mock_mpc_svc.create_instance.call_args[1]["game_name"],
+        # asserts
+        self.mock_mpc_svc.start_containers.assert_called_once_with(
+            cmd_args_list=["cmd_1", "cmd_2"],
+            onedocker_svc=self.mock_mpc_svc.onedocker_svc,
+            binary_version="latest",
+            binary_name=binary_name,
+            timeout=None,
+            env_vars={"ONEDOCKER_REPOSITORY_PATH": "test_path/"},
+            wait_for_containers_to_start_up=True,
+            existing_containers=None,
         )
         self.assertEqual(
-            test_game_args,
-            self.mock_mpc_svc.create_instance.call_args[1]["game_args"],
+            containers,
+            # pyre-ignore
+            private_computation_instance.infra_config.instances[-1].containers,
         )
-
         self.assertEqual(
-            mpc_instance, private_computation_instance.infra_config.instances[0]
+            "AGGREGATE",
+            # pyre-ignore
+            private_computation_instance.infra_config.instances[-1].stage_name,
         )
 
     def _create_pc_instance(self) -> PrivateComputationInstance:
         infra_config: InfraConfig = InfraConfig(
             instance_id="test_instance_123",
             role=PrivateComputationRole.PARTNER,
-            status=PrivateComputationInstanceStatus.COMPUTATION_COMPLETED,
+            status=PrivateComputationInstanceStatus.AGGREGATION_STARTED,
             status_update_ts=1600000000,
             instances=[],
             game_type=PrivateComputationGameType.LIFT,
