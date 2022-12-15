3180f73e33d04f631b298c0b7310cf0c154381cb
joewu <joewu@fb.com>
2022-12-15 13:57:53 -0800

    Migrate PA attribution/aggregration stages to use StageStateInstance
    Summary:
    ## Why
    During the MPC migration, one of the goal is to remove PCMPCInstace to use unify StageStateInstance.
    This diff is to migrate PA attribution/aggregration stages from PCMPCInstace to StageStateInstance.
    ## What
    migrate PCMPCInstance usage from PA attribution/aggregration stages (PCF2) to StageStateInstance
    fix UT due toe the changes
    
    Differential Revision: https://internalfb.com/D41991371
    
    fbshipit-source-id: 9cc5fd4c408e84bf7fdaef76ac734eb12a75c66c

--- fbpcs/private_computation/test/service/test_pcf2_aggregation_stage_service.py
+++ fbpcs/private_computation/test/service/test_pcf2_aggregation_stage_service.py
@@ -6,9 +6,9 @@
 
 from collections import defaultdict
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock
 
-from fbpcs.common.entity.pcs_mpc_instance import PCSMPCInstance
+from fbpcp.entity.container_instance import ContainerInstance, ContainerInstanceStatus
 from fbpcs.infra.certificate.null_certificate_provider import NullCertificateProvider
 from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
 from fbpcs.private_computation.entity.infra_config import (
@@ -31,18 +31,16 @@ from fbpcs.private_computation.entity.product_config import (
 )
 from fbpcs.private_computation.repository.private_computation_game import GameNames
 from fbpcs.private_computation.service.constants import NUM_NEW_SHARDS_PER_FILE
-
-from fbpcs.private_computation.service.mpc.entity.mpc_instance import MPCParty
+from fbpcs.private_computation.service.mpc.mpc import MPCService
 from fbpcs.private_computation.service.pcf2_aggregation_stage_service import (
     PCF2AggregationStageService,
 )
 
 
 class TestPCF2AggregationStageService(IsolatedAsyncioTestCase):
-    @patch("fbpcs.private_computation.service.mpc.mpc.MPCService")
-    def setUp(self, mock_mpc_svc) -> None:
-        self.mock_mpc_svc = mock_mpc_svc
-        self.mock_mpc_svc.create_instance = MagicMock()
+    def setUp(self) -> None:
+        self.mock_mpc_svc = MagicMock(spec=MPCService)
+        self.mock_mpc_svc.onedocker_svc = MagicMock()
         self.run_id = "681ba82c-16d9-11ed-861d-0242ac120002"
 
         onedocker_binary_config_map = defaultdict(
@@ -57,23 +55,24 @@ class TestPCF2AggregationStageService(IsolatedAsyncioTestCase):
         )
 
     async def test_aggregation_stage(self) -> None:
+        containers = [
+            ContainerInstance(
+                instance_id="test_container_id", status=ContainerInstanceStatus.STARTED
+            )
+        ]
+        self.mock_mpc_svc.start_containers.return_value = containers
         private_computation_instance = self._create_pc_instance()
-        mpc_instance = PCSMPCInstance.create_instance(
-            instance_id=private_computation_instance.infra_config.instance_id
-            + "_"
-            + GameNames.PCF2_AGGREGATION.value
-            + "0",
-            game_name=GameNames.PCF2_AGGREGATION.value,
-            mpc_party=MPCParty.CLIENT,
-            num_workers=private_computation_instance.infra_config.num_mpc_containers,
-        )
-
-        self.mock_mpc_svc.start_instance_async = AsyncMock(return_value=mpc_instance)
-
+        binary_name = "private_attribution/pcf2_aggregation"
         test_server_ips = [
             f"192.0.2.{i}"
             for i in range(private_computation_instance.infra_config.num_mpc_containers)
         ]
+        self.mock_mpc_svc.convert_cmd_args_list.return_value = (
+            binary_name,
+            ["cmd_1", "cmd_2"],
+        )
+
+        # act
         await self.stage_svc.run_async(
             private_computation_instance,
             NullCertificateProvider(),
@@ -83,8 +82,26 @@ class TestPCF2AggregationStageService(IsolatedAsyncioTestCase):
             test_server_ips,
         )
 
+        # asserts
+        self.mock_mpc_svc.start_containers.assert_called_once_with(
+            cmd_args_list=["cmd_1", "cmd_2"],
+            onedocker_svc=self.mock_mpc_svc.onedocker_svc,
+            binary_version="latest",
+            binary_name=binary_name,
+            timeout=None,
+            env_vars={"ONEDOCKER_REPOSITORY_PATH": "test_path/"},
+            wait_for_containers_to_start_up=True,
+            existing_containers=None,
+        )
         self.assertEqual(
-            mpc_instance, private_computation_instance.infra_config.instances[0]
+            containers,
+            # pyre-ignore
+            private_computation_instance.infra_config.instances[-1].containers,
+        )
+        self.assertEqual(
+            "PCF2_AGGREGATION",
+            # pyre-ignore
+            private_computation_instance.infra_config.instances[-1].stage_name,
         )
 
     def test_get_game_args(self) -> None:
@@ -141,7 +158,8 @@ class TestPCF2AggregationStageService(IsolatedAsyncioTestCase):
         infra_config: InfraConfig = InfraConfig(
             instance_id="test_instance_123",
             role=PrivateComputationRole.PARTNER,
-            status=PrivateComputationInstanceStatus.PCF2_ATTRIBUTION_COMPLETED,
+            _stage_flow_cls_name="PrivateComputationPCF2StageFlow",
+            status=PrivateComputationInstanceStatus.PCF2_AGGREGATION_STARTED,
             status_update_ts=1600000000,
             instances=[],
             game_type=PrivateComputationGameType.ATTRIBUTION,
