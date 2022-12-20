30f7e899bb7fdba4cd031afdca53b0cdb96d668c
Joe Wu <joewu@meta.com>
2022-12-19 16:39:56 -0800

    Migrate PrivateIdDfcaAggregateStageService stage to use StageStateInstance (#2003)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2003
    
    ## Why
    During the MPC migration, one of the goal is to remove PCMPCInstace to use unify StageStateInstance.
    This diff is to migrate PrivateIdDfcaAggregateStageService stage from PCMPCInstace to StageStateInstance.
    ## What
    migrate PCMPCInstance usage from PrivateIdDfcaAggregateStageService stage to StageStateInstance
    fix UT due to the changes
    
    Reviewed By: jrodal98
    
    Differential Revision: D42050080
    
    fbshipit-source-id: 51464b477b967d3f396e03b1492d0e1366286326

--- fbpcs/private_computation/test/service/test_private_id_dfca_aggregate_stage_service.py
+++ fbpcs/private_computation/test/service/test_private_id_dfca_aggregate_stage_service.py
@@ -6,9 +6,9 @@
 
 from collections import defaultdict
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock
 
-from fbpcs.common.entity.pcs_mpc_instance import PCSMPCInstance
+from fbpcp.entity.container_instance import ContainerInstance, ContainerInstanceStatus
 from fbpcs.infra.certificate.null_certificate_provider import NullCertificateProvider
 from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
 from fbpcs.private_computation.entity.infra_config import (
@@ -26,21 +26,18 @@ from fbpcs.private_computation.entity.product_config import (
     PrivateIdDfcaConfig,
     ProductConfig,
 )
-from fbpcs.private_computation.repository.private_computation_game import GameNames
 from fbpcs.private_computation.service.constants import NUM_NEW_SHARDS_PER_FILE
 
-from fbpcs.private_computation.service.mpc.entity.mpc_instance import MPCParty
+from fbpcs.private_computation.service.mpc.mpc import MPCService
 from fbpcs.private_computation.service.private_id_dfca_aggregate_stage_service import (
     PrivateIdDfcaAggregateStageService,
 )
 
 
 class TestPrivateIdDfcaAggregateStageService(IsolatedAsyncioTestCase):
-    @patch("fbpcs.private_computation.service.mpc.mpc.MPCService")
-    def setUp(self, mock_mpc_svc) -> None:
-        self.mock_mpc_svc = mock_mpc_svc
-        self.mock_mpc_svc.get_instance = MagicMock(side_effect=Exception())
-        self.mock_mpc_svc.create_instance = MagicMock()
+    def setUp(self) -> None:
+        self.mock_mpc_svc = MagicMock(spec=MPCService)
+        self.mock_mpc_svc.onedocker_svc = MagicMock()
         self.run_id = "681ba82c-16d9-11ed-861d-0242ac120002"
 
         onedocker_binary_config_map = defaultdict(
@@ -55,21 +52,24 @@ class TestPrivateIdDfcaAggregateStageService(IsolatedAsyncioTestCase):
         )
 
     async def test_private_id_dfca_aggregate(self) -> None:
+        containers = [
+            ContainerInstance(
+                instance_id="test_container_id", status=ContainerInstanceStatus.STARTED
+            )
+        ]
+        self.mock_mpc_svc.start_containers.return_value = containers
         private_computation_instance = self._create_pc_instance()
-        mpc_instance = PCSMPCInstance.create_instance(
-            instance_id=private_computation_instance.infra_config.instance_id
-            + "_private_id_dfca_aggregate",
-            game_name=GameNames.PRIVATE_ID_DFCA_AGGREGATION.value,
-            mpc_party=MPCParty.CLIENT,
-            num_workers=private_computation_instance.infra_config.num_mpc_containers,
-        )
-
-        self.mock_mpc_svc.start_instance_async = AsyncMock(return_value=mpc_instance)
-
+        binary_name = "private_id_dfca/private_id_dfca_aggregator"
         test_server_ips = [
             f"192.0.2.{i}"
             for i in range(private_computation_instance.infra_config.num_mpc_containers)
         ]
+        self.mock_mpc_svc.convert_cmd_args_list.return_value = (
+            binary_name,
+            ["cmd_1", "cmd_2"],
+        )
+
+        # act
         await self.stage_svc.run_async(
             private_computation_instance,
             NullCertificateProvider(),
@@ -78,36 +78,35 @@ class TestPrivateIdDfcaAggregateStageService(IsolatedAsyncioTestCase):
             "",
             test_server_ips,
         )
-        test_game_args = [
-            {
-                "input_path": f"{private_computation_instance.data_processing_output_path}_combine_{i}",
-                "output_path": f"{private_computation_instance.private_id_dfca_aggregate_stage_output_path}_{i}",
-                "run_name": private_computation_instance.infra_config.instance_id,
-                "log_cost": True,
-                "run_id": self.run_id,
-                "pc_feature_flags": private_computation_instance.feature_flags,
-            }
-            for i in range(private_computation_instance.infra_config.num_mpc_containers)
-        ]
 
-        self.assertEqual(
-            GameNames.PRIVATE_ID_DFCA_AGGREGATION.value,
-            self.mock_mpc_svc.create_instance.call_args[1]["game_name"],
+        # asserts
+        self.mock_mpc_svc.start_containers.assert_called_once_with(
+            cmd_args_list=["cmd_1", "cmd_2"],
+            onedocker_svc=self.mock_mpc_svc.onedocker_svc,
+            binary_version="latest",
+            binary_name=binary_name,
+            timeout=None,
+            env_vars={"ONEDOCKER_REPOSITORY_PATH": "test_path/"},
+            wait_for_containers_to_start_up=True,
+            existing_containers=None,
         )
         self.assertEqual(
-            test_game_args,
-            self.mock_mpc_svc.create_instance.call_args[1]["game_args"],
+            containers,
+            # pyre-ignore
+            private_computation_instance.infra_config.instances[-1].containers,
         )
-
         self.assertEqual(
-            mpc_instance, private_computation_instance.infra_config.instances[0]
+            "PRIVATE_ID_DFCA_AGGREGATE",
+            # pyre-ignore
+            private_computation_instance.infra_config.instances[-1].stage_name,
         )
 
     def _create_pc_instance(self) -> PrivateComputationInstance:
         infra_config: InfraConfig = InfraConfig(
             instance_id="test_instance_123",
             role=PrivateComputationRole.PARTNER,
-            status=PrivateComputationInstanceStatus.COMPUTATION_COMPLETED,
+            _stage_flow_cls_name="PrivateComputationPrivateIdDfcaStageFlow",
+            status=PrivateComputationInstanceStatus.PRIVATE_ID_DFCA_AGGREGATION_STARTED,
             status_update_ts=1600000000,
             instances=[],
             game_type=PrivateComputationGameType.PRIVATE_ID_DFCA,
