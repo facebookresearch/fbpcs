70f24c15b7cc830e3a2f05ae6a074c1ac83f460e
Jacob Rodal <jrodal@meta.com>
2023-02-07 11:56:28 -0800

    exception thrower hook (#2078)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2078
    
    ## What
    
    - Create BoltHook that throws exceptions
    
    ## Why
    
    - This can be used to simulate various failures (transient failures, capacity issues, advertiser keyboard interrupt, etc)
    
    ## Diff stack context
    
    - We've had SEVs in the past where our retry logic did not work as we expected. Failure injection can help with the creation of tests that invoke the retry logic.
    - https://docs.google.com/document/d/1tfvxFL2k0BnnYPlVhZyK7a18J79vbooB7jyqcqSeSl0/edit
    
    Reviewed By: calebzj, joe1234wu
    
    Differential Revision:
    D41792017
    
    Privacy Context Container: L416713
    
    fbshipit-source-id: 254c510d6c80d5857db0bf44350bb0d499cfb29b

---
+++ fbpcs/bolt/hooks/test/test_exception_thrower.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import Mock
+
+from fbpcs.bolt.hooks.exception_thrower import (
+    BoltExceptionThrowerHook,
+    BoltExceptionThrowerHookArgs,
+)
+
+
+class TestException(Exception):
+    pass
+
+
+class TestBoltExceptionThrowerHook(IsolatedAsyncioTestCase):
+    async def test_throw_exception(self) -> None:
+        exception = TestException()
+        test_hook = BoltExceptionThrowerHook(
+            hook_args=BoltExceptionThrowerHookArgs(exception=exception)
+        )
+        with self.assertRaises(TestException):
+            await test_hook._inject(injection_args=Mock())
