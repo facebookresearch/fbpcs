c9cb83bd3fefb631d0b2742dcde2bca4249ec2ff
Ramesh Chandran <rameshc@meta.com>
2023-02-14 21:58:13 -0800

    Add concurrent download in Shard Aggregator (#484)
    Summary:
    X-link: https://github.com/facebookresearch/fbpcf/pull/484
    
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2096
    
    1. Currently, the sequential download takes a long time (we have seen ~4h for some runs) - add concurrent download support
    
    Reviewed By: marksliva
    
    Differential Revision: D43166367
    
    fbshipit-source-id: 40e3802e08d5ac494b694ba2f8b27a9026fcde0a

--- fbpcs/emp_games/attribution/Constants.h
+++ fbpcs/emp_games/attribution/Constants.h
@@ -18,4 +18,6 @@ const int64_t PUBLISHER = emp::ALICE;
 const int64_t PARTNER = emp::BOB;
 
 const int64_t INVALID_TP_ID = -1;
+const unsigned long MAX_IO_THREADS = 20;
+
 } // namespace measurement::private_attribution
