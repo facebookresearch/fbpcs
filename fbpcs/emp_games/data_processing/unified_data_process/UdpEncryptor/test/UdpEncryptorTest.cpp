88424b527c0fb1d9ee880427685da4ef2eaa4d4a
Ruiyu Zhu <ruiyuzhu@meta.com>
2023-03-10 13:01:28 -0800

    logic for processing my data (#2163)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2163
    
    Adding logic for processing my data.
    
    Reviewed By: haochenuw
    
    Differential Revision:
    D43576518
    
    Privacy Context Container: L416713
    
    fbshipit-source-id: 59fd922bd3a8e36653990dc7e8ada53655c158cf

--- fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/test/UdpEncryptorTest.cpp
+++ fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/test/UdpEncryptorTest.cpp
@@ -14,7 +14,7 @@
 using namespace ::testing;
 namespace unified_data_process {
 
-TEST(UdpEncryptorTest, testProcessingPeerData) {
+TEST(UdpEncryptorTestWithMock, testProcessingPeerData) {
   int chunkSize = 500;
   int totalRow = 1200;
   size_t dataWidth = 32;
@@ -30,6 +30,105 @@ TEST(UdpEncryptorTest, testProcessingPeerData) {
 
   UdpEncryptor encryptor(std::move(mock), chunkSize);
   encryptor.setPeerConfig(totalRow, dataWidth, indexes);
+  encryptor.getEncryptionResults();
+}
+
+TEST(UdpEncryptorTestWithMock, testProcessingMyData) {
+  size_t chunkSize = 200;
+  size_t sampleSize = 219;
+  size_t totalRow = 1200;
+  size_t width = 32;
+
+  std::vector<std::vector<std::vector<unsigned char>>> testData;
+  std::vector<std::vector<std::vector<unsigned char>>> testData1;
+  for (size_t i = 0; i < totalRow; i += chunkSize) {
+    testData.push_back(std::vector<std::vector<unsigned char>>());
+    testData.back().reserve(std::min(chunkSize, totalRow - i));
+    for (size_t j = 0; (j < chunkSize) && (j + i < totalRow); j++) {
+      testData.back().push_back(
+          std::vector<unsigned char>(width, (i + j) & 0xFF));
+      if ((i + j) % sampleSize == 0) {
+        testData1.push_back(std::vector<std::vector<unsigned char>>());
+      }
+      testData1.back().push_back(
+          std::vector<unsigned char>(width, (i + j) & 0xFF));
+    }
+  }
+
+  auto mock = std::make_unique<unified_data_process::UdpEncryptionMock>();
+
+  EXPECT_CALL(*mock, prepareToProcessMyData(width)).Times(1);
+  for (size_t i = 0; i < testData.size(); i++) {
+    EXPECT_CALL(*mock, processMyData(testData.at(i))).Times(1);
+  }
+  EXPECT_CALL(*mock, getExpandedKey()).Times(1);
+
+  UdpEncryptor encryptor(std::move(mock), chunkSize);
+  for (size_t i = 0; i < testData1.size() / 2; i++) {
+    for (size_t j = 0; j < testData1.at(i).size(); j++) {
+      encryptor.pushOneLineFromMe(std::move(testData1.at(i).at(j)));
+    }
+  }
+  for (size_t i = testData1.size() / 2; i < testData1.size(); i++) {
+    encryptor.pushLinesFromMe(std::move(testData1.at(i)));
+  }
+  encryptor.getExpandedKey();
+}
+
+TEST(UdpEncryptorTestWithMock, testProcessingBothSidesData) {
+  size_t chunkSize = 200;
+  size_t sampleSize = 219;
+  size_t myTotalRow = 1200;
+  size_t peerTotalRow = 1500;
+  size_t myWidth = 32;
+  size_t peerWidth = 35;
+  std::vector<int32_t> indexes{3, 31, 6, 12, 5};
+
+  std::vector<std::vector<std::vector<unsigned char>>> testData;
+  std::vector<std::vector<std::vector<unsigned char>>> testData1;
+
+  for (size_t i = 0; i < myTotalRow; i += chunkSize) {
+    testData.push_back(std::vector<std::vector<unsigned char>>());
+    testData.back().reserve(std::min(chunkSize, myTotalRow - i));
+    for (size_t j = 0; j < chunkSize && j + i < myTotalRow; j++) {
+      testData.back().push_back(
+          std::vector<unsigned char>(myWidth, (i + j) & 0xFF));
+      if ((i + j) % sampleSize == 0) {
+        testData1.push_back(std::vector<std::vector<unsigned char>>());
+      }
+      testData1.back().push_back(
+          std::vector<unsigned char>(myWidth, (i + j) & 0xFF));
+    }
+  }
+
+  auto mock = std::make_unique<unified_data_process::UdpEncryptionMock>();
+
+  EXPECT_CALL(*mock, prepareToProcessMyData(myWidth)).Times(1);
+  for (size_t i = 0; i < testData.size(); i++) {
+    EXPECT_CALL(*mock, processMyData(testData.at(i))).Times(1);
+  }
+  EXPECT_CALL(*mock, getExpandedKey()).Times(1);
+
+  EXPECT_CALL(*mock, prepareToProcessPeerData(peerWidth, indexes)).Times(1);
+  EXPECT_CALL(*mock, processPeerData(chunkSize))
+      .Times(peerTotalRow / chunkSize);
+  if (peerTotalRow % chunkSize != 0) {
+    EXPECT_CALL(*mock, processPeerData(peerTotalRow % chunkSize)).Times(1);
+  }
+  EXPECT_CALL(*mock, getProcessedData()).Times(1);
+
+  UdpEncryptor encryptor(std::move(mock), chunkSize);
+  encryptor.setPeerConfig(peerTotalRow, peerWidth, indexes);
+
+  for (size_t i = 0; i < testData1.size() / 2; i++) {
+    for (size_t j = 0; j < testData1.at(i).size(); j++) {
+      encryptor.pushOneLineFromMe(std::move(testData1.at(i).at(j)));
+    }
+  }
+  for (size_t i = testData1.size() / 2; i < testData1.size(); i++) {
+    encryptor.pushLinesFromMe(std::move(testData1.at(i)));
+  }
+  encryptor.getExpandedKey();
   encryptor.getEncryptionResults();
 }
 
