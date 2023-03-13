ae401c9a94818b796e4cca816f070bd6a50e5e45
Tarek Elgamal <telgamal@meta.com>
2023-03-13 00:00:29 -0700

    Adding HEAgg App (#2175)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2175
    
    This diff adds HEAggApp and calls its run function from MainUtil.h. The HEAggApp will be used in subsequent diffs to read the input data, launch the HEAggGame and write the output.
    
    Reviewed By: chennyc
    
    Differential Revision:
    D43929346
    
    Privacy Context Container: L1173372
    
    fbshipit-source-id: 09d1afe6612e8bd27bbe443b53e54a868f51d830

--- fbpcs/emp_games/he_aggregation/MainUtil.h
+++ fbpcs/emp_games/he_aggregation/MainUtil.h
@@ -13,15 +13,23 @@
 
 #include "fbpcf/engine/communication/SocketPartyCommunicationAgentFactory.h"
 #include "fbpcs/emp_games/common/SchedulerStatistics.h"
+#include "fbpcs/emp_games/he_aggregation/HEAggApp.h"
 
 namespace pcf2_he {
 
 template <int PARTY>
 inline common::SchedulerStatistics startHEAggApp(
-    std::string serverIp,
-    int port,
-    fbpcf::engine::communication::SocketPartyCommunicationAgent::TlsInfo&
-        tlsInfo) {
+    const std::string& serverIp,
+    const int port,
+    const std::string& secretShareFilePath,
+    const std::string& inputFilePath,
+    const std::string& outFilePath,
+    const double delta,
+    const double eps,
+    const bool addDpNoise,
+    const fbpcf::engine::communication::SocketPartyCommunicationAgent::TlsInfo&
+        tlsInfo,
+    const common::InputEncryption inputEncryption) {
   std::map<
       int,
       fbpcf::engine::communication::SocketPartyCommunicationAgentFactory::
@@ -38,6 +46,19 @@ inline common::SchedulerStatistics startHEAggApp(
   ////////////////////////
   // Add HE Agg App Here
   ////////////////////////
+  auto app = std::make_unique<pcf2_he::HEAggApp<PARTY, PARTY>>(
+      std::move(communicationAgentFactory),
+      secretShareFilePath,
+      inputFilePath,
+      outFilePath,
+      metricCollector,
+      delta,
+      eps,
+      inputEncryption,
+      addDpNoise);
+
+  app->run();
+  return app->getSchedulerStatistics();
 
   common::SchedulerStatistics schedulerStatistics;
   return schedulerStatistics;
