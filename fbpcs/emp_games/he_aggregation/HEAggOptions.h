33f9cb10943a2b3162ed924e9df4c96d2f36face
telgamal <telgamal@fb.com>
2023-03-12 23:19:10 -0700

    HE-based aggrregation main function and connection between between parties
    Summary:
    This is the first of a series of diffs for adding support for HE based Aggregation game (More details in this design doc https://docs.google.com/document/d/1euw8LWccFNiPbO16Wm7YkRmOHji7WPy5ExixgEhFwAo/edit#heading=h.tpyrj7w890y2
    
    This diff identifies the command line params for the game and establishes the connection between the parties.
    
    Differential Revision: https://internalfb.com/D43926465
    
    fbshipit-source-id: 4463c5f116f06d62c5618dcb7f21626d429d5565

---
+++ fbpcs/emp_games/he_aggregation/HEAggOptions.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <gflags/gflags_declare.h>
+
+DECLARE_int32(party);
+DECLARE_string(server_ip);
+DECLARE_int32(port);
+DECLARE_string(input_base_path_secret_share);
+DECLARE_string(input_base_path);
+DECLARE_string(output_base_path);
+DECLARE_double(delta);
+DECLARE_double(eps);
+DECLARE_string(run_name);
+DECLARE_bool(log_cost);
+DECLARE_bool(add_dp_noise);
+DECLARE_string(log_cost_s3_bucket);
+DECLARE_string(log_cost_s3_region);
+DECLARE_bool(use_tls);
+DECLARE_string(ca_cert_path);
+DECLARE_string(server_cert_path);
+DECLARE_string(private_key_path);
+DECLARE_int32(input_encryption);
