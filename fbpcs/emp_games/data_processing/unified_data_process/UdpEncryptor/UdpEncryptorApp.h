5cc3b980bad6799cbf09e6690c183ff492fe908d
Ruiyu Zhu <ruiyuzhu@fb.com>
2023-03-15 22:04:51 -0700

    Encryptor app
    Summary:
    UDP encryption app
    This an assembling object to host file read/write and calling into the encryptor. The integration test will be in later diff.
    
    Differential Revision: https://internalfb.com/D43752781
    
    fbshipit-source-id: 99f1ebdcf4cb18b33b8795d554d653c95bc85d50

---
+++ fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/UdpEncryptorApp.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/UdpEncryptor.h"
+
+namespace unified_data_process {
+
+class UdpEncryptorApp {
+ public:
+  ~UdpEncryptorApp() {}
+
+  UdpEncryptorApp(std::unique_ptr<UdpEncryptor> encryptor, bool amIPublisher)
+      : encryptor_(std::move(encryptor)), amIPublisher_(amIPublisher) {}
+
+  void invokeUdpEncryption(
+      const std::vector<std::string>& indexFiles,
+      const std::vector<std::string>& serializedDataFiles,
+      const std::string& globalParameters,
+      const std::string& dataFile,
+      const std::string& expandedKeyFile);
+
+ private:
+  static folly::coro::Task<std::vector<int32_t>> readIndexFile(
+      const std::string& fileName);
+  static folly::coro::Task<std::vector<std::vector<unsigned char>>>
+  readDataFile(const std::string& fileName);
+
+  folly::coro::Task<void> processPeerData(
+      const std::vector<std::string>& indexFiles,
+      const std::string& globalParameterFile) const;
+
+  folly::coro::Task<void> processMyData(
+      const std::vector<std::string>& serializedDataFiles) const;
+
+  std::unique_ptr<UdpEncryptor> encryptor_;
+  bool amIPublisher_;
+};
+
+} // namespace unified_data_process
