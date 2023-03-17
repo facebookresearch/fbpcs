10ea49d3c80ac9277d673b18f53a13433c469c21
jrodal <jrodal@fb.com>
2023-03-17 15:37:37 -0700

    anonymizer stage service
    Differential Revision: D44149302
    
    fbshipit-source-id: 3d57a6bbe1a0320ae131f6c9004783acdbd138d9

---
+++ fbpcs/private_computation/test/service/test_anonymizer_stage_service.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from collections import defaultdict
+
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import Mock, patch
+
+from fbpcs.onedocker_binary_config import OneDockerBinaryConfig
+from fbpcs.private_computation.service.anonymizer_stage_service import (
+    AnonymizerStageService,
+)
+
+
+class TestAnonymizerStageService(IsolatedAsyncioTestCase):
+    @patch("fbpcp.service.onedocker.OneDockerService")
+    def setUp(self, onedocker_service: Mock) -> None:
+        onedocker_binary_config_map = defaultdict(
+            lambda: OneDockerBinaryConfig(
+                tmp_directory="/test_tmp_directory/",
+                binary_version="latest",
+                repository_path="test_path/",
+            )
+        )
+        self.stage_svc = AnonymizerStageService(
+            onedocker_service, onedocker_binary_config_map
+        )
+
+    async def test_anonymizer(self) -> None:
+        # Once the AnonymizerStageService is implemented, this test case
+        # can be implemented
+        pass
