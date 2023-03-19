fa1cf6a6ff747bc7ad1e2a55a9a8878427448d65
Ruiyu Zhu <ruiyuzhu@meta.com>
2023-03-18 18:27:36 -0700

    Add cost monitor
    Summary:
    As title.
    Add the cost monitor to udp encryption binary.
    
    Differential Revision:
    D44192607
    
    Privacy Context Container: L416713
    
    fbshipit-source-id: 6c531bb093ec427827bfa5f270b97fc816c6028c

--- fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/main.cpp
+++ fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/main.cpp
@@ -5,13 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <folly/dynamic.h>
 #include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <signal.h>
+#include <filesystem>
 #include <string>
+#include "fbpcf/aws/AwsSdk.h"
 #include "fbpcf/engine/communication/SocketPartyCommunicationAgentFactory.h"
-#include "fbpcf/mpc_std_lib/unified_data_process/data_processor/UdpEncryption.h"
-#include "fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/UdpEncryptorApp.h"
+#include "fbpcs/emp_games/common/FeatureFlagUtil.h"
+#include "fbpcs/performance_tools/CostEstimation.h"
+#include "folly/String.h"
 #include "folly/init/Init.h"
 #include "folly/logging/xlog.h"
+
+#include "fbpcf/mpc_std_lib/unified_data_process/data_processor/UdpEncryption.h"
+#include "fbpcs/emp_games/data_processing/unified_data_process/UdpEncryptor/UdpEncryptorApp.h"
 
 DEFINE_int32(party, 1, "1 = publisher, 2 = partner");
 DEFINE_string(server_ip, "127.0.0.1", "Server's IP address");
@@ -67,6 +76,20 @@ DEFINE_string(
     "",
     "Relative file path where private key is stored. It will be prefixed with $HOME.");
 
+DEFINE_bool(
+    log_cost,
+    false,
+    "Log cost info into cloud which will be used for dashboard");
+DEFINE_string(log_cost_s3_bucket, "", "s3 bucket name");
+DEFINE_string(
+    log_cost_s3_region,
+    ".s3.us-west-2.amazonaws.com/",
+    "s3 regioni name");
+DEFINE_string(
+    run_name,
+    "",
+    "A user given run name that will be used in s3 filename");
+
 std::vector<std::string> generateFileNames(
     const std::string& basename,
     int count) {
@@ -80,6 +103,17 @@ std::vector<std::string> generateFileNames(
 int main(int argc, char* argv[]) {
   folly::init(&argc, &argv);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  fbpcs::performance_tools::CostEstimation cost =
+      fbpcs::performance_tools::CostEstimation(
+          "udp", FLAGS_log_cost_s3_bucket, FLAGS_log_cost_s3_region, "pcf2");
+
+  cost.start();
+
+  fbpcf::AwsSdk::aquire();
+
+  signal(SIGPIPE, SIG_IGN);
+
   XLOGF(INFO, "Party: {}", FLAGS_party);
   XLOGF(INFO, "Server IP: {}", FLAGS_server_ip);
   XLOGF(INFO, "Port: {}", FLAGS_port);
@@ -129,5 +163,21 @@ int main(int argc, char* argv[]) {
       generateFileNames(
           FLAGS_encryption_output_base_path, FLAGS_encryption_output_num),
       FLAGS_expanded_key_file);
-  return 1;
+
+  cost.end();
+  XLOG(INFO, cost.getEstimatedCostString());
+
+  if (FLAGS_log_cost) {
+    bool run_name_specified = FLAGS_run_name != "";
+    auto run_name = run_name_specified ? FLAGS_run_name : "temp_run_name";
+    auto party = (FLAGS_party == 0) ? "Publisher" : "Partner";
+    folly::dynamic costDict = cost.getEstimatedCostDynamic(
+        run_name, party, metricCollector->collectMetrics());
+    auto objectName = run_name_specified
+        ? run_name
+        : folly::to<std::string>(
+              FLAGS_run_name, '_', costDict["timestamp"].asString());
+    XLOGF(INFO, "{}", cost.writeToS3(party, objectName, costDict));
+  }
+  return 0;
 }
