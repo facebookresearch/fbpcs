fe66da24319029d886fa51c95ebfc28949de49d2
Ajinkya Ghonge <ajinkyaghonge@meta.com>
2023-06-05 17:43:13 -0700

    Adding KIA deployment script (#2303)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2303
    
    # Context
    KIA Deployment is a python library that we need to deploy as an AWS lambda on advertiser side as part of cloud bridge. Thus adding deployment scripts for the same here.
    
    # Details
    Refer to kia deployment document - https://docs.google.com/document/d/1veMIk6idEHlMZZjRXS0NxhwWNdHlVApxQvQB3ZiMvb0/edit?usp=sharing, for detailed steps to deploy KIA from local machine. But tl;dr, we need to follow the fbpcs/infra/cloud_bridge deployment process and https://www.internalfb.com/code/fbsource/fbcode/fbpcs/infra/cloud_bridge/README.md with some modifications to get the KIA code in the docker directory.
    
    # Next diff
    Will add a flag to only Run KIA cmds when a tee specific flag is enabled.
    
    Reviewed By: wenhaizhu
    
    Differential Revision: D46244452
    
    fbshipit-source-id: ab1677e33370b1ccbc4f563d6991fb24e64a0f69

---
+++ fbpcs/infra/cloud_bridge/key_injection_agent/variable.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  description = "region of the aws resources"
+  default     = "us-west-2"
+}
+
+variable "tag_postfix" {
+  description = "the postfix to append after a resource name or tag"
+  default     = ""
+}
+
+variable "aws_account_id" {
+  description = "your aws account id, that's used to read encrypted S3 files"
+  default     = ""
+}
+
+variable "kia_lambda_function_name" {
+  description = "Name of the KIA lambda"
+  default     = ""
+}
+
+variable "kia_lambda_s3_bucket" {
+  description = "S3 bucket where source code zip file for KIA is stored."
+  default     = ""
+}
+
+variable "kia_lambda_input_bucket" {
+  description = "S3 bucket where input data for KIA is stored."
+  default     = ""
+}
+
+variable "kia_lambda_s3_key" {
+  description = "S3 key for source code zip file for KIA."
+  default     = ""
+}
