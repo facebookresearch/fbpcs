9db0de7b45b34e41f9fde4a37f2873b5d424f9df
Somnath Chakrabarti <somnathc@meta.com>
2023-08-22 18:27:50 -0700

    Deploy base logging infra like kinesis stream, s3 logging bucket etc. (#2327)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2327
    
    There are primarily 3 foundational layers needed to setup observability pipeline in TEE-PL advertiser infra.
    1.  Logging Infra consisting of logging bucket, cloudtrail, cloudwatch and kinesis data streams that captures logs from TEE-PL S3 data and config buckets as well as logs from data ingestion, KIA and cleanup agent lambda
    
    2. Cloudbridge based log ingestion service that ingests all logs from kinesis data stream and pushes it out to Meta infra through dedicated scribe pipes
    
    3. Backend service in Meta to consume and process these logs from these scribe pipes and store them appropriately in Scuba
    
    Please refer to document on "Telemetry design for ingesting Cloudwatch logs and metrics from Advertiser Infrastructure" for more details - https://fburl.com/gdoc/igjkjx02
    
     {F1067376388}
    
    This diff is part of the logging infra that sets up the base components like kinesis stream, s3 logging bucket etc. and their linkages
    
    Reviewed By: ramesc
    
    Differential Revision: D48212731
    
    fbshipit-source-id: 6fe1e0ebd95fdeb98e981f30e36567bbb6ba8cdc

---
+++ fbpcs/infra/cloud_bridge/advertiser_infra_logging/base_logging_infra/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "region of the advertiser aws resources"
+  default     = "us-west-2"
+}
+
+variable "s3_logging_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket where all logs generated from other S3 buckets will be stored"
+  default     = "s3-log-bucket-advertiser"
+}
+
+variable "kinesis_log_stream_name" {
+  type        = string
+  description = "Name of the kinesys stream where various cloudwatch log groups (s3, Lambda etc.) would push logs"
+  default     = "kinesis-log-stream-advertiser"
+}
+
