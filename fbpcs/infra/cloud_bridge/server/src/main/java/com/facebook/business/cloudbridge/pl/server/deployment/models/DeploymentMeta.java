4f16b6edf8cc39adff220490eb0bfe4ef7941e57
Jaroslaw Kasperkiewicz <jkasperkiewicz@meta.com>
2023-09-19 17:39:43 -0700

    Remove lombok dependency from fbpcs/infra/cloud_bridge/server (#2340)
    Summary:
    
    Remove lombok dependency in favor of inlined code.
    Lombok annotations were only applied to two classes, which is not worth maintaining the dependency.
    
    Code was generated using 'delombok' option, and manually cleaned up.
    java -jar ./lombok.jar delombok -p <filename>
    
    Differential Revision: D49326096

--- fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/deployment/models/DeploymentMeta.java
+++ fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/deployment/models/DeploymentMeta.java
@@ -7,12 +7,66 @@
 
 package com.facebook.business.cloudbridge.pl.server.deployment.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class DeploymentMeta {
   private String deploymentId;
   private DeploymentStatus deploymentStatus;
+
+  public DeploymentMeta(final String deploymentId, final DeploymentStatus deploymentStatus) {
+    this.deploymentId = deploymentId;
+    this.deploymentStatus = deploymentStatus;
+  }
+
+  public String getDeploymentId() {
+    return this.deploymentId;
+  }
+
+  public DeploymentStatus getDeploymentStatus() {
+    return this.deploymentStatus;
+  }
+
+  public void setDeploymentId(String deploymentId) {
+    this.deploymentId = deploymentId;
+  }
+
+  public void setDeploymentStatus(DeploymentStatus deploymentStatus) {
+    this.deploymentStatus = deploymentStatus;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (!(o instanceof DeploymentMeta)) return false;
+    DeploymentMeta other = (DeploymentMeta) o;
+    Object this$deploymentId = this.getDeploymentId();
+    Object other$deploymentId = other.getDeploymentId();
+    if (this$deploymentId == null
+        ? other$deploymentId != null
+        : !this$deploymentId.equals(other$deploymentId)) return false;
+    Object this$deploymentStatus = this.getDeploymentStatus();
+    Object other$deploymentStatus = other.getDeploymentStatus();
+    if (this$deploymentStatus == null
+        ? other$deploymentStatus != null
+        : !this$deploymentStatus.equals(other$deploymentStatus)) return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int PRIME = 59;
+    int result = 1;
+    Object $deploymentId = this.getDeploymentId();
+    result = result * PRIME + ($deploymentId == null ? 43 : $deploymentId.hashCode());
+    Object $deploymentStatus = this.getDeploymentStatus();
+    result = result * PRIME + ($deploymentStatus == null ? 43 : $deploymentStatus.hashCode());
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "DeploymentMeta(deploymentId="
+        + getDeploymentId()
+        + ", deploymentStatus="
+        + getDeploymentStatus()
+        + ")";
+  }
 }
