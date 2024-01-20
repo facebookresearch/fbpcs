c0dc1ff7ce14bbca36ed99131457bd88c6132d4f
Somnath Chakrabarti <somnathc@meta.com>
2024-01-19 17:04:04 -0800

    Unified cloudtrail - changes in s3 buckets logging infra (#2375)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2375
    
    Cloudtrail and Cloudwatch subscription filters have fixed quota in cloud infra and the current pc toolkit deployment requires 4 available cloudtrails in the advertiser infra.
    
    This diff reduces the required number of available CloudTrails down to 2 (one for s3 and one for rest of the services).
    
    ## original
    {F1109677040}
    
    ## new
     {F1324452307}
    
    ## This diff
    
    Changes s3 bucket logging infra to use the new s3 unified cloudtrail.
    
    Reviewed By: ankushksingh
    
    Differential Revision: D52864372
    
    fbshipit-source-id: d6fd374740a6b1d54cbcdbfc031239f8c5dcd5f3

--- fbpcs/infra/cloud_bridge/advertiser_infra_logging/s3_bucket_logging/log_to_cloudwatch.tf
+++ fbpcs/infra/cloud_bridge/advertiser_infra_logging/s3_bucket_logging/log_to_cloudwatch.tf
@@ -14,151 +14,27 @@ terraform {
   }
 }
 
-## Create the cloudwatch log group name for TEE-PL data S3 buckets 
-locals {
-  s3_cloudwatch_log_group = "/aws/cloudtrail/s3/${var.s3_bucket_name}"
-}
-
-## Get the existing resource for TEE-PL data S3 bucket
-data "aws_s3_bucket" "s3_bucket" {
-  bucket = var.s3_bucket_name
-}
-
-## Get the existing resource for S3 logging bucket
-data "aws_s3_bucket" "s3_logging_bucket" {
-  bucket = var.s3_logging_bucket_name
-}
-
 ## Get the advertiser infra common kinesis Log Stream
 data "aws_kinesis_stream" "logs_kinesis_stream" {
   name = var.kinesis_log_stream_name
 }
 
-## Create a cloudwatch S3 data event log group
-resource "aws_cloudwatch_log_group" "cloudtrail_s3_logs" {
-  name              = local.s3_cloudwatch_log_group
-  retention_in_days = 7
+## Get the common cloudwatch log group
+data "aws_cloudwatch_log_group" "cloudtrail_logs" {
+  name = "/aws/cloudtrail/s3/${var.installation_tag}-cw-logs"
 }
 
-## Setup s3 cloudtrail iam role and policies to write to cloudwatch log group
-resource "aws_iam_role" "cloudtrail" {
-  name = "${var.s3_bucket_name}-cloudtrail-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "cloudtrail.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+## Get IAM Role of common cloudWatch to publish logs to Kinesis
+data "aws_iam_role" "cloudwatch_kinesis_logging" {
+  name = "${var.installation_tag}-cw-role"
 }
-EOF
-}
-
-
-resource "aws_iam_role_policy" "cloudwatch_logs" {
-  name = "${var.s3_bucket_name}-cloudtrail-cloudwatch-policy"
-  role = aws_iam_role.cloudtrail.id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {   
-            "Sid": "AWSCloudTrailCreateLogs",
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],  
-            "Resource": [
-                "${aws_cloudwatch_log_group.cloudtrail_s3_logs.arn}:*"
-            ]   
-        }  
-    ]   
-}
-EOF
-}
-
-
-## Create IAM Role for CloudWatch to publish logs to Kinesis
-resource "aws_iam_role" "cloudwatch_log" {
-  name = "${var.s3_bucket_name}_cloudwatch_log_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "logs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-## Create IAM Policy for CloudWatch to publish logs to the advertiser infra commot Kinesis stream
-resource "aws_iam_role_policy" "kinesis_write_policy" {
-  name = "${var.s3_bucket_name}_cloudwatch_kinesis_write_policy"
-  role = aws_iam_role.cloudwatch_log.id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {   
-            "Sid": "AWSCloudwatchKinesisWriteLogs",
-            "Effect": "Allow",
-            "Action": [
-                "kinesis:PutRecord"
-            ],  
-            "Resource": [
-                "${data.aws_kinesis_stream.logs_kinesis_stream.arn}"
-            ]   
-        }  
-    ]   
-}
-EOF
-}
-
-
-
-## Setup S3 bucket logs to cloudtrail to cloudwatch pipe
-resource "aws_cloudtrail" "cloudtrail_s3_cloudwatch_logging" {
-  name                       = "${var.s3_bucket_name}-cloudtrail-s3-cloudwatch-logging"
-  s3_bucket_name             = var.s3_logging_bucket_name
-  s3_key_prefix              = "${var.s3_bucket_name}_logs"
-  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail.arn
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail_s3_logs.arn}:*"
-
-  event_selector {
-    read_write_type           = "WriteOnly"
-    include_management_events = false
-
-    data_resource {
-      type   = "AWS::S3::Object"
-      values = ["${data.aws_s3_bucket.s3_bucket.arn}/"]
-    }
-  }
-  depends_on = [aws_cloudwatch_log_group.cloudtrail_s3_logs]
-}
-
 
 ## Push s3 bucket cloudwatch log group to Kinesis stream
 resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_to_kinesis_subscription" {
-  name            = "${var.s3_bucket_name}-cloudwatch-log-to-kinesis-subscription"
-  log_group_name  = aws_cloudwatch_log_group.cloudtrail_s3_logs.name
-  filter_pattern  = "" # forward all logs to kinesis stream
+  name           = "${var.s3_bucket_name}-cloudwatch-log-to-kinesis-subscription"
+  log_group_name = data.aws_cloudwatch_log_group.cloudtrail_logs.name
+  # forward S3 logs for this bucket only to kinesis stream
+  filter_pattern  = "{($.eventSource = \"s3.amazonaws.com\") && ($.requestParameters.bucketName = %${var.s3_bucket_name}%)}"
   destination_arn = data.aws_kinesis_stream.logs_kinesis_stream.arn
-  role_arn        = aws_iam_role.cloudwatch_log.arn
+  role_arn        = data.aws_iam_role.cloudwatch_kinesis_logging.arn
 }
