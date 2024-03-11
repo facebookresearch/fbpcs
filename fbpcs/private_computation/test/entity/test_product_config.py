c8e3049d3801881d15781267038a1a83ae7f479a
Conner Nilsen <connernilsen@meta.com>
2024-03-11 13:46:24 -0700

    Final big migration push [7/22] (#2394)
    Summary: Pull Request resolved: https://github.com/facebookresearch/fbpcs/pull/2394
    
    Reviewed By: kinto0
    
    Differential Revision: D54731232
    
    fbshipit-source-id: 2cf2afd0037c164c530531a453152d3d1747eb09

--- fbpcs/private_computation/test/entity/test_product_config.py
+++ fbpcs/private_computation/test/entity/test_product_config.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 import unittest
 
 from fbpcs.private_computation.entity.product_config import (
