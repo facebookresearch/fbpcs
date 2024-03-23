d190aa6ca565631d014a03414d5afa2f1aa9f3cd
Jeff Bahr <jbahr@meta.com>
2024-03-23 16:07:04 -0700

    Migrate google java format from 1.7 -> 1.21.0
    Summary:
    X-link: https://github.com/facebookexternal/OculusManufacturing/pull/524
    
    X-link: https://github.com/facebookexternal/fbpay/pull/3
    
    X-link: https://github.com/facebook/screenshot-tests-for-android/pull/324
    
    X-link: https://github.com/pytorch/executorch/pull/1771
    
    X-link: https://github.com/facebook/igl/pull/68
    
    X-link: https://github.com/facebook/mariana-trench/pull/153
    
    X-link: https://github.com/facebook/fresco/pull/2757
    
    X-link: https://github.com/facebook/litho/pull/974
    
    X-link: https://github.com/facebook/react-native/pull/42754
    
    X-link: https://github.com/facebook/hhvm/pull/9431
    
    X-link: https://github.com/WhatsApp/eqwalizer/pull/52
    
    X-link: https://github.com/facebookincubator/spectrum/pull/1858
    
    X-link: https://github.com/fbsamples/metapay/pull/1
    
    X-link: https://github.com/facebookincubator/fbjni/pull/95
    
    X-link: https://github.com/facebookincubator/Battery-Metrics/pull/30
    
    X-link: https://github.com/facebook/ktfmt/pull/440
    
    X-link: https://github.com/facebook/flipper/pull/5456
    
    X-link: https://github.com/facebook/hermes/pull/1290
    
    X-link: https://github.com/facebook/TextLayoutBuilder/pull/35
    
    X-link: https://github.com/facebook/SoLoader/pull/122
    
    allow-large-files
    
    This diff migrates google java format form 1.7 to 1.21.0. This update will allow for new language features from java 17 and 21. This diff also formats all necessary files.
    
     Changelog:
        [Internal][Changed] - Updated format from google-java-format 1.7 -> 1.21.0
    
    Reviewed By: IanChilds
    
    Differential Revision: D52786052
    
    fbshipit-source-id: b675ae215084f340b93dfe628e329e696ca0616e

--- fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeploymentParams.java
+++ fbpcs/infra/cloud_bridge/server/src/main/java/com/facebook/business/cloudbridge/pl/server/DeploymentParams.java
@@ -112,7 +112,9 @@ public class DeploymentParams {
       logAndThrow(
           "Invalid Tag Postfix: "
               + tag
-              + "\nMake sure the tag length is less than 20 characters, and using lowercase letters, numbers and dash only.");
+              + "\n"
+              + "Make sure the tag length is less than 20 characters, and using lowercase letters,"
+              + " numbers and dash only.");
     }
     if (!validConfigStorage()) {
       logAndThrow("Invalid Configuration Storage: " + configStorage);
